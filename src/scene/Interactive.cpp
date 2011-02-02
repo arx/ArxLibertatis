8a2f8a2e27245bff0d72b021d9410d2ddc3da331
Erik Lund <culex.eriklund@gmail.com>
2011-02-02 01:42:42 +0100

    Fixed NULL being sent to const std::string& for ScriptEvent::send

--- src/scene/Interactive.cpp
+++ src/scene/Interactive.cpp
@@ -3212,25 +3212,25 @@ void ReloadScript(INTERACTIVE_OBJ * io)
 		if (inter.iobj[num]
 		        &&	inter.iobj[num]->script.data)
 		{
-			ScriptEvent::send(&inter.iobj[num]->script, SM_INIT, "", inter.iobj[num], NULL);
+			ScriptEvent::send(&inter.iobj[num]->script, SM_INIT, "", inter.iobj[num], "");
 		}
 
 		if (inter.iobj[num]
 		        &&	inter.iobj[num]->over_script.data)
 		{
-			ScriptEvent::send(&inter.iobj[num]->over_script, SM_INIT, "", inter.iobj[num], NULL);
+			ScriptEvent::send(&inter.iobj[num]->over_script, SM_INIT, "", inter.iobj[num], "");
 		}
 
 		if (inter.iobj[num]
 		        &&	inter.iobj[num]->script.data)
 		{
-			ScriptEvent::send(&inter.iobj[num]->script, SM_INITEND, "", inter.iobj[num], NULL);
+			ScriptEvent::send(&inter.iobj[num]->script, SM_INITEND, "", inter.iobj[num], "");
 		}
 
 		if (inter.iobj[num]
 		        &&	inter.iobj[num]->over_script.data)
 		{
-			ScriptEvent::send(&inter.iobj[num]->over_script, SM_INITEND, "", inter.iobj[num], NULL);
+			ScriptEvent::send(&inter.iobj[num]->over_script, SM_INITEND, "", inter.iobj[num], "");
 		}
 	}
 }
