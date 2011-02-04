e0a040fc4c4373dcd70ec2104cfba9725f0694c7
Lubosz <lubosz@gmail.com>
2011-02-04 13:56:49 +0100

    remove hardcoded text from GetTextSize

--- src/gui/MenuWidgets.cpp
+++ src/gui/MenuWidgets.cpp
@@ -465,9 +465,6 @@ bool MENU_NoActiveWindow()
 void GetTextSize(HFONT _hFont, const std::string& _lpszUText, int& _iWidth, int& _iHeight)
 {
 	HDC hDC;
-	LogDebug << "GetTextSize " << _lpszUText;
-	//TODO(lubosz): Hardcoded text for klickable menu items
-	string newText = "foo";
 
 	if (danaeApp.m_pddsRenderTarget)
 	{
@@ -477,7 +474,7 @@ void GetTextSize(HFONT _hFont, const std::string& _lpszUText, int& _iWidth, int&
 
 			SIZE sSize;
 
-			GetTextExtentPoint32(hDC, newText.c_str(), newText.length(), &sSize);
+			GetTextExtentPoint32(hDC, _lpszUText.c_str(), _lpszUText.length(), &sSize);
 			_iWidth = sSize.cx;
 			_iHeight = sSize.cy;
 
