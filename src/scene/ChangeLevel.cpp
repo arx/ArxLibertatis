6d1f80e1c1b5029b48981937869c5040c7b96923
Daniel Scharrer <dscharrer@gmail.com>
2011-02-13 23:49:57 +0100

    Remove include from header file.

--- src/scene/ChangeLevel.cpp
+++ src/scene/ChangeLevel.cpp
@@ -85,9 +85,8 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include "io/IO.h"
 #include "io/PakManager.h"
 #include "io/Filesystem.h"
-#include "io/Blast.h"
-#include "io/Implode.h"
 #include "io/Logger.h"
+#include "io/ClusterSave.h"
 
 #include "graphics/d3dwrapper.h"
 #include "graphics/Math.h"
