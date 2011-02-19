b622a7b0de5c255b7f40709056234ead14fc09be
Daniel Scharrer <dscharrer@gmail.com>
2011-02-19 22:44:28 +0100

    Fix warning in Spells.cpp: change variable to size_t.

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -5557,8 +5557,7 @@ bool ARX_SPELLS_Launch( const long& typ, const long& source, const long& flagss,
 				
 				SPELLCAST_Notify(i);
 
-				for (long n=0;n<MAX_SPELLS;n++)
-				{
+				for(size_t n = 0; n < MAX_SPELLS; n++) {
 					if (!spells[n].exist) continue;
 
 					switch (spells[n].type)
