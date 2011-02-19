c6d4988b4000b614bdd25b816d95a79e9e6f4922
Daniel Scharrer <dscharrer@gmail.com>
2011-02-20 00:47:05 +0100

    Fix warning in CollisionShapes.cpp: Change variable to size_t.

--- src/physics/Anchors.cpp
+++ src/physics/Anchors.cpp
@@ -551,7 +551,7 @@ bool ANCHOR_ARX_COLLISION_Move_Cylinder(IO_PHYSICS * ip, INTERACTIVE_OBJ * io, f
 			DIRECT_PATH = false;
 			// Must Attempt To Slide along collisions
 			register EERIE_3D	vecatt;
-			EERIE_3D			rpos		= { 0, 0, 0 };
+			EERIE_3D			rpos		= {{0},{0},{0}};
 			EERIE_3D			lpos		= { 0, 0, 0 };
 			long				RFOUND		=	0;
 			long				LFOUND		=	0;
