db67d2acb099a6e3d3918d7ed8f6a560af0173e5
Daniel Scharrer <dscharrer@gmail.com>
2011-02-20 16:15:30 +0100

    Fix warning in Spells01.cpp: Remove check that is always true.

--- src/graphics/spells/Spells01.cpp
+++ src/graphics/spells/Spells01.cpp
@@ -999,8 +999,6 @@ void CDoze::AddLightDoze(int aiLight)
 {
 	if (ARXPausedTimer)  return;
 
-	if (this->nblight > 255) return;
-
 	this->tablight[this->nblight].actif = 1;
 	this->tablight[this->nblight].iLightNum = aiLight;
 	this->tablight[this->nblight].poslight.x = GLight[aiLight]->pos.x;
