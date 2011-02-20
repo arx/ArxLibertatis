6237af596e74a4b92bf8e376768224355e78fe20
Daniel Scharrer <dscharrer@gmail.com>
2011-02-20 01:29:08 +0100

    Fix warning in Dialog.cpp: Move statement out of if().

--- src/core/Dialog.cpp
+++ src/core/Dialog.cpp
@@ -369,7 +369,8 @@ INT_PTR CALLBACK PathwayOptionsProc(HWND hWnd, UINT uMsg, WPARAM wParam,
 				HDC dc;
 				InvalidateRect(thWnd, NULL, true);
 
-				if (dc = GetDC(thWnd))
+				dc = GetDC(thWnd);
+				if (dc)
 				{
 					RECT rect;
 					GetClientRect(thWnd, &rect);
