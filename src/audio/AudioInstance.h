0319a0c0ac13fe7cf82732ca62ad710de7e8a571
Daniel Scharrer <dscharrer@gmail.com>
2011-02-25 22:45:28 +0100

    Cleanup AudioInstance.h

--- src/audio/AudioInstance.h
+++ src/audio/AudioInstance.h
@@ -22,79 +22,75 @@ If you have questions concerning this license or the applicable additional terms
 ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 ===========================================================================
 */
-#ifndef __ATHENA_INSTANCE_H__
-#define __ATHENA_INSTANCE_H__
+
+#ifndef ARX_AUDIO_AUDIOINSTANCE_H
+#define ARX_AUDIO_AUDIOINSTANCE_H
 
 #include "AudioTypes.h"
 #include "Sample.h"
 #include "Stream.h"
 #include "dsoundfwd.h"
 
-namespace ATHENA
-{
-
-	///////////////////////////////////////////////////////////////////////////////
-	//                                                                           //
-	// Class Athena::Instance                                                    //
-	//                                                                           //
-	///////////////////////////////////////////////////////////////////////////////
-	class Instance
-	{
-		public:
-			// Constructor and destrtructor
-			Instance();
-			~Instance();
-
-			// Setup
-			aalError Init(Sample * sample, const aalChannel & channel);
-			aalError Init(Instance * instance, const aalChannel & channel);
-			aalError Clean();
-			aalError SetVolume(const aalFloat & volume);
-			aalError SetPitch(const aalFloat & pitch);
-			aalError SetPan(const aalFloat & pan);
-			aalError SetPosition(const aalVector & position);
-			aalError SetVelocity(const aalVector & velocity);
-			aalError SetDirection(const aalVector & direction);
-			aalError SetCone(const aalCone & cone);
-			aalError SetFalloff(const aalFalloff & falloff);
-			aalError SetMixer(const aalSLong & mixer_handle);
-			aalError SetEnvironment(const aalSLong & environment_handle);
-
-			//Status
-			aalError GetPosition(aalVector & position) const;
-			aalError GetFalloff(aalFalloff & falloff) const;
-			aalError GetMixer(aalSLong & mixer_handle);
-			aalError GetEnvironment(aalSLong & environment_handle);
-			aalError GetStatistics(aalFloat & average_volume, aalFloat & deviance) const;
-			aalUBool IsIdled();
-			aalUBool IsPlaying();
-			aalULong Time(const aalUnit & unit = AAL_UNIT_MS);
-
-			// Control
-			aalError Play(const aalULong & play_count = 1);
-			aalError Stop();
-			aalError Pause();
-			aalError Resume();
-			aalError Update();
-			aalVoid UpdateStreaming();
-			aalUBool IsTooFar();
-
-			// Data
-			aalSLong id;
-			Sample * sample;
-			aalChannel channel;
-			aalULong status;
-			aalULong callb_i;                     //Next callback index
-			aalULong loop;                        //Remaining loop count
-			aalULong time;                        //Elapsed 'time'
-			Stream * stream;
-			aalULong read, write;                 //Streaming status
-			aalULong size;                        //Buffer size
-			LPDIRECTSOUNDBUFFER lpdsb;
-			LPDIRECTSOUND3DBUFFER lpds3db;
-			LPKSPROPERTYSET lpeax;
+namespace ATHENA {
+	
+	class Instance {
+		
+	public:
+		
+		Instance();
+		~Instance();
+		
+		// Setup
+		aalError Init(Sample * sample, const aalChannel & channel);
+		aalError Init(Instance * instance, const aalChannel & channel);
+		aalError Clean();
+		aalError SetVolume(const aalFloat & volume);
+		aalError SetPitch(const aalFloat & pitch);
+		aalError SetPan(const aalFloat & pan);
+		aalError SetPosition(const aalVector & position);
+		aalError SetVelocity(const aalVector & velocity);
+		aalError SetDirection(const aalVector & direction);
+		aalError SetCone(const aalCone & cone);
+		aalError SetFalloff(const aalFalloff & falloff);
+		aalError SetMixer(const aalSLong & mixer_handle);
+		aalError SetEnvironment(const aalSLong & environment_handle);
+		
+		//Status
+		aalError GetPosition(aalVector & position) const;
+		aalError GetFalloff(aalFalloff & falloff) const;
+		aalError GetMixer(aalSLong & mixer_handle);
+		aalError GetEnvironment(aalSLong & environment_handle);
+		aalError GetStatistics(aalFloat & average_volume, aalFloat & deviance) const;
+		aalUBool IsIdled();
+		aalUBool IsPlaying();
+		aalULong Time(const aalUnit & unit = AAL_UNIT_MS);
+		
+		// Control
+		aalError Play(const aalULong & play_count = 1);
+		aalError Stop();
+		aalError Pause();
+		aalError Resume();
+		aalError Update();
+		aalVoid UpdateStreaming();
+		aalUBool IsTooFar();
+		
+		// Data
+		aalSLong id;
+		Sample * sample;
+		aalChannel channel;
+		aalULong status;
+		aalULong callb_i; // Next callback index
+		aalULong loop; // Remaining loop count
+		aalULong time; // Elapsed 'time'
+		Stream * stream;
+		aalULong read, write; // Streaming status
+		aalULong size; // Buffer size
+		LPDIRECTSOUNDBUFFER lpdsb;
+		LPDIRECTSOUND3DBUFFER lpds3db;
+		LPKSPROPERTYSET lpeax;
+		
 	};
+	
+} // namespace ATHENA
 
-}//ATHENA::
-
-#endif//__ATHENA_INSTANCE_H__
+#endif // ARX_AUDIO_AUDIOINSTANCE_H
