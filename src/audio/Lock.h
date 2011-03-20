6554887a51a74cc3bca0ba45dabeb21cc2683aa6
Daniel Scharrer <dscharrer@gmail.com>
2011-03-21 00:22:02 +0100

    Move lockiing abstraction out of Audio.cpp, fix unity build under under gcc.

---
+++ src/audio/Lock.h
@@ -0,0 +1,38 @@
+
+#ifndef ARX_AUDIO_LOCK_H
+#define ARX_AUDIO_LOCK_H
+
+#include "core/Common.h"
+
+#ifdef HAVE_PTHREADS
+#include <pthread.h>
+#else
+#include <windows.h>
+#endif
+
+class Lock {
+	
+private:
+	
+#ifdef HAVE_PTHREADS
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+	bool locked;
+#elif ARX_PLATFORM == ARX_PLATFORM_WIN32
+	HANDLE mutex;
+#else
+	#warning "locking not supported"
+#endif
+	
+public:
+	
+	Lock();
+	~Lock();
+	
+	bool lock(long timeout);
+	
+	void unlock();
+	
+};
+
+#endif // ARX_AUDIO_LOCK_H
