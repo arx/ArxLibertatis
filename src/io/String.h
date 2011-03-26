7cddbbd288ef9a7f1fe45e8ffc40bbd166128fd6
Erik Lund <culex.eriklund@gmail.com>
2011-03-26 22:45:28 +0100

    Changed new functions to use doxygen style comments

--- src/io/String.h
+++ src/io/String.h
@@ -4,13 +4,30 @@
 
 #include <string>
 
+/**
+ * Converts a given string into uppercase characters
+ * @param A reference to the string being converted
+ * @return A copy of the converted string
+ */
 std::string MakeUpcase(std::string & str);
+
 bool IsIn(const std::string & strin, const std::string & str);
 bool NC_IsIn(std::string strin, std::string str);
 int strcasecmp(const std::string & str1, const std::string & str2);
 int strcmp(const std::string & str1, const std::string & str2);
 
+/**
+ * Converts a given string to an integer using stringstream
+ * @param str The string to be converted
+ * @return The integer created by stringstream
+ */
 int atoi( const std::string& str );
+
+/**
+ * Converts a given integer to a string using stringstream
+ * @param i The integer to be converted
+ * @return The string create by stringstream
+ */
 std::string itoa( int i );
 
 /**
