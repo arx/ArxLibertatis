fd6ae2f876893bbbdbf822c9784dd7b40591607f
Daniel Scharrer <dscharrer@gmail.com>
2011-04-22 05:35:15 +0200

    Cleanup TextManager.h

--- src/gui/TextManager.h
+++ src/gui/TextManager.h
@@ -5,8 +5,8 @@
  *      Author: bmonkey
  */
 
-#ifndef TEXTMANAGER_H_
-#define TEXTMANAGER_H_
+#ifndef ARX_GUI_TEXTMANAGER_H
+#define ARX_GUI_TEXTMANAGER_H
 
 #include <windows.h> // for RECT
 #include <vector>
@@ -14,25 +14,28 @@
 
 class Font;
 
-class TextManager
-{
+class TextManager {
+	
 public:
+	
 	TextManager();
 	~TextManager();
-
-	bool AddText(Font*, const std::string &, const RECT &, long _lCol = -1,
-				 long _lTimeOut = 0, long _lTimeScroll = 0,
+	
+	bool AddText(Font *, const std::string &, const RECT &, long _lCol = -1,
+	             long _lTimeOut = 0, long _lTimeScroll = 0,
 	             float _fSpeedScroll = 0.f, int iNbLigneClipp = 0);
-
-	bool AddText(Font*, const std::string&, long x, long y, long fgcolor );
+	
+	bool AddText(Font* , const std::string &, long x, long y, long fgcolor);
 	void Update(float);
 	void Render();
 	void Clear();
 	bool Empty() const;
 	
 private:
+	
 	struct ManagedText;
-	std::vector<ManagedText *> entries;	
+	std::vector<ManagedText *> entries;
+	
 };
 
-#endif /* TEXTMANAGER_H_ */
+#endif // ARX_GUI_TEXTMANAGER_H
