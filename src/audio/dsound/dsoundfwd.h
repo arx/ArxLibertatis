e0bc12cae955cd655c5d63231ed6bd8cefc852f8
Daniel Scharrer <dscharrer@gmail.com>
2011-04-24 02:37:23 +0200

    Change include guard defines in dsound headers.

--- src/audio/dsound/dsoundfwd.h
+++ src/audio/dsound/dsoundfwd.h
@@ -1,21 +1,21 @@
 
-#ifndef DSOUNDFWD_H
-#define DSOUNDFWD_H
+#ifndef ARX_AUDIO_DSOUND_DSOUNDFWD_H
+#define ARX_AUDIO_DSOUND_DSOUNDFWD_H
 
 // dsound.h forward-declarations
 
 struct IKsPropertySet;
-typedef struct IKsPropertySet *LPKSPROPERTYSET;
+typedef struct IKsPropertySet * LPKSPROPERTYSET;
 
 struct IDirectSoundBuffer;
-typedef struct IDirectSoundBuffer *LPDIRECTSOUNDBUFFER;
+typedef struct IDirectSoundBuffer * LPDIRECTSOUNDBUFFER;
 struct IDirectSound3DBuffer;
-typedef struct IDirectSound3DBuffer *LPDIRECTSOUND3DBUFFER;
+typedef struct IDirectSound3DBuffer * LPDIRECTSOUND3DBUFFER;
 
 struct IDirectSound;
-typedef struct IDirectSound *LPDIRECTSOUND;
+typedef struct IDirectSound * LPDIRECTSOUND;
 
 struct IDirectSound3DListener;
-typedef struct IDirectSound3DListener *LPDIRECTSOUND3DLISTENER;
+typedef struct IDirectSound3DListener * LPDIRECTSOUND3DLISTENER;
 
-#endif // DSOUNDFWD_H
+#endif // ARX_AUDIO_DSOUND_DSOUNDFWD_H
