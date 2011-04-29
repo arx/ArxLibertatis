32c8cce1be67d1580342ad8256e65419da5b5450
Daniel Scharrer <dscharrer@gmail.com>
2011-04-29 21:49:04 +0200

    Remove using namespace from Sample.cpp

--- src/audio/Sample.cpp
+++ src/audio/Sample.cpp
@@ -30,8 +30,6 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include "audio/AudioBackend.h"
 #include "audio/AudioSource.h"
 
-using namespace std;
-
 namespace audio {
 
 Sample::Sample(const string & _name) : ResourceHandle(), name(_name), length(0) {
