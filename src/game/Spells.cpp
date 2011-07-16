3a72b33c6dd4abc05759b7296f8a975599449749
Daniel Scharrer <dscharrer@gmail.com>
2011-07-16 21:15:33 +0200

    Convert strings to lowercase in Spells.cpp

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -764,27 +764,27 @@ static void ARX_SPELLS_RequestSymbolDraw2(INTERACTIVE_OBJ *io, Rune symb, float 
 //-----------------------------------------------------------------------------
 void ARX_SPELLS_RequestSymbolDraw3(const char *_pcName,char *_pcRes)
 {
-	if		(!strcmp(_pcName, "AAM"))		strcpy(_pcRes, "6666");
-	else if (!strcmp(_pcName, "CETRIUS"))	strcpy(_pcRes, "33388886666");
-	else if (!strcmp(_pcName, "COMUNICATUM")) 	strcpy(_pcRes, "6666622244442226666");
-	else if (!strcmp(_pcName, "COSUM"))     strcpy(_pcRes, "66666222244448888");
-	else if (!strcmp(_pcName, "FOLGORA"))   strcpy(_pcRes, "99993333");
-	else if (!strcmp(_pcName, "FRIDD"))		strcpy(_pcRes, "888886662222");
-	else if (!strcmp(_pcName, "KAOM"))		strcpy(_pcRes, "44122366");
-	else if (!strcmp(_pcName, "MEGA"))		strcpy(_pcRes, "88888");
-	else if (!strcmp(_pcName, "MORTE"))		strcpy(_pcRes, "66666222");
-	else if (!strcmp(_pcName, "MOVIS"))		strcpy(_pcRes, "666611116666");
-	else if (!strcmp(_pcName, "NHI"))		strcpy(_pcRes, "4444");
-	else if (!strcmp(_pcName, "RHAA"))		strcpy(_pcRes, "22222");
-	else if (!strcmp(_pcName, "SPACIUM"))	strcpy(_pcRes, "44444222266688");
-	else if (!strcmp(_pcName, "STREGUM"))	strcpy(_pcRes, "8888833338888");
-	else if (!strcmp(_pcName, "TAAR"))		strcpy(_pcRes, "666222666");
-	else if (!strcmp(_pcName, "TEMPUS"))	strcpy(_pcRes, "88886662226668866");
-	else if (!strcmp(_pcName, "TERA"))		strcpy(_pcRes, "99922266");
-	else if (!strcmp(_pcName, "VISTA"))		strcpy(_pcRes, "333111");
-	else if (!strcmp(_pcName, "VITAE"))		strcpy(_pcRes, "66666888");
-	else if (!strcmp(_pcName, "YOK"))		strcpy(_pcRes, "222226666888");
-	else if (!strcmp(_pcName, "AKBAA"))		strcpy(_pcRes, "22666772222");
+	if		(!strcmp(_pcName, "aam"))		strcpy(_pcRes, "6666");
+	else if (!strcmp(_pcName, "cetrius"))	strcpy(_pcRes, "33388886666");
+	else if (!strcmp(_pcName, "comunicatum")) 	strcpy(_pcRes, "6666622244442226666");
+	else if (!strcmp(_pcName, "cosum"))     strcpy(_pcRes, "66666222244448888");
+	else if (!strcmp(_pcName, "folgora"))   strcpy(_pcRes, "99993333");
+	else if (!strcmp(_pcName, "fridd"))		strcpy(_pcRes, "888886662222");
+	else if (!strcmp(_pcName, "kaom"))		strcpy(_pcRes, "44122366");
+	else if (!strcmp(_pcName, "mega"))		strcpy(_pcRes, "88888");
+	else if (!strcmp(_pcName, "morte"))		strcpy(_pcRes, "66666222");
+	else if (!strcmp(_pcName, "movis"))		strcpy(_pcRes, "666611116666");
+	else if (!strcmp(_pcName, "nhi"))		strcpy(_pcRes, "4444");
+	else if (!strcmp(_pcName, "rhaa"))		strcpy(_pcRes, "22222");
+	else if (!strcmp(_pcName, "spacium"))	strcpy(_pcRes, "44444222266688");
+	else if (!strcmp(_pcName, "stregum"))	strcpy(_pcRes, "8888833338888");
+	else if (!strcmp(_pcName, "taar"))		strcpy(_pcRes, "666222666");
+	else if (!strcmp(_pcName, "tempus"))	strcpy(_pcRes, "88886662226668866");
+	else if (!strcmp(_pcName, "tera"))		strcpy(_pcRes, "99922266");
+	else if (!strcmp(_pcName, "vista"))		strcpy(_pcRes, "333111");
+	else if (!strcmp(_pcName, "vitae"))		strcpy(_pcRes, "66666888");
+	else if (!strcmp(_pcName, "yok"))		strcpy(_pcRes, "222226666888");
+	else if (!strcmp(_pcName, "akbaa"))		strcpy(_pcRes, "22666772222");
 }
 
 #define OFFSET_X 8*2//0
@@ -833,172 +833,172 @@ static bool MakeSpellName(char * spell, Spell num) {
 	{
 		// Level 1
 		case SPELL_MAGIC_SIGHT           :
-			strcpy(spell, "MAGIC_SIGHT");
+			strcpy(spell, "magic_sight");
 			break;
 		case SPELL_MAGIC_MISSILE         :
-			strcpy(spell, "MAGIC_MISSILE");
+			strcpy(spell, "magic_missile");
 			break;
 		case SPELL_IGNIT                 :
-			strcpy(spell, "IGNIT");
+			strcpy(spell, "ignit");
 			break;
 		case SPELL_DOUSE                 :
-			strcpy(spell, "DOUSE");
+			strcpy(spell, "douse");
 			break;
 		case SPELL_ACTIVATE_PORTAL       :
-			strcpy(spell, "ACTIVATE_PORTAL");
+			strcpy(spell, "activate_portal");
 			break;
 
 		// Level 2
 		case SPELL_HEAL                  :
-			strcpy(spell, "HEAL");
+			strcpy(spell, "heal");
 			break;
 		case SPELL_DETECT_TRAP           :
-			strcpy(spell, "DETECT_TRAP");
+			strcpy(spell, "detect_trap");
 			break;
 		case SPELL_ARMOR                 :
-			strcpy(spell, "ARMOR");
+			strcpy(spell, "armor");
 			break;
 		case SPELL_LOWER_ARMOR           :
-			strcpy(spell, "LOWER_ARMOR");
+			strcpy(spell, "lower_armor");
 			break;
 		case SPELL_HARM                  :
-			strcpy(spell, "HARM");
+			strcpy(spell, "harm");
 			break;
 
 		// Level 3
 		case SPELL_SPEED                 :
-			strcpy(spell, "SPEED");
+			strcpy(spell, "speed");
 			break;
 		case SPELL_DISPELL_ILLUSION      :
-			strcpy(spell, "DISPELL_ILLUSION");
+			strcpy(spell, "dispell_illusion");
 			break;
 		case SPELL_FIREBALL              :
-			strcpy(spell, "FIREBALL");
+			strcpy(spell, "fireball");
 			break;
 		case SPELL_CREATE_FOOD           :
-			strcpy(spell, "CREATE_FOOD");
+			strcpy(spell, "create_food");
 			break;
 		case SPELL_ICE_PROJECTILE        :
-			strcpy(spell, "ICE_PROJECTILE");
+			strcpy(spell, "ice_projectile");
 			break;
 
 		// Level 4 
 		case SPELL_BLESS                 :
-			strcpy(spell, "BLESS");
+			strcpy(spell, "bless");
 			break;
 		case SPELL_DISPELL_FIELD         :
-			strcpy(spell, "DISPELL_FIELD");
+			strcpy(spell, "dispell_field");
 			break;
 		case SPELL_FIRE_PROTECTION       :
-			strcpy(spell, "FIRE_PROTECTION");
+			strcpy(spell, "fire_protection");
 			break;
 		case SPELL_TELEKINESIS           :
-			strcpy(spell, "TELEKINESIS");
+			strcpy(spell, "telekinesis");
 			break;
 		case SPELL_CURSE                 :
-			strcpy(spell, "CURSE");
+			strcpy(spell, "curse");
 			break;
 		case SPELL_COLD_PROTECTION       :
-			strcpy(spell, "COLD_PROTECTION");
+			strcpy(spell, "cold_protection");
 			break;
 
 		// Level 5 
 		case SPELL_RUNE_OF_GUARDING      :
-			strcpy(spell, "RUNE_OF_GUARDING");
+			strcpy(spell, "rune_of_guarding");
 			break;
 		case SPELL_LEVITATE              :
-			strcpy(spell, "LEVITATE");
+			strcpy(spell, "levitate");
 			break;
 		case SPELL_CURE_POISON           :
-			strcpy(spell, "CURE_POISON");
+			strcpy(spell, "cure_poison");
 			break;
 		case SPELL_REPEL_UNDEAD          :
-			strcpy(spell, "REPEL_UNDEAD");
+			strcpy(spell, "repel_undead");
 			break;
 		case SPELL_POISON_PROJECTILE     :
-			strcpy(spell, "POISON_PROJECTILE");
+			strcpy(spell, "poison_projectile");
 			break;
 
 		// Level 6 
 		case SPELL_RISE_DEAD             :
-			strcpy(spell, "RAISE_DEAD");
+			strcpy(spell, "raise_dead");
 			break;
 		case SPELL_PARALYSE              :
-			strcpy(spell, "PARALYSE");
+			strcpy(spell, "paralyse");
 			break;
 		case SPELL_CREATE_FIELD          :
-			strcpy(spell, "CREATE_FIELD");
+			strcpy(spell, "create_field");
 			break;
 		case SPELL_DISARM_TRAP           :
-			strcpy(spell, "DISARM_TRAP");
+			strcpy(spell, "disarm_trap");
 			break;
 		case SPELL_SLOW_DOWN             :
-			strcpy(spell, "SLOWDOWN");
+			strcpy(spell, "slowdown");
 			break;
 
 		// Level 7  
 		case SPELL_FLYING_EYE            :
-			strcpy(spell, "FLYING_EYE");
+			strcpy(spell, "flying_eye");
 			break;
 		case SPELL_FIRE_FIELD            :
-			strcpy(spell, "FIRE_FIELD");
+			strcpy(spell, "fire_field");
 			break;
 		case SPELL_ICE_FIELD             :
-			strcpy(spell, "ICE_FIELD");
+			strcpy(spell, "ice_field");
 			break;
 		case SPELL_LIGHTNING_STRIKE      :
-			strcpy(spell, "LIGHTNING_STRIKE");
+			strcpy(spell, "lightning_strike");
 			break;
 		case SPELL_CONFUSE               :
-			strcpy(spell, "CONFUSE");
+			strcpy(spell, "confuse");
 			break;
 
 		// Level 8
 		case SPELL_INVISIBILITY          :
-			strcpy(spell, "INVISIBILITY");
+			strcpy(spell, "invisibility");
 			break;
 		case SPELL_MANA_DRAIN            :
-			strcpy(spell, "MANA_DRAIN");
+			strcpy(spell, "mana_drain");
 			break;
 		case SPELL_EXPLOSION             :
-			strcpy(spell, "EXPLOSION");
+			strcpy(spell, "explosion");
 			break;
 		case SPELL_ENCHANT_WEAPON        :
-			strcpy(spell, "ENCHANT_WEAPON");
+			strcpy(spell, "enchant_weapon");
 			break;
 		case SPELL_LIFE_DRAIN            :
-			strcpy(spell, "LIFE_DRAIN");
+			strcpy(spell, "life_drain");
 			break;
 
 		// Level 9
 		case SPELL_SUMMON_CREATURE       :
-			strcpy(spell, "SUMMON_CREATURE");
+			strcpy(spell, "summon_creature");
 			break;
 		case SPELL_FAKE_SUMMON		     :
-			strcpy(spell, "FAKE_SUMMON");
+			strcpy(spell, "fake_summon");
 			break;
 		case SPELL_NEGATE_MAGIC          :
-			strcpy(spell, "NEGATE_MAGIC");
+			strcpy(spell, "negate_magic");
 			break;
 		case SPELL_INCINERATE            :
-			strcpy(spell, "INCINERATE");
+			strcpy(spell, "incinerate");
 			break;
 		case SPELL_MASS_PARALYSE         :
-			strcpy(spell, "MASS_PARALYSE");
+			strcpy(spell, "mass_paralyse");
 			break;
 
 		// Level 10
 		case SPELL_MASS_LIGHTNING_STRIKE :
-			strcpy(spell, "MASS_LIGHTNING_STRIKE");
+			strcpy(spell, "mass_lightning_strike");
 			break;
 		case SPELL_CONTROL_TARGET        :
-			strcpy(spell, "CONTROL");
+			strcpy(spell, "control");
 			break;
 		case SPELL_FREEZE_TIME           :
-			strcpy(spell, "FREEZE_TIME");
+			strcpy(spell, "freeze_time");
 			break;
 		case SPELL_MASS_INCINERATE       :
-			strcpy(spell, "MASS_INCINERATE");
+			strcpy(spell, "mass_incinerate");
 			break;
 		default :
 			return false;
@@ -2678,27 +2678,27 @@ void ARX_SPELLS_Init_Rects() {
 	lMaxSymbolDrawSizeX=INT_MIN;
 	lMaxSymbolDrawSizeY=INT_MIN;
 
-	ARX_SPEELS_GetMaxRect("AAM");
-	ARX_SPEELS_GetMaxRect("CETRIUS");
-	ARX_SPEELS_GetMaxRect("COMUNICATUM");
-	ARX_SPEELS_GetMaxRect("COSUM");
-	ARX_SPEELS_GetMaxRect("FOLGORA");
-	ARX_SPEELS_GetMaxRect("FRIDD");
-	ARX_SPEELS_GetMaxRect("KAOM");
-	ARX_SPEELS_GetMaxRect("MEGA");
-	ARX_SPEELS_GetMaxRect("MORTE");
-	ARX_SPEELS_GetMaxRect("MOVIS");
-	ARX_SPEELS_GetMaxRect("NHI");
-	ARX_SPEELS_GetMaxRect("RHAA");
-	ARX_SPEELS_GetMaxRect("SPACIUM");
-	ARX_SPEELS_GetMaxRect("STREGUM");
-	ARX_SPEELS_GetMaxRect("TAAR");
-	ARX_SPEELS_GetMaxRect("TEMPUS");
-	ARX_SPEELS_GetMaxRect("TERA");
-	ARX_SPEELS_GetMaxRect("VISTA");
-	ARX_SPEELS_GetMaxRect("VITAE");
-	ARX_SPEELS_GetMaxRect("YOK");
-	ARX_SPEELS_GetMaxRect("AKBAA");
+	ARX_SPEELS_GetMaxRect("aam");
+	ARX_SPEELS_GetMaxRect("cetrius");
+	ARX_SPEELS_GetMaxRect("comunicatum");
+	ARX_SPEELS_GetMaxRect("cosum");
+	ARX_SPEELS_GetMaxRect("folgora");
+	ARX_SPEELS_GetMaxRect("fridd");
+	ARX_SPEELS_GetMaxRect("kaom");
+	ARX_SPEELS_GetMaxRect("mega");
+	ARX_SPEELS_GetMaxRect("morte");
+	ARX_SPEELS_GetMaxRect("movis");
+	ARX_SPEELS_GetMaxRect("nhi");
+	ARX_SPEELS_GetMaxRect("rhaa");
+	ARX_SPEELS_GetMaxRect("spacium");
+	ARX_SPEELS_GetMaxRect("stregum");
+	ARX_SPEELS_GetMaxRect("taar");
+	ARX_SPEELS_GetMaxRect("tempus");
+	ARX_SPEELS_GetMaxRect("tera");
+	ARX_SPEELS_GetMaxRect("vista");
+	ARX_SPEELS_GetMaxRect("vitae");
+	ARX_SPEELS_GetMaxRect("yok");
+	ARX_SPEELS_GetMaxRect("akbaa");
 }
 
 //-----------------------------------------------------------------------------
@@ -3272,7 +3272,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 		Player_Magic_Level = ARX_CLEAN_WARN_CAST_FLOAT(level);
 	}
 
-	static TextureContainer * tc4 = TextureContainer::Load("Graph\\Particles\\smoke.bmp");
+	static TextureContainer * tc4 = TextureContainer::Load("graph\\particles\\smoke.bmp");
 
 
 	if ((target < 0)
@@ -3326,7 +3326,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 					&&	( ioo->ioflags & IO_NPC )
 					&&	( ioo->_npcdata->life > 0.f )
 					&&	( ioo->show == SHOW_FLAG_IN_SCENE )
-					&&	( IsIOGroup( ioo, "DEMON") )	
+					&&	( IsIOGroup( ioo, "demon") )	
 					&&	( distSqr(ioo->pos, cpos) < square(900.f))
 					)
 				{
@@ -5115,7 +5115,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			{
 				pCSpellFx->spellinstance=i;
 				char tmptext[256];
-				strcpy(tmptext,"Graph\\Obj3D\\Interactive\\Fix_inter\\blue_cube\\blue_cube.asl");
+				strcpy(tmptext,"graph\\obj3d\\interactive\\fix_inter\\blue_cube\\blue_cube.asl");
 				INTERACTIVE_OBJ * io;
 				io=AddFix(tmptext,IO_IMMEDIATELOAD);
 
@@ -6276,14 +6276,14 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 					&&	(ioo->ioflags & IO_NPC)
 					&&	(ioo->_npcdata->life>0.f)
 					&&	(ioo->show==SHOW_FLAG_IN_SCENE)
-					&&	(IsIOGroup(ioo,"DEMON"))	
+					&&	(IsIOGroup(ioo, "demon"))
 					&&	(distSqr(ioo->pos, spells[i].caster_pos) < square(900.f))
 					)
 				{
 					tcount++;
 					long n = spells[i].caster_level;
 					std::string str = tmp_io->long_name() + ' ' + itoa(n);
-					SendIOScriptEvent( ioo, SM_NULL, str, "NPC_CONTROL" );
+					SendIOScriptEvent( ioo, SM_NULL, str, "npc_control" );
 				}
 			}
 
@@ -6408,7 +6408,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 //*************************************************************************************
 void ARX_SPELLS_Kill(long i) {
 	
-	static TextureContainer * tc4=TextureContainer::Load("Graph\\Particles\\smoke.bmp");
+	static TextureContainer * tc4=TextureContainer::Load("graph\\particles\\smoke.bmp");
 
 	if (!spells[i].exist) return;
 
@@ -7604,7 +7604,7 @@ void ARX_SPELLS_Update()
 
 							if (EEfabs(anything)<30)
 							{
-								strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\Undead_base\\Undead_base.asl");
+								strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\undead_base\\undead_base.asl");
 								INTERACTIVE_OBJ * io;
 								io=AddNPC(tmptext,IO_IMMEDIATELOAD);
 
@@ -8003,7 +8003,7 @@ void ARX_SPELLS_Update()
 							if (spells[i].caster_level>=9)
 							{
 								tokeep=1;
-								strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\Demon\\Demon.asl");
+								strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\demon\\demon.asl");
 							}
 							else 
 							{
@@ -8011,22 +8011,22 @@ void ARX_SPELLS_Update()
 
 								if (rnd()>0.98f)
 								{
-									strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\WRat_base\\WRat_base.asl");
+									strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\wrat_base\\wrat_base.asl");
 									tokeep=-1;
 								}
 								else
-									strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\Chicken_Base\\Chicken_Base.asl");
+									strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\chicken_base\\chicken_base.asl");
 							}
 
 							if ((rnd()>0.997f) || ((cur_rf>=3) && (rnd()>0.8f)) || ((cur_mr>=3) && (rnd()>0.3f)))
 							{
-								strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\WRat_base\\WRat_base.asl");
+								strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\wrat_base\\wrat_base.asl");
 								tokeep=-1;
 							}
 
 							if ((rnd()>0.997f) || (sp_max && (rnd()>0.8f)) || ((cur_mr>=3) && (rnd()>0.3f)))
 							{
-								strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\Y_mx\\Y_mx.asl");
+								strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\y_mx\\y_mx.asl");
 								tokeep=0;
 							}
 
@@ -8034,12 +8034,12 @@ void ARX_SPELLS_Update()
 							{
 								if (rnd()>0.5) 
 								{
-									strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\WRat_base\\WRat_base.asl");
+									strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\wrat_base\\wrat_base.asl");
 									tokeep=-1;
 								}
 								else
 								{
-									strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\Y_mx\\Y_mx.asl");
+									strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\y_mx\\y_mx.asl");
 									tokeep=0;
 								}
 							}
@@ -8049,7 +8049,7 @@ void ARX_SPELLS_Update()
 
 							if (!io)
 							{
-								strcpy(tmptext,"Graph\\Obj3D\\Interactive\\NPC\\Chicken_Base\\Chicken_Base.asl");
+								strcpy(tmptext,"graph\\obj3d\\interactive\\npc\\chicken_base\\chicken_base.asl");
 								tokeep=0;
 								io=AddNPC(tmptext,IO_IMMEDIATELOAD);
 							}
@@ -8597,7 +8597,7 @@ static void ApplySPWep() {
 		ARX_SPSound();
 		std::string tex;
 		std::string tex2;
-		tex2 = "Graph\\Obj3D\\Interactive\\Items\\Weapons\\sword_mx\\sword_mx.teo";
+		tex2 = "graph\\obj3d\\interactive\\items\\weapons\\sword_mx\\sword_mx.teo";
 		File_Standardize(tex2,tex);
 		INTERACTIVE_OBJ * ioo=(INTERACTIVE_OBJ *)AddItem(tex,IO_IMMEDIATELOAD);
 
@@ -8663,7 +8663,7 @@ static void ApplyCurSOS() {
 static void ApplySPBow() {
 	
 	ARX_SPSound();
-	const char OBJ_BOW[] = "Graph\\Obj3D\\Interactive\\Items\\Weapons\\bow_mx\\bow_mx.teo";
+	const char OBJ_BOW[] = "graph\\obj3d\\interactive\\items\\weapons\\bow_mx\\bow_mx.teo";
 	INTERACTIVE_OBJ * ioo=(INTERACTIVE_OBJ *)AddItem(OBJ_BOW,IO_IMMEDIATELOAD);
 	
 	if(ioo!=NULL) {
@@ -8692,13 +8692,13 @@ static void ApplySPArm() {
 	switch (sp_arm)
 	{
 		case 0:
-			tex2 = "%sGraph\\Obj3D\\Interactive\\Items\\Armor\\Helmet_plate_cm\\Helmet_plate_cm.teo";
+			tex2 = "%sgraph\\obj3d\\interactive\\items\\armor\\helmet_plate_cm\\helmet_plate_cm.teo";
 		break;
 		case 1:
-			tex2 = "%sGraph\\Obj3D\\Interactive\\Items\\Armor\\Legging_plate_cm\\Legging_plate_cm.teo";
+			tex2 = "%sgraph\\obj3d\\interactive\\items\\armor\\legging_plate_cm\\legging_plate_cm.teo";
 		break;
 		case 2:
-			tex2 = "%sGraph\\Obj3D\\Interactive\\Items\\Armor\\Chest_plate_cm\\Chest_plate_cm.teo";
+			tex2 = "%sgraph\\obj3d\\interactive\\items\\armor\\chest_plate_cm\\chest_plate_cm.teo";
 		break;
 		default:
 			return;
@@ -8716,7 +8716,7 @@ static void ApplySPArm() {
 		ioo->scriptload=1;
 		MakeTemporaryIOIdent(ioo);
 		SendInitScriptEvent(ioo);
-		ioo->show=SHOW_FLAG_IN_INVENTORY;									
+		ioo->show=SHOW_FLAG_IN_INVENTORY;
 
 		if (!CanBePutInInventory(ioo))
 			PutInFrontOfPlayer(ioo);
@@ -8831,14 +8831,14 @@ static void ApplySPMax() {
 	}
 	else
 	{
-		TextureContainer * tcm=TextureContainer::Load("Graph\\Obj3D\\Textures\\npc_human_cm_hero_head.bmp");
+		TextureContainer * tcm=TextureContainer::Load("graph\\obj3d\\textures\\npc_human_cm_hero_head.bmp");
 
 		if (tcm)
 		{
 			delete tcm;
-			player.heads[0]=TextureContainer::Load("Graph\\Obj3D\\Textures\\npc_human_base_hero_head.bmp");	
-			player.heads[1]=TextureContainer::Load("Graph\\Obj3D\\Textures\\npc_human_base_hero2_head.bmp");	
-			player.heads[2]=TextureContainer::Load("Graph\\Obj3D\\Textures\\npc_human_base_hero3_head.bmp");	
+			player.heads[0]=TextureContainer::Load("graph\\obj3d\\textures\\npc_human_base_hero_head.bmp");
+			player.heads[1]=TextureContainer::Load("graph\\obj3d\\textures\\npc_human_base_hero2_head.bmp");
+			player.heads[2]=TextureContainer::Load("graph\\obj3d\\textures\\npc_human_base_hero3_head.bmp");
 			ARX_EQUIPMENT_RecreatePlayerMesh();
 		}
 	}	
