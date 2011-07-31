b5e0aa04bb8ab57550de283af9de3924bdfaf753
Daniel Scharrer <dscharrer@gmail.com>
2011-07-31 20:20:25 +0200

    Add missing space in assert message.

--- src/audio/openal/OpenALSource.cpp
+++ src/audio/openal/OpenALSource.cpp
@@ -774,7 +774,7 @@ aalError OpenALSource::updateBuffers() {
 	time = time - read + newRead;
 	TraceAL("update: read " << read << " -> " << newRead << "  time " << oldTime << " -> " << time);
 	
-	arx_assert_msg(time >= oldTime, " oldTime=" PRINT_SIZE_T "time=" PRINT_SIZE_T " read=" PRINT_SIZE_T " newRead=%d nbuffersProcessed=%d status=%d sourceState=%d" , oldTime, time, read, newRead, nbuffersProcessed, (int)status, sourceState);
+	arx_assert_msg(time >= oldTime, " oldTime=" PRINT_SIZE_T " time=" PRINT_SIZE_T " read=" PRINT_SIZE_T " newRead=%d nbuffersProcessed=%d status=%d sourceState=%d" , oldTime, time, read, newRead, nbuffersProcessed, (int)status, sourceState);
 	ARX_UNUSED(oldTime);
 	read = newRead;
 	
