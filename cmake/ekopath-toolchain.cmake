5bf2978e49002a664dde5a5e6a63cda211673d89
Daniel Scharrer <dscharrer@gmail.com>
2011-08-30 16:14:54 +0200

    Tell cmake that ekopath supports -isystem.

--- cmake/ekopath-toolchain.cmake
+++ cmake/ekopath-toolchain.cmake
@@ -12,3 +12,7 @@ endif()
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER "${Ekopath}")
 set(CMAKE_CXX_COMPILER "${EkopathXX}")
+
+# tell cmake that ekopath supports -isystem
+set(CMAKE_INCLUDE_SYSTEM_FLAG_C "-isystem ")
+set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
