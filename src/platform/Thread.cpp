5f98c3214f433c36f7493a4679a156952189f327
Daniel Scharrer <dscharrer@gmail.com>
2011-09-20 14:20:05 +0200

    Don't make any assumptions about the pthread_t type.

--- src/platform/Thread.cpp
+++ src/platform/Thread.cpp
@@ -7,13 +7,13 @@
 
 #include <sched.h>
 
-Thread::Thread() : thread((pthread_t)0) {
+Thread::Thread() : started(false) {
 	setPriority(Normal);
 }
 
 void Thread::start() {
 	
-	if(thread) {
+	if(started) {
 		return;
 	}
 	
@@ -28,6 +28,7 @@ void Thread::start() {
 	
 	pthread_attr_destroy(&attr);
 	
+	started = true;
 }
 
 void Thread::setPriority(Priority _priority) {
@@ -39,7 +40,7 @@ void Thread::setPriority(Priority _priority) {
 	
 	priority = min + ((_priority - Lowest) * (max - min) / (Highest - Lowest));
 	
-	if(thread) {
+	if(started) {
 		pthread_setschedprio(thread, priority);
 	}
 }
@@ -47,7 +48,7 @@ void Thread::setPriority(Priority _priority) {
 Thread::~Thread() { }
 
 void Thread::waitForCompletion() {
-	if(thread) {
+	if(started) {
 		pthread_join(thread, NULL);
 	}
 }
