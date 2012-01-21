arx/ArxLibertatis

commit 0ad8b728c69c78601c3ff7ec05c54794c5386be9

Allow NPCs to move even if they are culled by the portal system.

src/animation/AnimationRender.cpp
@@ -217,13 +217,13 @@ static void Cedric_GetTime(float & timm, INTERACTIVE_OBJ * io) {
 	}
 }
 
-
 /* Evaluate main entity translation */
-static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse, float scale, bool render, Vec3f & ftr, Vec3f & ftr2)
-{
+static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse, float scale,
+                                       Vec3f & ftr) {
+	
 	// Resets Frame Translate
 	ftr = Vec3f::ZERO;
-	ftr2 = Vec3f::ZERO;
+	Vec3f ftr2 = Vec3f::ZERO;
 
 
 	// Fill frame translate values with multi-layer translate informations...
@@ -272,7 +272,7 @@ static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse,
 			// Linear interpolation of object translation (MOVE)
 			ftr = sFrame->translate + (eFrame->translate - sFrame->translate) * animuse->pour;
 
-			if(io && render) {
+			if(io) {
 				
 				ftr *= scale;
 
@@ -288,7 +288,7 @@ static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse,
 		}
 	}
 	
-	if(io && (io->animlayer[0].cur_anim) && render) {
+	if(io && io->animlayer[0].cur_anim) {
 		
 		// Use calculated value to notify the Movement engine of the translation to do
 		if(io->ioflags & IO_NPC) {
@@ -2183,7 +2183,7 @@ void Cedric_AnimateDrawEntity(EERIE_3DOBJ * eobj,
 	float 				invisibility;
 	float 				scale;
 	float				timm;
-	Vec3f			ftr, ftr2;
+	Vec3f			ftr;
 	EERIE_C_DATA	*	obj;
 
 	
@@ -2210,7 +2210,7 @@ void Cedric_AnimateDrawEntity(EERIE_3DOBJ * eobj,
 
 	memset(grps, 0, eobj->nbgroups);
 
-	Cedric_AnimCalcTranslation(io, animuse, scale, render, ftr, ftr2);
+	Cedric_AnimCalcTranslation(io, animuse, scale, ftr);
 
 	if(Cedric_IO_Visible(io)) {
 

commit e2c05343d13595664ab6d4212eb6041c9059e205

Don't update the player's movement vector when not actually rendering. This fixes a bug in cinematics caused by 0ad8b728c69c78601c3ff7ec05c54794c5386be9.

src/animation/Animation.cpp
@@ -602,7 +602,8 @@ void EERIEDrawAnimQuat(EERIE_3DOBJ * eobj,
                        Vec3f * pos,
                        unsigned long time,
                        INTERACTIVE_OBJ * io,
-                       bool render) {
+                       bool render,
+                       bool update_movement) {
 	
 	if(io && io != inter.iobj[0]) {
 		
@@ -644,7 +645,7 @@ suite:
 
 	DESTROYED_DURING_RENDERING=NULL;
 
-	Cedric_AnimateDrawEntity(eobj, eanim, angle, pos, io, render);
+	Cedric_AnimateDrawEntity(eobj, eanim, angle, pos, io, render, update_movement);
 }
 
 extern float GLOBAL_LIGHT_FACTOR;

src/animation/Animation.h
@@ -88,7 +88,7 @@ TexturedVertex * PushVertexInTableCull_TMetal(TextureContainer * tex);
 void CalculateInterZMapp(EERIE_3DOBJ * _pobj3dObj, long lIdList, long * _piInd, TextureContainer * _pTex, TexturedVertex * _pVertex);
 void EERIE_ANIMMANAGER_ReloadAll();
 
-void EERIEDrawAnimQuat(EERIE_3DOBJ * eobj, ANIM_USE * eanim, Anglef * angle, Vec3f  * pos, unsigned long time, INTERACTIVE_OBJ * io, bool render = true);
+void EERIEDrawAnimQuat(EERIE_3DOBJ * eobj, ANIM_USE * eanim, Anglef * angle, Vec3f  * pos, unsigned long time, INTERACTIVE_OBJ * io, bool render = true, bool update_movement = true);
 
 void DrawEERIEInterMatrix(EERIE_3DOBJ * eobj, EERIEMATRIX * mat, Vec3f  * pos, INTERACTIVE_OBJ * io, EERIE_MOD_INFO * modinfo = NULL);
 

src/animation/AnimationRender.cpp
@@ -219,7 +219,7 @@ static void Cedric_GetTime(float & timm, INTERACTIVE_OBJ * io) {
 
 /* Evaluate main entity translation */
 static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse, float scale,
-                                       Vec3f & ftr) {
+                                       Vec3f & ftr, bool update_movement) {
 	
 	// Resets Frame Translate
 	ftr = Vec3f::ZERO;
@@ -272,7 +272,7 @@ static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse,
 			// Linear interpolation of object translation (MOVE)
 			ftr = sFrame->translate + (eFrame->translate - sFrame->translate) * animuse->pour;
 
-			if(io) {
+			if(io && update_movement) {
 				
 				ftr *= scale;
 
@@ -288,7 +288,7 @@ static void Cedric_AnimCalcTranslation(INTERACTIVE_OBJ * io, ANIM_USE * animuse,
 		}
 	}
 	
-	if(io && io->animlayer[0].cur_anim) {
+	if(io && io->animlayer[0].cur_anim && update_movement) {
 		
 		// Use calculated value to notify the Movement engine of the translation to do
 		if(io->ioflags & IO_NPC) {
@@ -2178,7 +2178,8 @@ void Cedric_AnimateDrawEntity(EERIE_3DOBJ * eobj,
                               Anglef * angle,
                               Vec3f * pos,
                               INTERACTIVE_OBJ * io,
-                              bool render) {
+                              bool render,
+                              bool update_movement) {
 	
 	float 				invisibility;
 	float 				scale;
@@ -2210,7 +2211,7 @@ void Cedric_AnimateDrawEntity(EERIE_3DOBJ * eobj,
 
 	memset(grps, 0, eobj->nbgroups);
 
-	Cedric_AnimCalcTranslation(io, animuse, scale, ftr);
+	Cedric_AnimCalcTranslation(io, animuse, scale, ftr, update_movement);
 
 	if(Cedric_IO_Visible(io)) {
 

src/animation/AnimationRender.h
@@ -54,7 +54,7 @@ struct EERIEMATRIX;
 struct EERIE_QUAT;
 struct TexturedVertex;
 
-void Cedric_AnimateDrawEntity(EERIE_3DOBJ * eobj, ANIM_USE * animuse, Anglef * angle, Vec3f * pos, INTERACTIVE_OBJ * io, bool render);
+void Cedric_AnimateDrawEntity(EERIE_3DOBJ * eobj, ANIM_USE * animuse, Anglef * angle, Vec3f * pos, INTERACTIVE_OBJ * io, bool render, bool update_movement);
 
 void ARX_DrawPrimitive(TexturedVertex *, TexturedVertex *, TexturedVertex *, float _fAdd = 0.0f);
 

src/core/ArxGame.cpp
@@ -1162,7 +1162,7 @@ bool ArxGame::Render() {
 
 				arx_assert(inter.iobj[0]->obj != NULL);
 				EERIEDrawAnimQuat(inter.iobj[0]->obj, &inter.iobj[0]->animlayer[0], &inter.iobj[0]->angle,
-				                  &inter.iobj[0]->pos, checked_range_cast<unsigned long>(iCalc), inter.iobj[0], false);
+				                  &inter.iobj[0]->pos, checked_range_cast<unsigned long>(iCalc), inter.iobj[0], false, false);
 
 					if ((player.Interface & INTER_COMBATMODE) && (inter.iobj[0]->animlayer[1].cur_anim != NULL))
 				ManageCombatModeAnimations();
@@ -1191,7 +1191,7 @@ bool ArxGame::Render() {
 
 			arx_assert(inter.iobj[0]->obj != NULL);
 			EERIEDrawAnimQuat(inter.iobj[0]->obj, &inter.iobj[0]->animlayer[0], &inter.iobj[0]->angle,
-			                  &inter.iobj[0]->pos, checked_range_cast<unsigned long>(val), inter.iobj[0], false);
+			                  &inter.iobj[0]->pos, checked_range_cast<unsigned long>(val), inter.iobj[0], false, false);
 
 
 				if ((player.Interface & INTER_COMBATMODE) && (inter.iobj[0]->animlayer[1].cur_anim != NULL))