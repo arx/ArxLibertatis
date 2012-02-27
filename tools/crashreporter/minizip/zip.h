9b28633bde4bc32c5b67ad1888057f77f4d933c2
Daniel Scharrer <daniel@constexpr.org>
2012-02-27 13:22:53 +0100

    crashreporter: Adjust includes in zip.h.

--- tools/crashreporter/minizip/zip.h
+++ tools/crashreporter/minizip/zip.h
@@ -47,15 +47,15 @@ extern "C" {
 //#define HAVE_BZIP2
 
 #ifndef _ZLIB_H
-#include "zlib.h"
+#include <zlib.h>
 #endif
 
 #ifndef _ZLIBIOAPI_H
-#include "ioapi.h"
+#include "tools/crashreporter/minizip/ioapi.h"
 #endif
 
 #ifdef HAVE_BZIP2
-#include "bzlib.h"
+#include <bzlib.h>
 #endif
 
 #define Z_BZIP2ED 12
