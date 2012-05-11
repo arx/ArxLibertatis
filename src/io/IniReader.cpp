9044d54df3b4a796736dc93ad7ec542079154ee2
Daniel Scharrer <daniel@constexpr.org>
2012-05-11 21:04:11 +0200

    IniReader: Work around erroneous newlines in the spanish localisation file.

--- src/io/IniReader.cpp
+++ src/io/IniReader.cpp
@@ -119,30 +119,39 @@ static const string ALPHANUM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV
 
 bool IniReader::read(istream & is) {
 	
-	// The current section.
+	// The current section
 	IniSection * section = NULL;
 	
 	bool ok = true;
+	
+	bool readline = true;
+	
+	string str;
 	
 	// While lines remain to be extracted
 	for(size_t line = 1; is.good(); line++) {
 		
 		// Get a line to process
-		string str;
-		getline(is, str);
+		if(readline) {
+			str.clear();
+			getline(is, str);
+		} else {
+			readline = true;
+		}
 		
 		size_t start = str.find_first_not_of(WHITESPACE);
 		if(start == string::npos) {
-			// Empty line (only whitespace).
+			// Empty line (only whitespace)
 			continue;
 		}
 		
-		if(str[start] == '#' || (start + 1 < str.length() && str[start] == '/' && str[start+1] == '/')) {
-			// Whole line was commented, no need to do anything with it. Continue getting the next line.
+		if(str[start] == '#'
+		   || (start + 1 < str.length() && str[start] == '/' && str[start+1] == '/')) {
+			// Whole line was commented, no need to do anything with it. Continue getting the next line
 			continue;
 		}
 		
-		// Section header.
+		// Section header
 		if(str[start] == '[') {
 			
 			size_t end = str.find(']', start + 1);
@@ -195,23 +204,87 @@ bool IniReader::read(istream & is) {
 			continue;
 		}
 		
+		std::string key = str.substr(start, nameEnd - start);
+		std::string value;
+		
 		size_t valueEnd;
 		if(str[valueStart] == '"') {
 			valueStart++;
 			valueEnd = str.find_last_of('"');
 			arx_assert(valueEnd != string::npos);
+			
 			if(valueEnd < valueStart) {
-				// The localisation files are broken (missing ending quote), so ignore this error.
+				
+				// The localisation files are broken (missing ending quote)
+				// But the spanish localisation files hae erroneous newlines in some values
 				LogDebug("invalid quoted value @ line " << line << ": " << str);
-				valueEnd = str.length();
+				
+				valueEnd = str.find_last_not_of(WHITESPACE) + 1;
+				arx_assert(valueEnd >= valueStart);
+				value = str.substr(valueStart, valueEnd - valueStart);
+				
+				// Add following lines until we find either a terminating quote,
+				// an empty or commented line, a new section or a new key
+				for(; is.good(); line++) {
+					
+					str.clear();
+					getline(is, str);
+					
+					size_t start = str.find_first_not_of(WHITESPACE);
+					if(start == string::npos) {
+						// Empty line (only whitespace)
+						break;
+					}
+					
+					if(str[start] == '#'
+					   || (start + 1 < str.length() && str[start] == '/' && str[start+1] == '/')) {
+						// Whole line was commented
+						break;
+					}
+					
+					if(str[start] == '[') {
+						// New section
+						line--, readline = false;
+						break;
+					}
+					
+					size_t nameEnd = str.find_first_not_of(ALPHANUM, start);
+					if(nameEnd != string::npos && nameEnd != start) {
+						size_t separator = str.find_first_not_of(WHITESPACE, nameEnd);
+						if(separator != string::npos && str[separator] == '=') {
+							// New key
+							line--, readline = false;
+							break;
+						}
+					}
+					
+					// Replace newlines with spaces!
+					value += ' ';
+					
+					size_t valueEnd = str.find_last_of('"');
+					if(valueEnd != string::npos) {
+						// End of multi-line value
+						value += str.substr(start, valueEnd - start);
+						break;
+					}
+					
+					valueEnd = str.find_last_not_of(WHITESPACE) + 1;
+					arx_assert(valueEnd > start);
+					value += str.substr(start, valueEnd - start);
+				}
+				
+			} else {
+				value = str.substr(valueStart, valueEnd - valueStart);
 			}
+			
 		} else {
 			valueEnd = str.find_last_not_of(WHITESPACE) + 1;
 			arx_assert(valueEnd != string::npos);
+			arx_assert(valueEnd >= valueStart);
+			value = str.substr(valueStart, valueEnd - valueStart);
 		}
-		arx_assert(valueEnd >= valueStart);
 		
-		section->addKey(str.substr(start, nameEnd - start), str.substr(valueStart, valueEnd - valueStart));
+		section->addKey(key, value);
 		
 		// Ignoring rest of the line, not verifying that it's only whitespace / comment
 		
