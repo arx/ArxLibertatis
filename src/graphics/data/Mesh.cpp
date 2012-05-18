arx/ArxLibertatis

commit 4725049c44b1a7c3b012c9f34bae3eccc6c25e38

Change variable from D3DCOLOR to Vec3f in Mesh.cpp

src/graphics/data/Mesh.cpp
@@ -1076,12 +1076,11 @@ float GetColorz(float x, float y, float z) {
 
 		ApplyDynLight(ep);
 
-		for (long i = 0; i < to; i++)
-		{
-			D3DCOLOR col = ep->tv[i].color;
-			_ffr += (float)(long)((col >> 16) & 255);
-			_ffg += (float)(long)((col >> 8) & 255);
-			_ffb += (float)(long)((col) & 255);
+		for(long i = 0; i < to; i++) {
+			Color3f col = Color3f::fromBGR(ep->tv[i].color);
+			_ffr += col.r;
+			_ffg += col.g;
+			_ffb += col.b;
 		}
 
 		_ffr *= div;

commit 0b605eecf28ea3c6830736945b854be6161ace36

Use correct scale for the player brightness value to fix stealth calculations.
This was broken in commit 4725049c44b1a7c3b012c9f34bae3eccc6c25e38
Fixes bug #262

Thanks Cypher

src/graphics/data/Mesh.cpp
@@ -1092,10 +1092,10 @@ float GetColorz(float x, float y, float z) {
 		ApplyDynLight(ep);
 
 		for(long i = 0; i < to; i++) {
-			Color3f col = Color3f::fromBGR(ep->tv[i].color);
-			_ffr += col.r;
-			_ffg += col.g;
-			_ffb += col.b;
+			Color col = Color::fromBGR(ep->tv[i].color);
+			_ffr += float(col.r);
+			_ffg += float(col.g);
+			_ffb += float(col.b);
 		}
 
 		_ffr *= div;