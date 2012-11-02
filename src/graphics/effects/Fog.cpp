175ca005010bdf65b06a809d50ae3ab76acf42ac
Daniel Scharrer <daniel@constexpr.org>
2012-11-02 11:14:12 +0100

    Use createParticle() in Fog.cpp

--- src/graphics/effects/Fog.cpp
+++ src/graphics/effects/Fog.cpp
@@ -161,110 +161,90 @@ long ARX_FOGS_Count()
 void ARX_FOGS_TimeReset()
 {
 }
-void AddPoisonFog(Vec3f * pos, float power)
-{
+
+void AddPoisonFog(Vec3f * pos, float power) {
+	
 	int iDiv = 4 - config.video.levelOfDetail;
-
+	
 	float flDiv = static_cast<float>(1 << iDiv);
-	long count = checked_range_cast<long>(FrameDiff / flDiv);
-
-	if (count < 1) count = 1;
-
-	while (count) 
-	{
-		count--;
-		long j = ARX_PARTICLES_GetFree();
-
-		if ((j != -1) && (!arxtime.is_paused()) && (rnd() * 2000.f < power))
-		{
-			ParticleCount++;
-			particle[j].special		=	FADE_IN_AND_OUT | ROTATING | MODULATE_ROTATION | DISSIPATING;
-			particle[j].exist		=	true;
-			particle[j].zdec		=	0;
-			particle[j].ov.x		=	pos->x + 100.f - 200.f * rnd();
-			particle[j].ov.y		=	pos->y + 100.f - 200.f * rnd();
-			particle[j].ov.z		=	pos->z + 100.f - 200.f * rnd();
-			float speed				=	1.f;
-			float fval				=	speed * ( 1.0f / 5 );
-			particle[j].scale.x		=	particle[j].scale.y		=	particle[j].scale.z		=	10.f;
-			particle[j].move.x		=	(speed - rnd()) * fval;
-			particle[j].move.y		=	(speed - speed * rnd()) * ( 1.0f / 15 );
-			particle[j].move.z		=	(speed - rnd()) * fval;
-			particle[j].scale.x		=	particle[j].scale.y		=	8;
-			particle[j].timcreation	=	static_cast<long>(arxtime.get_updated());
-			particle[j].tolive		=	Random::get(4500, 9000);
-			particle[j].tc			=	TC_smoke;
-			particle[j].siz			=	(80 + rnd() * 80 * 2.f) * ( 1.0f / 3 );
-			particle[j].rgb = Color3f(rnd() * (1.f/3), 1.f, rnd() * (1.f/10));
-			particle[j].fparam		=	0.001f;
+	
+	arxtime.update();
+	
+	long count = std::max(1l, checked_range_cast<long>(FrameDiff / flDiv));
+	while(count--) {
+		
+		if(rnd() * 2000.f >= power) {
+			continue;
 		}
+		
+		PARTICLE_DEF * pd = createParticle();
+		if(!pd) {
+			return;
+		}
+		
+		float speed = 1.f;
+		float fval = speed * 0.2f;
+		pd->special = FADE_IN_AND_OUT | ROTATING | MODULATE_ROTATION | DISSIPATING;
+		pd->zdec = 0;
+		pd->ov = *pos + randomVec(-100.f, 100.f);
+		pd->scale = Vec3f(8.f, 8.f, 10.f);
+		pd->move = Vec3f((speed - rnd()) * fval, (speed - speed * rnd()) * (1.f / 15),
+		                 (speed - rnd()) * fval);
+		pd->tolive = Random::get(4500, 9000);
+		pd->tc = TC_smoke;
+		pd->siz = (80.f + rnd() * 160.f) * (1.f / 3);
+		pd->rgb = Color3f(rnd() * (1.f / 3), 1.f, rnd() * 0.1f);
+		pd->fparam = 0.001f;
 	}
 }
-//*************************************************************************************
-//*************************************************************************************
+
 void ARX_FOGS_Render() {
 	
-	if (arxtime.is_paused()) return;
-
+	if(arxtime.is_paused()) {
+		return;
+	}
+	
 	int iDiv = 4 - config.video.levelOfDetail;
-
+	
 	float flDiv = static_cast<float>(1 << iDiv);
-
-	for (long i = 0; i < MAX_FOG; i++)
-	{
-		if (fogs[i].exist)
-		{
-			float fval;
-
-			long count = checked_range_cast<long>(FrameDiff / flDiv);
-
-			if (count < 1) count = 1;
-
-			while (count)
-			{
-				count--;
-				long j = ARX_PARTICLES_GetFree();
-
-				if ((j != -1) && (rnd() * 2000.f < (float)fogs[i].frequency))
-				{
-					ParticleCount++;
-					particle[j].special = FADE_IN_AND_OUT | ROTATING | MODULATE_ROTATION | DISSIPATING;
-					particle[j].exist = true;
-					particle[j].zdec = 0;
-
-					if (fogs[i].special & FOG_DIRECTIONAL)
-					{
-						particle[j].ov.x = fogs[i].pos.x;
-						particle[j].ov.y = fogs[i].pos.y;
-						particle[j].ov.z = fogs[i].pos.z;
-						fval = fogs[i].speed * ( 1.0f / 10 );
-						particle[j].move.x = (fogs[i].move.x * fval);
-						particle[j].move.y = (fogs[i].move.y * fval);
-						particle[j].move.z = (fogs[i].move.z * fval);
-					}
-					else
-					{
-						particle[j].ov.x = fogs[i].pos.x + 100.f - 200.f * rnd();
-						particle[j].ov.y = fogs[i].pos.y + 100.f - 200.f * rnd();
-						particle[j].ov.z = fogs[i].pos.z + 100.f - 200.f * rnd();
-						fval = fogs[i].speed * ( 1.0f / 5 );
-						particle[j].move.x = (fogs[i].speed - rnd() * 2.f) * fval;
-						particle[j].move.y = (fogs[i].speed - rnd() * 2.f) * ( 1.0f / 15 );
-						particle[j].move.z = (fogs[i].speed - rnd() * 2.f) * fval;
-					}
-
-					particle[j].scale.x		=	particle[j].scale.y		=	particle[j].scale.z		=	fogs[i].scale;
-					particle[j].timcreation	=	(long)arxtime;
-					particle[j].tolive		=	fogs[i].tolive + Random::get(0, fogs[i].tolive);
-					particle[j].tc			=	TC_smoke;
-					particle[j].siz			=	(fogs[i].size + rnd() * fogs[i].size * 2.f) * ( 1.0f / 3 );
-					particle[j].rgb = fogs[i].rgb;
-					particle[j].fparam		=	fogs[i].rotatespeed;
-				}
-
-				fogs[i].lastupdate = (unsigned long)(arxtime); 
-			}
+	
+	for(long i = 0; i < MAX_FOG; i++) {
+		
+		if(!fogs[i].exist) {
+			continue;
 		}
+		
+		long count = std::max(1l, checked_range_cast<long>(FrameDiff / flDiv));
+		while(count--) {
+			
+			if(rnd() * 2000.f >= fogs[i].frequency) {
+				continue;
+			}
+			
+			PARTICLE_DEF * pd = createParticle(true);
+			if(!pd) {
+				break;
+			}
+			
+			pd->special = FADE_IN_AND_OUT | ROTATING | MODULATE_ROTATION | DISSIPATING;
+			pd->zdec = 0;
+			if(fogs[i].special & FOG_DIRECTIONAL) {
+				pd->ov = fogs[i].pos;
+				pd->move = fogs[i].move * (fogs[i].speed * 0.1f);
+			} else {
+				pd->ov = fogs[i].pos + randomVec(-100.f, 100.f);
+				pd->move = Vec3f::repeat(fogs[i].speed) - randomVec(0.f, 2.f);
+				pd->move *= Vec3f(fogs[i].speed * 0.2f,  1.f / 15, fogs[i].speed * 0.2f);
+			}
+			pd->scale = Vec3f::repeat(fogs[i].scale);
+			pd->tolive = fogs[i].tolive + Random::get(0, fogs[i].tolive);
+			pd->tc = TC_smoke;
+			pd->siz = (fogs[i].size + rnd() * fogs[i].size * 2.f) * (1.0f / 3);
+			pd->rgb = fogs[i].rgb;
+			pd->fparam = fogs[i].rotatespeed;
+		}
+		
+		fogs[i].lastupdate = (unsigned long)(arxtime);
 	}
 }
 
