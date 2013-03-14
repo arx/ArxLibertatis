20cefa56a38cd5bf20c0df2aab3ec2e02b2de07b
Daniel Scharrer <daniel@constexpr.org>
2013-03-14 10:43:17 +0100

    ColorLogger: Color the message for warnings/errors

--- src/io/log/ColorLogger.cpp
+++ src/io/log/ColorLogger.cpp
@@ -37,14 +37,18 @@ void ColorConsole::log(const Source & file, int line, Logger::LogLevel level,
 	
 	std::ostream * os;
 	
+	const char * c = "\e[m";
+	const char * e = "";
 	size_t length = 3;
 	switch(level) {
 		case Logger::Debug:    std::cout << "\e[1;36m[D]\e[0;36m", os = &std::cout; break;
 		case Logger::Info:     std::cout << "\e[1;32m[I]\e[0;32m", os = &std::cout; break;
-		case Logger::Warning:  std::cerr << "\e[1;33m[W]\e[0;33m", os = &std::cerr; break;
-		case Logger::Error:    std::cerr << "\e[1;31m[E]\e[0;31m", os = &std::cerr; break;
+		case Logger::Warning:  std::cerr << "\e[1;33m[W]\e[0;33m";
+			                     os = &std::cerr, e = c, c = "\e[0;33m"; break;
+		case Logger::Error:    std::cerr << "\e[1;31m[E]\e[0;31m";
+		                       os = &std::cerr, e = c, c = "\e[0;31m"; break;
 		case Logger::Critical: std::cerr << "\e[1;31m[CRITICAL]\e[0;31m";
-			                     os = &std::cerr, length = 10; break;
+			                     os = &std::cerr, e = c, c = "\e[1;31m", length = 10; break;
 		case Logger::None: ARX_DEAD_CODE(); return;
 	}
 	(*os) << ' ' << file.name << "\e[m:\e[0;33m";
@@ -54,7 +58,7 @@ void ColorConsole::log(const Source & file, int line, Logger::LogLevel level,
 		(*os) << std::left << std::setfill(' ') << std::setw(alignment - length);
 	}
 	
-	(*os) << line << "\e[m " << str << std::endl;
+	(*os) << line << " " << c << str << e << std::endl;
 }
 
 void ColorConsole::flush() {
