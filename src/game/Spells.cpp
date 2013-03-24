cd4d6d60943c3b7fbda30f7f8f6c663570572c03
Dimoks <dimoks11@gmail.com>
2013-03-24 22:56:00 +0400

    Revert "Some fixes of spells"
    This reverts commit b0874d54bebfc117f0fa030a14b661c23903678b.

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -2974,7 +2974,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 	if ( cur_rf == 3 )
 	{ 
 		flags |= SPELLCAST_FLAG_NOCHECKCANCAST | SPELLCAST_FLAG_NOMANA;	
-		//level += 2; // excluded by Dimoks
+		level += 2;
 	}
 
 	if ( sp_max ) 
@@ -3037,14 +3037,9 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 		return true;
 	}
 
-	/*if ( flags & SPELLCAST_FLAG_NOMANA )
-	{ // function was excluded by Dimoks
+	if ( flags & SPELLCAST_FLAG_NOMANA )
+	{
 		Player_Magic_Level = static_cast<float>(level);
-	}*/
-
-	if ( cur_rf == 3 ) 
-	{ // function was added by Dimoks
-		level += 2;
 	}
 
 	static TextureContainer * tc4 = TextureContainer::Load("graph/particles/smoke");
@@ -3561,10 +3556,8 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 				spells[iCancel].tolive = 0;
 			}
 			
-			if (spells[i].caster==0) { // fixed by Dimoks
-				spells[i].target=spells[i].caster; // © Dimoks
-				if (!(spells[i].flags & SPELLCAST_FLAG_NOSOUND)) // fixed by Dimoks
-					ARX_SOUND_PlayInterface(SND_SPELL_DETECT_TRAP);
+			if(spells[i].caster == 0 && !(spells[i].flags & SPELLCAST_FLAG_NOSOUND)) {
+				ARX_SOUND_PlayInterface(SND_SPELL_DETECT_TRAP);
 			}
 			
 			spells[i].snd_loop = SND_SPELL_DETECT_TRAP_LOOP;
@@ -3578,8 +3571,6 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			spells[i].tolive = 60000;
 			spells[i].fManaCostPerSecond = 0.4f;
 			spells[i].bDuration = true;
-			
-			ARX_SPELLS_AddSpellOn(spells[i].target, i); // © Dimoks
 			
 			break;
 		}
@@ -3671,8 +3662,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			}
 			
 			if(!(spells[i].flags & SPELLCAST_FLAG_NOSOUND)) {
-				//ARX_SOUND_PlaySFX(SND_SPELL_ARMOR_START, &spells[i].caster_pos); // excluded by Dimoks
-				ARX_SOUND_PlaySFX(SND_SPELL_LOWER_ARMOR, &spells[i].caster_pos); // © Dimoks
+				ARX_SOUND_PlaySFX(SND_SPELL_ARMOR_START, &spells[i].caster_pos);
 			}
 			
 			spells[i].exist = true;
@@ -3825,7 +3815,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			
 			spells[i].exist = true;
 			spells[i].lastupdate = spells[i].timcreation = (unsigned long)(arxtime);
-			spells[i].tolive = 20000; // TODO probably never read
+			spells[i].tolive = 20000; // TODO probbaly never read
 			
 			CFireBall * effect = new CFireBall();
 			effect->spellinstance = i;
@@ -4064,10 +4054,6 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			spells[i].lastupdate = spells[i].timcreation = (unsigned long)(arxtime);
 			spells[i].tolive = 2000000; // TODO should respect user-supplied duration
 			
-			if (duration>-1) { // function added by Dimoks (TODO has been done...) 
-				spells[i].tolive=duration; 
-			}
-			
 			if(spells[i].caster == 0) {
 				spells[i].target = 0;
 			}
@@ -4082,10 +4068,6 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			spells[i].tolive = effect->GetDuration();
 			
 			ARX_SPELLS_AddSpellOn(spells[i].target, i);
-			
-			spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_SPELL_FIRE_PROTECTION_LOOP, 
-			                                       &spells[i].caster_pos, 1.f, 
-			                                       ARX_SOUND_PLAY_LOOPED); // © Dimoks
 			
 			break;
 		}
@@ -4776,10 +4758,6 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			spells[i].pSpellFx = effect;
 			spells[i].tolive = effect->GetDuration();
 			
-			spells[i].snd_loop = ARX_SOUND_PlaySFX( SND_SPELL_ICE_FIELD_LOOP, 
-			                                       &target, 1.f, 
-			                                       ARX_SOUND_PLAY_LOOPED ); // © Dimoks
-			
 			break;
 		}
 		
@@ -5185,13 +5163,9 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			}
 			
 			ARX_SOUND_PlaySFX(SND_SPELL_INCINERATE);
-			/*spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_FIREPLACE,
+			spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_FIREPLACE,
 			                                       &spells[i].caster_pos, 1.f,
-			                                       ARX_SOUND_PLAY_LOOPED);*/ // excluded by Dimoks
-			
-			spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_SPELL_INCINERATE_LOOP, 
-			                                       &spells[i].caster_pos, 1.f, 
-			                                       ARX_SOUND_PLAY_LOOPED); // © Dimoks
+			                                       ARX_SOUND_PLAY_LOOPED);
 			
 			spells[i].exist = true;
 			spells[i].lastupdate = spells[i].timcreation = (unsigned long)(arxtime);
@@ -5410,12 +5384,9 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			}
 			
 			if(nb_targets) {
-				/*spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_FIREPLACE,
+				spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_FIREPLACE,
 				                                       &spells[i].caster_pos, 1.f,
-				                                       ARX_SOUND_PLAY_LOOPED);*/ // excluded by Dimoks
-				spells[i].snd_loop = ARX_SOUND_PlaySFX(SND_SPELL_INCINERATE_LOOP, 
-				                                       &spells[i].caster_pos, 1.f, 
-				                                       ARX_SOUND_PLAY_LOOPED); // © Dimoks
+				                                       ARX_SOUND_PLAY_LOOPED);
 			} else {
 				spells[i].snd_loop = -1;
 			}
@@ -5576,12 +5547,6 @@ void ARX_SPELLS_Kill(long i) {
 			ARX_SOUND_Stop(spells[i].snd_loop);
 			ARX_SOUND_PlaySFX(SND_SPELL_FIRE_FIELD_END);
 			break;
-		}
-		
-		case SPELL_ICE_FIELD: { // function was added by Dimoks
-			ARX_SOUND_Stop(spells[i].snd_loop); 
-			ARX_SOUND_PlaySFX(SND_SPELL_ICE_FIELD_END); 
-			break; 
 		}
 		
 		case SPELL_MASS_PARALYSE: {
@@ -5776,11 +5741,9 @@ void ARX_SPELLS_Update()
 
 					if (spells[i].caster==0)
 					{
-						//Project.improve=0; // excluded by Dimoks
+						Project.improve=0;
 						ARX_SOUND_Stop(spells[i].snd_loop);
-					}
-					
-					ARX_SPELLS_RemoveSpellOn(spells[i].target,i); // © Dimoks
+					}					
 
 				break;					
 				//----------------------------------------------------------------------------
@@ -5829,8 +5792,7 @@ void ARX_SPELLS_Update()
 
 					if (spells[i].caster == 0) ARX_SOUND_Stop(spells[i].snd_loop);
 
-					//ARX_SOUND_PlaySFX(SND_SPELL_INVISIBILITY_END, &spells[i].caster_pos); // excluded by Dimoks
-					ARX_SOUND_PlaySFX(SND_SPELL_SPEED_END, &spells[i].caster_pos); // © Dimoks
+					ARX_SOUND_PlaySFX(SND_SPELL_INVISIBILITY_END, &spells[i].caster_pos);
 				break;
 				//----------------------------------------------------------------------------------
 				case SPELL_FIREBALL:
@@ -5855,8 +5817,6 @@ void ARX_SPELLS_Update()
 					ARX_SOUND_PlaySFX(SND_SPELL_TELEKINESIS_END, &spells[i].caster_pos);					
 				break;
 				case SPELL_FIRE_PROTECTION:
-					ARX_SOUND_Stop(spells[i].snd_loop); // © Dimoks
-					ARX_SOUND_PlaySFX(SND_SPELL_FIRE_PROTECTION_END); // © Dimoks
 					ARX_SPELLS_RemoveSpellOn(spells[i].target,i);;
 
 					if (ValidIONum(spells[i].target))
@@ -6045,7 +6005,6 @@ void ARX_SPELLS_Update()
 				case SPELL_INCINERATE:
 					ARX_SPELLS_RemoveSpellOn(spells[i].target,i);					
 					ARX_SOUND_Stop(spells[i].snd_loop);
-					ARX_SOUND_PlaySFX(SND_SPELL_INCINERATE_END, &spells[i].caster_pos); // © Dimoks
 				break;
 				//----------------------------------------------------------------------------------
 				//**********************************************************************************
@@ -6059,7 +6018,6 @@ void ARX_SPELLS_Update()
 				case SPELL_MASS_INCINERATE:
 					ARX_SPELLS_RemoveMultiSpellOn(i);
 					ARX_SOUND_Stop(spells[i].snd_loop);
-					ARX_SOUND_PlaySFX(SND_SPELL_INCINERATE_END, &spells[i].caster_pos); // © Dimoks
 				break;
 				default: break;
 				//----------------------------------------------------------------------------------
@@ -6466,8 +6424,7 @@ void ARX_SPELLS_Update()
 							LaunchFireballBoom(&pCRG->eSrc,(float)spells[i].caster_level);
 							DoSphericDamage(&pCRG->eSrc,4.f*spells[i].caster_level,30.f*spells[i].caster_level,DAMAGE_AREA,DAMAGE_TYPE_FIRE | DAMAGE_TYPE_MAGICAL,spells[i].caster);
 							spells[i].tolive=0;
-							//ARX_SOUND_PlaySFX(SND_SPELL_FIRE_HIT, &sphere.origin); // excluded by Dimoks
-							ARX_SOUND_PlaySFX(SND_SPELL_RUNE_OF_GUARDING_END, &sphere.origin); // © Dimoks
+							ARX_SOUND_PlaySFX(SND_SPELL_FIRE_HIT, &sphere.origin);
 						}
 					}
 				}
