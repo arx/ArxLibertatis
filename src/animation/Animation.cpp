arx/ArxLibertatis

commit c7d3ce915a6143a40b07efd33b6faeac9b0500ff

Use quaternion for rotation

src/animation/Animation.cpp
@@ -932,12 +932,17 @@ void DrawEERIEInter(EERIE_3DOBJ *eobj, Anglef *angle, Vec3f *poss, Entity *io, E
 	if(!io && INVISIBILITY_OVERRIDE != 0.f)
 		invisibility = INVISIBILITY_OVERRIDE;
 	
+	EERIE_QUAT rotation;
+	Quat_Init(&rotation);
+
 	// Precalc rotations
 	if(angle) {
-		Anglef tempAngle = *angle;
-		arx_assert(!modinfo);
+		Anglef tempAngle = Anglef(radians(angle->a), radians(angle->b), radians(angle->g));
+		QuatFromAngles(&rotation, &tempAngle);
+	}
 
-		Ncam.orgTrans.updateFromAngle(tempAngle);
+	if(mat) {
+		QuatFromMatrix(rotation, *mat);
 	}
 	
 	// Test for Mipmeshing then pre-computes vertices
@@ -957,13 +962,9 @@ void DrawEERIEInter(EERIE_3DOBJ *eobj, Anglef *angle, Vec3f *poss, Entity *io, E
 			if(io && !modinfo) {
 				vert_list_static[0].p -= io->obj->pbox->vert[0].initpos * scale - io->obj->point0;
 			}
-
-			VectorMatrixMultiply(&vert_list_static[1].p, &vert_list_static[0].p, mat);
-		} else {
-			YRotatePoint(&vert_list_static[0].p, &vert_list_static[1].p, Ncam.orgTrans.ycos, Ncam.orgTrans.ysin);
-			XRotatePoint(&vert_list_static[1].p, &vert_list_static[0].p, Ncam.orgTrans.xcos, Ncam.orgTrans.xsin);
-			ZRotatePoint(&vert_list_static[0].p, &vert_list_static[1].p, Ncam.orgTrans.zcos, Ncam.orgTrans.zsin);
 		}
+
+		TransformVertexQuat(&rotation, &vert_list_static[0].p, &vert_list_static[1].p);
 
 		eobj->vertexlist3[i].v = vert_list_static[1].p += pos;
 

commit 507fd93c59b41e2a534dcf076cfcb150f6d45033

Fix wrong item render rotations introduced in c7d3ce91

src/animation/Animation.cpp
@@ -883,6 +883,34 @@ void CalculateInterZMapp(EERIE_3DOBJ * _pobj3dObj, long lIdList, long * _piInd,
 	}
 }
 
+
+void worldAngleToQuat(EERIE_QUAT *dest, Anglef *src, bool isNpc = false) {
+
+	//TODO copy-paste
+	if(!isNpc) {
+		// To correct invalid angle in Animated FIX/ITEMS
+		Anglef ang = *src;
+		ang.a = (360 - ang.a);
+		ang.b = (ang.b);
+		ang.g = (ang.g);
+		EERIEMATRIX mat;
+		Vec3f vect(0, 0, 1);
+		Vec3f up(0, 1, 0);
+		VRotateY(&vect, ang.b);
+		VRotateX(&vect, ang.a);
+		VRotateZ(&vect, ang.g);
+		VRotateY(&up, ang.b);
+		VRotateX(&up, ang.a);
+		VRotateZ(&up, ang.g);
+		MatrixSetByVectors(&mat, &vect, &up);
+		QuatFromMatrix(*dest, mat);
+	} else {
+		Anglef vt1 = Anglef(radians(src->a), radians(src->b), radians(src->g));
+		QuatFromAngles(dest, &vt1);
+	}
+}
+
+
 void EE_RT(TexturedVertex * in, Vec3f * out);
 void EE_P(Vec3f * in, TexturedVertex * out);
 
@@ -937,8 +965,7 @@ void DrawEERIEInter(EERIE_3DOBJ *eobj, Anglef *angle, Vec3f *poss, Entity *io, E
 
 	// Precalc rotations
 	if(angle) {
-		Anglef tempAngle = Anglef(radians(angle->a), radians(angle->b), radians(angle->g));
-		QuatFromAngles(&rotation, &tempAngle);
+		worldAngleToQuat(&rotation, angle);
 	}
 
 	if(mat) {