8bfa91b416c6f6bf5dc5aebb29487c55e604ed56
Dimoks <dimoks11@gmail.com>
2013-05-31 02:29:29 +0400

    Changes of aura controls.
    Makes the aura control method for the "Armor" spell analogous to the one
    used for the "Protection from fire" spell.
    
    Turns on the aura for the "Lower armor" spell if the spell is active but
    the aura is disabled. If a protection spell and "Lower armor" are active
    at the same time on the same target, the lower armor aura is hidden.
    Previously, the lower armor aura did not appear when protection ended
    before the end of "Lower armor"; this is now fixed.

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -5784,11 +5784,8 @@ void ARX_SPELLS_Update()
 					ARX_SOUND_PlaySFX(SND_SPELL_ARMOR_END, &entities[spells[i].target]->pos);					
 					Entity * io=entities[spells[i].target];
 
-					if (spells[i].longinfo)
-					{
-						io->halo.flags&=~HALO_ACTIVE;
-						ARX_HALO_SetToNative(io);
-					}
+					if (ValidIONum(spells[i].target))
+						ARX_HALO_SetToNative(entities[spells[i].target]);
 
 					ARX_SPELLS_RemoveSpellOn(spells[i].target,i);
 				}
