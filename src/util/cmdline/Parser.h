abd97e7dc2b0309677cab5be8ae83c0a650175aa
Daniel Scharrer <daniel@constexpr.org>
2013-07-06 16:34:50 +0200

    Add documentation to util::cmdline::parse

--- src/util/cmdline/Parser.h
+++ src/util/cmdline/Parser.h
@@ -37,6 +37,56 @@
 
 namespace util { namespace cmdline {
 
+/*!
+ * Parse the command-line arguments in argv into the options specified by the
+ * given interpreter
+ *
+ * The command-line syntax consists of three different option types:
+ *
+ * <b>Long options:</b> \code--option\endcode
+ *
+ *   This is the default form of options and consists of two dashes followed by
+ *   one or more characters. Any character except for the \c = sign is supported,
+ *   but it is suggested to only use alpha-numeric letters and dashes.
+ *
+ *   Long options can have required and optional arguments. For options with \c n
+ *   required the following \c n command-line tokens will will always be interpreted
+ *   as arguments to the option, even if they start with a dash.
+ *
+ *   Optional arguments will only be considered until the next token starting with
+ *   a dash. To force a token to be treated as an (optional) argument the
+ *   \code--option=argument\endcode syntax is supported.
+ *
+ * <b>Short options:</b> \code-o\endcode
+ *
+ *   Short options are a convenience alias of the long options. Sort options
+ *   can be any single character except for the dash. Mandatory and optional arguments
+ *   following the option in separate tokens are handled in exactly the same way.
+ *
+ *   Short options support the \code-oargument\endcode syntax to force a token to be
+ *   treated as an argument.
+ *
+ *   Multiple short options can be combined into a single token with the restriction
+ *   that only the last option takes any arguments. If options \code-a\endcode and
+ *   \code-b\endcode don't take any arguments, but \code-c\endcode does, then
+ *   \code-abcargument\endcode is equivalent to \code-a -b -cargument\endcode.
+ *
+ * <b>Positional arguments:</b>
+ *
+ *   Positional arguments are everything else: tokens that don't start with a dash
+ *   \b and are not consumed as arguments to any options. They are passed to the \c --
+ *   option in @param cli.
+ *
+ *   The special \code--\endcode token can be used to force all following command-line
+ *   tokens to be treated as positional arguments, even if they start with a dash.
+ *
+ * @param cli  An @ref interpreter<std::string> specifying which options are available
+ * @param argc Number of entries in argv
+ * @param argv Array of command-line tokens - first one will be ignored
+ *
+ * @throws error If there was a syntax error or unknown options in the command-line.
+ *               @ref error::what will contain a user-friendly error message.
+ */
 void parse(interpreter<std::string> & cli, int argc, char ** argv);
 
 } } // namespace util::cmdline
