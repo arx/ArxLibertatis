eb0fde776ede200d031f506240eda1771f552e8f
Eli2 <FabianRoth64@googlemail.com>
2013-08-16 15:38:03 +0200

    Remove unnecessary goto

--- src/animation/Animation.cpp
+++ src/animation/Animation.cpp
@@ -379,7 +379,6 @@ void PrepareAnim(ANIM_USE *eanim, unsigned long time, Entity *io) {
 				eanim->ctime = lost;
 				eanim->flags=eanim->nextflags;
 				eanim->flags&=~EA_ANIMEND;
-				goto suite;
 			}
 		}
 	} else if (eanim->ctime > eanim->cur_anim->anims[eanim->altidx_cur]->anim_time) {
