d02cbeeac8fe63069f0edc3e2524acf715405ceb
Dimoks <dimoks11@gmail.com>
2013-08-19 16:52:28 +0400

    Reposition sound of "Confuse" spell.
    The starting sound is bound to the spell target current position.

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -4540,7 +4540,7 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			break;
 		}
 		case SPELL_CONFUSE: {
-			ARX_SOUND_PlaySFX(SND_SPELL_CONFUSE);
+			ARX_SOUND_PlaySFX(SND_SPELL_CONFUSE, &entities[spells[i].target]->pos);
 			
 			spells[i].exist = true;
 			spells[i].lastupdate = spells[i].timcreation = (unsigned long)(arxtime);
