5a18c674428cfd47a1b328920f04a105c311b591
Eli2 <FabianRoth64@googlemail.com>
2013-08-22 05:34:47 +0200

    Add Color test

---
+++ tests/graphics/ColorTest.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011-2012 Arx Libertatis Team (see the AUTHORS file)
+ *
+ * This file is part of Arx Libertatis.
+ *
+ * Arx Libertatis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Arx Libertatis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Arx Libertatis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARX_GRAPHICS_COLORTEST_H
+#define ARX_GRAPHICS_COLORTEST_H
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "graphics/Color.h"
+
+class ColorTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(ColorTest);
+	CPPUNIT_TEST(Color3fToColorBGRA);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void Color3fToColorBGRA();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ColorTest);
+
+#endif
--- tests/testMain.cpp
+++ tests/testMain.cpp
@@ -2,6 +2,7 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include "graphics/ColorTest.h"
 #include "graphics/GraphicsUtilityTest.h"
 
 int main(int argc, char *argv[]) {
