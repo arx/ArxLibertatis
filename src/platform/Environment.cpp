f9df682ff6e839eb0a730897e09d1965a9caa4e4
Daniel Scharrer <daniel@constexpr.org>
2013-10-28 20:15:08 +0100

    Add an OS abstraction for running helper programs

--- src/platform/Environment.cpp
+++ src/platform/Environment.cpp
@@ -22,8 +22,10 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
+#include <cstring>
 
 #include <stdlib.h> // needed for setenv, realpath and more
+#include <stdarg.h>
 
 #include <boost/scoped_array.hpp>
 
@@ -38,8 +40,21 @@
 #include <wordexp.h>
 #endif
 
-#ifdef ARX_HAVE_READLINK
+#if defined(ARX_HAVE_OPEN) && defined(ARX_HAVE_DUP2)
+#include <fcntl.h>
+#endif
+
+#if defined(ARX_HAVE_READLINK) || defined(ARX_HAVE_EXECVP)
 #include <unistd.h>
+#endif
+
+#if defined(ARX_HAVE_POSIX_SPAWNP)
+#include <spawn.h>
+extern char ** environ;
+#endif
+
+#if defined(ARX_HAVE_WAITPID)
+#include <sys/wait.h>
 #endif
 
 #if ARX_PLATFORM == ARX_PLATFORM_MACOSX
@@ -50,6 +65,7 @@
 #ifdef ARX_HAVE_SYSCTL
 #include <sys/types.h>
 #include <sys/sysctl.h>
+#include <fcntl.h>
 #endif
 
 #include "io/fs/PathConstants.h"
@@ -371,3 +387,168 @@ fs::path getHelperExecutable(const std::string & name) {
 	
 	return fs::path(name);
 }
+
+namespace platform {
+
+static int run(const fs::path & exe, bool wait, const char * const args[]) {
+	
+#if ARX_PLATFORM == ARX_PLATFORM_WIN32
+	
+	// Format the command line arguments
+	std::ostringstream cmdline;
+	for(size_t i = 0; args[i] != NULL; i++) {
+		if(i == 0) {
+			continue; // skip the program name
+		} else if(i != 1) {
+			cmdline << ' ';
+		}
+		cmdline << util::escapeString(args[i], "\\\" '$!");
+	}
+	
+	STARTUPINFO si;
+	std::memset(&si, 0, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+	PROCESS_INFORMATION pi;
+	std::memset(&pi, 0, sizeof(PROCESS_INFORMATION));
+	
+	if(!CreateProcess(exe.string().c_str(), cmdline, 0, 0, 0, 0, 0, 0, &si, &pi)) {
+		return -1; // Could not start process
+	}
+	
+	int status = 0;
+	if(wait) {
+		status = WaitForSingleObject(pi.hProcess, INFINITE);
+	}
+	
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	
+	return status;
+	
+#else
+	
+	#if defined(ARX_HAVE_OPEN)
+	static int dev_null = open("/dev/null", O_RDWR);
+	#endif
+	
+	char ** argv = const_cast<char **>(args);
+	
+#if defined(ARX_HAVE_POSIX_SPAWNP) && 0
+	
+	// Fast POSIX implementation: posix_spawnp avoids unnecessary vm copies
+	
+	// Redirect standard input, output and error to /dev/null
+	static posix_spawn_file_actions_t * file_actionsp = NULL;
+	#if defined(ARX_HAVE_OPEN) && defined(ARX_HAVE_CLOSE)
+	static posix_spawn_file_actions_t file_actions;
+	if(!file_actionsp && dev_null > 0 && !posix_spawn_file_actions_init(&file_actions)) {
+		file_actionsp = &file_actions;
+		(void)posix_spawn_file_actions_adddup2(file_actionsp, dev_null, 0);
+		(void)posix_spawn_file_actions_adddup2(file_actionsp, dev_null, 1);
+		(void)posix_spawn_file_actions_adddup2(file_actionsp, dev_null, 2);
+	}
+	#endif
+	
+	// Detach the child process from the parent
+	static posix_spawnattr_t * attrp = NULL;
+	static posix_spawnattr_t attr;
+	if(!attrp && !posix_spawnattr_init(&attr)) {
+		attrp = &attr;
+		(void)posix_spawnattr_setflags(attrp, POSIX_SPAWN_SETPGROUP);
+		(void)posix_spawnattr_setpgroup(attrp, 0);
+	}
+	
+	// Run the executable in a new process
+	pid_t pid;
+	(void)posix_spawnp(&pid, exe.string().c_str(), file_actionsp, attrp, argv, environ);
+	
+#elif defined(ARX_HAVE_FORK) && defined(ARX_HAVE_EXECVP)
+	
+	// Compatibility POSIX implementation
+	
+	// Start a new process
+	pid_t pid = fork();
+	if(pid == 0) {
+		
+		// Redirect standard input, output and error to /dev/null
+		#if defined(ARX_HAVE_OPEN) && defined(ARX_HAVE_DUP2) && defined(ARX_HAVE_CLOSE)
+		if(dev_null > 0) {
+			(void)dup2(dev_null, 0);
+			(void)dup2(dev_null, 1);
+			(void)dup2(dev_null, 2);
+		}
+		#endif
+		
+		// Detach the child process from the parent
+		#ifdef ARX_HAVE_SETPGRP
+		(void)setpgrp();
+		#endif
+		
+		// Run the executable
+		(void)execvp(exe.string().c_str(), argv);
+		
+		exit(128);
+	}
+	
+#else
+	(void)argv;
+	#warning "Executing helper processes not supported on this system."
+#endif
+	
+	if(pid < 0) {
+		return -1;
+	}
+	
+	#if defined(ARX_HAVE_WAITPID)
+	if(wait) {
+		int status;
+		(void)waitpid(pid, &status, 0);
+		if(WIFEXITED(status) && (WEXITSTATUS(status) >= 0 && WEXITSTATUS(status) < 127)) {
+			return WEXITSTATUS(status);
+		} else {
+			return -1;
+		}
+	}
+	#else
+	(void)wait;
+	# warning "Waiting for processes not supported on this system."
+	#endif
+	
+	return 0;
+	
+#endif
+	
+}
+
+int run(const fs::path & exe, const char * const args[]) {
+	return run(exe, true, args);
+}
+
+void runAsync(const fs::path & exe, const char * const args[]) {
+	(void)run(exe, false, args);
+}
+
+void runHelper(const std::string & name, ...) {
+	
+	fs::path exe = getHelperExecutable(name);
+	
+	// Parse the argument list
+	std::vector<const char *> arglist;
+	arglist.push_back(exe.string().c_str());
+	va_list args;
+	va_start(args, name);
+	while(true) {
+		const char * arg = va_arg(args, const char *);
+		arglist.push_back(arg);
+		if(!arg) {
+			break;
+		}
+	}
+	va_end(args);
+	const char * const * argv = &arglist.front();
+	
+	runAsync(getHelperExecutable(name), argv);
+}
+
+} // namespace platform
