75e9a12f680a13afbf44948773aa69a89fb0ef70
Sebastien Lussier <sebastien.lussier@hotmail.com>
2013-11-05 20:33:59 -0500

    SDL2 is still eating our ALT+F4 under windows... reapplied hack found in SDL1Window.cpp

--- src/window/SDL2Window.cpp
+++ src/window/SDL2Window.cpp
@@ -390,6 +390,20 @@ void SDL2Window::tick() {
 				}
 				break;
 			}
+
+		#if ARX_PLATFORM == ARX_PLATFORM_WIN32
+			// SDL2 is still eating our ALT+F4 under windows...
+			// See bug report here: https://bugzilla.libsdl.org/show_bug.cgi?id=1555
+			case SDL_KEYDOWN: {
+				
+				if(event.key.keysym.sym == SDLK_F4
+					&& (event.key.keysym.mod & KMOD_ALT) != KMOD_NONE) {
+					onDestroy();
+					return;
+				}
+				break;
+			}
+		#endif
 			
 			case SDL_QUIT: {
 				// The user has requested to close the whole program
