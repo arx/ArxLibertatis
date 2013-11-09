24d0129e5680e3c20b22b5e1997f032b0ba3f8c0
Daniel Scharrer <daniel@constexpr.org>
2013-11-09 02:29:03 +0100

    Add EERIE_3D_BBOX::valid() member

--- src/graphics/BaseGraphicsTypes.h
+++ src/graphics/BaseGraphicsTypes.h
@@ -111,8 +111,8 @@ struct EERIE_2D_BBOX {
 	Vec2f max;
 	
 	void reset() {
-		min.y = min.x = 32000;
-		max.y = max.x = -32000;
+		min = Vec2f(32000);
+		max = Vec2f(-32000);
 	}
 	
 	void add(const Vec3f & pos) {
@@ -127,18 +127,28 @@ struct EERIE_2D_BBOX {
 };
 
 struct EERIE_3D_BBOX {
+	
 	Vec3f min;
 	Vec3f max;
-
+	
+	EERIE_3D_BBOX() { }
+	
+	EERIE_3D_BBOX(const Vec3f & min, const Vec3f & max) : min(min), max(max) { }
+	
 	void reset() {
 		min = Vec3f(99999999.f);
 		max = Vec3f(-99999999.f);
 	}
-
+	
 	void add(const Vec3f & pos) {
 		min = glm::min(min, pos);
 		max = glm::max(max, pos);
 	}
+	
+	bool valid() const {
+		return (min.x <= max.x && min.y <= max.y && min.z <= max.z);
+	}
+	
 };
 
 enum Material {
