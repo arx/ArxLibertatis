6f8b5eb3be0fca9b63ca59fddeb6d90b648b249c
Daniel Scharrer <daniel@constexpr.org>
2013-11-22 07:40:39 +0100

    Remove unused EERIEPOLY_DrawWired()

--- src/graphics/DrawLine.h
+++ src/graphics/DrawLine.h
@@ -38,6 +38,4 @@ void EERIEDrawTrue3DLine(const Vec3f & orgn, const Vec3f & dest, Color col);
 void EERIEDraw3DLine(const Vec3f & orgn, const Vec3f & dest, Color col, float zbias = 0.f);
 void EERIEDraw3DLine(const Vec3f & orgn, const Vec3f & dest, Color color1, Color color2, float zbias = 0.f);
 
-void EERIEPOLY_DrawWired(EERIEPOLY * ep, Color col = Color::none);
-
 #endif // ARX_GRAPHICS_DRAWLINE_H
