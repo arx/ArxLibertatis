4c8a882bc34d9c7ac5347b24d02c76c996808023
Daniel Scharrer <daniel@constexpr.org>
2013-11-22 07:04:48 +0100

    [cppcheck] Remove unused SpecialFade{,R}()

--- src/graphics/effects/CinematicEffects.h
+++ src/graphics/effects/CinematicEffects.h
@@ -55,8 +55,6 @@ int FX_FadeIN(float a, int color, int colord);
 int FX_FadeOUT(float a, int color, int colord);
 bool FX_FlashBlanc(float w, float h, float speed, int color, float fps, float currfps);
 bool FX_Blur(Cinematic * c, CinematicBitmap * tb, EERIE_CAMERA &camera);
-bool SpecialFade(TextureContainer * mask, float ws, float h, float speed, float fps, float fpscurr);
-bool SpecialFadeR(TextureContainer * mask, float ws, float h, float speed, float fps, float fpscurr);
 void FX_DreamPrecalc(CinematicBitmap * bi, float amp, float fps);
 
 #endif // ARX_GRAPHICS_EFFECTS_CINEMATICEFFECTS_H
