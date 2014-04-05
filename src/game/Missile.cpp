910c8325068bf12d628b2a080617805cea8ca3f5
Eli2 <FabianRoth64@googlemail.com>
2014-04-05 14:46:47 +0200

    Replace direct array access with getter

--- src/game/Missile.cpp
+++ src/game/Missile.cpp
@@ -151,14 +151,15 @@ void ARX_MISSILES_Spawn(Entity * io, ARX_SPELLS_MISSILE_TYPE type, const Vec3f *
 			missiles[i].velocity *= 0.8f;
 			missiles[i].longinfo = GetFreeDynLight();
 
-			if (missiles[i].longinfo != -1)
-			{
-				DynLight[missiles[i].longinfo].intensity = 1.3f;
-				DynLight[missiles[i].longinfo].exist = 1;
-				DynLight[missiles[i].longinfo].fallend = 420.f;
-				DynLight[missiles[i].longinfo].fallstart = 250.f;
-				DynLight[missiles[i].longinfo].rgb = Color3f(1.f, .8f, .6f);
-				DynLight[missiles[i].longinfo].pos = *startpos;
+			if(missiles[i].longinfo != -1) {
+				EERIE_LIGHT * light = lightHandleGet(missiles[i].longinfo);
+				
+				light->intensity = 1.3f;
+				light->exist = 1;
+				light->fallend = 420.f;
+				light->fallstart = 250.f;
+				light->rgb = Color3f(1.f, .8f, .6f);
+				light->pos = *startpos;
 			}
 
 			ARX_SOUND_PlaySFX(SND_SPELL_FIRE_WIND, &missiles[i].startpos, 2.0F);
