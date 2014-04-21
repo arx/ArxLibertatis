1082496231615d16d5c06e36fec37f6c0a4e05d9
Eli2 <FabianRoth64@googlemail.com>
2014-04-21 23:58:06 +0200

    Extract spell launch functions

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -2259,6 +2259,141 @@ bool RiseDeadSpellLaunch(SpellType typ, long i, long duration)
 	return true;
 }
 
+void ParalyseSpellLaunch(long i, long duration)
+{
+	ARX_SOUND_PlaySFX(SND_SPELL_PARALYSE, &entities[spells[i].target]->pos);
+	
+	spells[i].exist = true;
+	spells[i].tolive = (duration > -1) ? duration : 5000;
+	
+	float resist_magic = 0.f;
+	if(spells[i].target == 0 && spells[i].caster_level <= player.level) {
+		resist_magic = player.resist_magic;
+	} else if(entities[spells[i].target]->ioflags & IO_NPC) {
+		resist_magic = entities[spells[i].target]->_npcdata->resist_magic;
+	}
+	if(rnd() * 100.f < resist_magic) {
+		float mul = max(0.5f, 1.f - (resist_magic * 0.005f));
+		spells[i].tolive = long(spells[i].tolive * mul);
+	}
+	
+	entities[spells[i].target]->ioflags |= IO_FREEZESCRIPT;
+	
+	ARX_SPELLS_AddSpellOn(spells[i].target, i);
+	ARX_NPC_Kill_Spell_Launch(entities[spells[i].target]);
+}
+
+void CreateFieldSpellLaunch(SpellcastFlags flags, long i, long duration)
+{
+	spells[i].exist = true;
+	
+	unsigned long start = (unsigned long)(arxtime);
+	if(flags & SPELLCAST_FLAG_RESTORE) {
+		start -= std::min(start, 4000ul);
+	}
+	spells[i].lastupdate = spells[i].timcreation = start;
+	
+	spells[i].tolive = (duration > -1) ? duration : 800000;
+	spells[i].bDuration = true;
+	spells[i].fManaCostPerSecond = 1.2f;
+	
+	Vec3f target;
+	float beta;
+	bool displace = false;
+	if(spells[i].caster == 0) {
+		target = entities.player()->pos;
+		beta = player.angle.getPitch();
+		displace = true;
+	} else {
+		if(ValidIONum(spells[i].caster)) {
+			Entity * io = entities[spells[i].caster];
+			target = io->pos;
+			beta = io->angle.getPitch();
+			displace = (io->ioflags & IO_NPC) == IO_NPC;
+		} else {
+			ARX_DEAD_CODE();
+		}
+	}
+	if(displace) {
+		target.x -= std::sin(radians(MAKEANGLE(beta))) * 250.f;
+		target.z += std::cos(radians(MAKEANGLE(beta))) * 250.f;
+	}
+	
+	ARX_SOUND_PlaySFX(SND_SPELL_CREATE_FIELD, &target);
+	
+	CCreateField * effect  = new CCreateField();
+	effect->spellinstance = i;
+	
+	res::path cls = "graph/obj3d/interactive/fix_inter/blue_cube/blue_cube";
+	Entity * io = AddFix(cls, -1, IO_IMMEDIATELOAD);
+	if(io) {
+		
+		ARX_INTERACTIVE_HideGore(io);
+		RestoreInitialIOStatusOfIO(io);
+		spells[i].longinfo_entity = io->index();
+		io->scriptload = 1;
+		io->ioflags |= IO_NOSAVE | IO_FIELD;
+		io->initpos = io->pos = target;
+		SendInitScriptEvent(io);
+		
+		effect->Create(target);
+		effect->SetDuration(spells[i].tolive);
+		effect->lLightId = GetFreeDynLight();
+		
+		if(lightHandleIsValid(effect->lLightId)) {
+			EERIE_LIGHT * light = lightHandleGet(effect->lLightId);
+			
+			light->intensity = 0.7f + 2.3f;
+			light->fallend = 500.f;
+			light->fallstart = 400.f;
+			light->rgb = Color3f(0.8f, 0.0f, 1.0f);
+			light->pos = effect->eSrc - Vec3f(0.f, 150.f, 0.f);
+		}
+		
+		spells[i].pSpellFx = effect;
+		spells[i].tolive = effect->GetDuration();
+		
+		if(flags & SPELLCAST_FLAG_RESTORE) {
+			effect->Update(4000);
+		}
+		
+	} else {
+		spells[i].tolive = 0;
+	}
+}
+
+void DisarmTrapSpellLaunch(long i)
+{
+	ARX_SOUND_PlaySFX(SND_SPELL_DISARM_TRAP);
+	
+	spells[i].exist = true;
+	spells[i].lastupdate = spells[i].timcreation = (unsigned long)(arxtime);
+	spells[i].tolive = 1;
+	
+	EERIE_SPHERE sphere;
+	sphere.origin = player.pos;
+	sphere.radius = 400.f;
+	
+	for(size_t n = 0; n < MAX_SPELLS; n++) {
+		
+		if(!spells[n].exist || spells[n].type != SPELL_RUNE_OF_GUARDING) {
+			continue;
+		}
+		
+		if(!spells[n].pSpellFx) {
+			continue;
+		}
+		
+		CSpellFx * effect = spells[n].pSpellFx;
+		if(sphere.contains(static_cast<CRuneOfGuarding *>(effect)->eSrc)) {
+			spells[n].caster_level -= spells[i].caster_level;
+			if(spells[n].caster_level <= 0) {
+				spells[n].tolive = 0;
+			}
+		}
+	}
+}
+
 bool ARX_SPELLS_Launch(SpellType typ, long source, SpellcastFlags flagss, long levell, long target, long duration) {
 	
 	SpellcastFlags flags = flagss;
@@ -2673,138 +2808,15 @@ bool ARX_SPELLS_Launch(SpellType typ, long source, SpellcastFlags flagss, long l
 			break;
 		}
 		case SPELL_PARALYSE: {
-			ARX_SOUND_PlaySFX(SND_SPELL_PARALYSE, &entities[spells[i].target]->pos);
-			
-			spells[i].exist = true;
-			spells[i].tolive = (duration > -1) ? duration : 5000;
-			
-			float resist_magic = 0.f;
-			if(spells[i].target == 0 && spells[i].caster_level <= player.level) {
-				resist_magic = player.resist_magic;
-			} else if(entities[spells[i].target]->ioflags & IO_NPC) {
-				resist_magic = entities[spells[i].target]->_npcdata->resist_magic;
-			}
-			if(rnd() * 100.f < resist_magic) {
-				float mul = max(0.5f, 1.f - (resist_magic * 0.005f));
-				spells[i].tolive = long(spells[i].tolive * mul);
-			}
-			
-			entities[spells[i].target]->ioflags |= IO_FREEZESCRIPT;
-			
-			ARX_SPELLS_AddSpellOn(spells[i].target, i);
-			ARX_NPC_Kill_Spell_Launch(entities[spells[i].target]);
-			
+			ParalyseSpellLaunch(i, duration);
 			break;
 		}
 		case SPELL_CREATE_FIELD: {
-			spells[i].exist = true;
-			
-			unsigned long start = (unsigned long)(arxtime);
-			if(flags & SPELLCAST_FLAG_RESTORE) {
-				start -= std::min(start, 4000ul);
-			}
-			spells[i].lastupdate = spells[i].timcreation = start;
-			
-			spells[i].tolive = (duration > -1) ? duration : 800000;
-			spells[i].bDuration = true;
-			spells[i].fManaCostPerSecond = 1.2f;
-			
-			Vec3f target;
-			float beta;
-			bool displace = false;
-			if(spells[i].caster == 0) {
-				target = entities.player()->pos;
-				beta = player.angle.getPitch();
-				displace = true;
-			} else {
-				if(ValidIONum(spells[i].caster)) {
-					Entity * io = entities[spells[i].caster];
-					target = io->pos;
-					beta = io->angle.getPitch();
-					displace = (io->ioflags & IO_NPC) == IO_NPC;
-				} else {
-					ARX_DEAD_CODE();
-				}
-			}
-			if(displace) {
-				target.x -= std::sin(radians(MAKEANGLE(beta))) * 250.f;
-				target.z += std::cos(radians(MAKEANGLE(beta))) * 250.f;
-			}
-			
-			ARX_SOUND_PlaySFX(SND_SPELL_CREATE_FIELD, &target);
-			
-			CCreateField * effect  = new CCreateField();
-			effect->spellinstance = i;
-			
-			res::path cls = "graph/obj3d/interactive/fix_inter/blue_cube/blue_cube";
-			Entity * io = AddFix(cls, -1, IO_IMMEDIATELOAD);
-			if(io) {
-				
-				ARX_INTERACTIVE_HideGore(io);
-				RestoreInitialIOStatusOfIO(io);
-				spells[i].longinfo_entity = io->index();
-				io->scriptload = 1;
-				io->ioflags |= IO_NOSAVE | IO_FIELD;
-				io->initpos = io->pos = target;
-				SendInitScriptEvent(io);
-				
-				effect->Create(target);
-				effect->SetDuration(spells[i].tolive);
-				effect->lLightId = GetFreeDynLight();
-				
-				if(lightHandleIsValid(effect->lLightId)) {
-					EERIE_LIGHT * light = lightHandleGet(effect->lLightId);
-					
-					light->intensity = 0.7f + 2.3f;
-					light->fallend = 500.f;
-					light->fallstart = 400.f;
-					light->rgb = Color3f(0.8f, 0.0f, 1.0f);
-					light->pos = effect->eSrc - Vec3f(0.f, 150.f, 0.f);
-				}
-				
-				spells[i].pSpellFx = effect;
-				spells[i].tolive = effect->GetDuration();
-				
-				if(flags & SPELLCAST_FLAG_RESTORE) {
-					effect->Update(4000);
-				}
-				
-			} else {
-				spells[i].tolive = 0;
-			}
-			
+			CreateFieldSpellLaunch(flags, i, duration);
 			break;
 		}
 		case SPELL_DISARM_TRAP: {
-			ARX_SOUND_PlaySFX(SND_SPELL_DISARM_TRAP);
-			
-			spells[i].exist = true;
-			spells[i].lastupdate = spells[i].timcreation = (unsigned long)(arxtime);
-			spells[i].tolive = 1;
-			
-			EERIE_SPHERE sphere;
-			sphere.origin = player.pos;
-			sphere.radius = 400.f;
-			
-			for(size_t n = 0; n < MAX_SPELLS; n++) {
-				
-				if(!spells[n].exist || spells[n].type != SPELL_RUNE_OF_GUARDING) {
-					continue;
-				}
-				
-				if(!spells[n].pSpellFx) {
-					continue;
-				}
-				
-				CSpellFx * effect = spells[n].pSpellFx;
-				if(sphere.contains(static_cast<CRuneOfGuarding *>(effect)->eSrc)) {
-					spells[n].caster_level -= spells[i].caster_level;
-					if(spells[n].caster_level <= 0) {
-						spells[n].tolive = 0;
-					}
-				}
-			}
-			
+			DisarmTrapSpellLaunch(i);
 			break;
 		}
 		case SPELL_SLOW_DOWN: {
