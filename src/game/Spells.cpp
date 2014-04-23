c4efc866e637ff1585d7564b8af3d32300e018cc
Eli2 <FabianRoth64@googlemail.com>
2014-04-23 03:51:55 +0200

    Extract spell update functions

--- src/game/Spells.cpp
+++ src/game/Spells.cpp
@@ -2273,7 +2273,7 @@ void FireballSpellUpdate(size_t i)
 
 		pCSpellFX->Update(framedelay);
 		ARX_SOUND_RefreshPosition(spells[i].snd_loop, pCF->eCurPos);
-	}	
+	}
 }
 
 void SpeedSpellUpdate(size_t i)
@@ -2284,7 +2284,7 @@ void SpeedSpellUpdate(size_t i)
 		
 		spells[i].pSpellFx->Update(framedelay);
 		spells[i].pSpellFx->Render();
-	}	
+	}
 }
 
 void CreateFoodSpellUpdate(size_t i)
@@ -2300,7 +2300,7 @@ void IceProjectileUpdate(size_t i)
 	if(spells[i].pSpellFx) {
 		spells[i].pSpellFx->Update(framedelay);
 		spells[i].pSpellFx->Render();
-	}	
+	}
 }
 
 void DispellIllusionSpellUpdate(size_t i)
@@ -2308,7 +2308,146 @@ void DispellIllusionSpellUpdate(size_t i)
 	if(spells[i].pSpellFx) {
 		spells[i].pSpellFx->Update(framedelay);
 		spells[i].pSpellFx->Render();
-	}	
+	}
+}
+
+void BlessSpellUpdate(size_t i)
+{
+	if(spells[i].pSpellFx) {
+		CBless * pBless=(CBless *)spells[i].pSpellFx;
+
+		if(pBless) {
+			if(ValidIONum(spells[i].target)) {
+				pBless->eSrc = entities[spells[i].target]->pos;
+				Anglef angle = Anglef::ZERO;
+
+				if(spells[i].target == 0)
+					angle.setPitch(player.angle.getPitch());
+				else 
+					angle.setPitch(entities[spells[i].target]->angle.getPitch());
+
+				pBless->Set_Angle(angle);
+			}
+		}
+
+		spells[i].pSpellFx->Update(framedelay);
+		spells[i].pSpellFx->Render();
+	}
+}
+
+void CurseSpellUpdate(size_t i)
+{
+	if(spells[i].pSpellFx) {
+		CCurse * curse=(CCurse *)spells[i].pSpellFx;
+		Vec3f target = Vec3f_ZERO;
+			
+		if(spells[i].target >= 0 && entities[spells[i].target]) {
+			target = entities[spells[i].target]->pos;
+
+			if(spells[i].target == 0)
+				target.y -= 200.f;
+			else
+				target.y += entities[spells[i].target]->physics.cyl.height - 30.f;
+		}
+		
+		curse->Update(checked_range_cast<unsigned long>(framedelay));
+		
+		curse->eTarget = target;
+		curse->Render();
+	}
+}
+
+void FireProtectionSpellUpdate(size_t i)
+{
+	spells[i].pSpellFx->Update(framedelay);
+	spells[i].pSpellFx->Render();
+	ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
+}
+
+void ColdProtectionSpellUpdate(size_t i)
+{
+	spells[i].pSpellFx->Update(framedelay);
+	spells[i].pSpellFx->Render();
+	ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
+}
+
+void CurePoisonSpellUpdate(size_t i)
+{
+	if(spells[i].pSpellFx) {
+		spells[i].pSpellFx->Update(framedelay);
+		spells[i].pSpellFx->Render();					
+	}
+}
+
+void RuneOfGuardingSpellUpdate(size_t i)
+{
+	if(spells[i].pSpellFx) {
+		spells[i].pSpellFx->Update(framedelay);
+		spells[i].pSpellFx->Render();
+		CRuneOfGuarding * pCRG=(CRuneOfGuarding *)spells[i].pSpellFx;
+
+		if (pCRG)
+		{
+			EERIE_SPHERE sphere;
+			sphere.origin = pCRG->eSrc;
+			sphere.radius=std::max(spells[i].caster_level*15.f,50.f);
+
+			if (CheckAnythingInSphere(&sphere,spells[i].caster,CAS_NO_SAME_GROUP | CAS_NO_BACKGROUND_COL | CAS_NO_ITEM_COL| CAS_NO_FIX_COL | CAS_NO_DEAD_COL))
+			{
+				ARX_BOOMS_Add(pCRG->eSrc);
+				LaunchFireballBoom(&pCRG->eSrc,(float)spells[i].caster_level);
+				DoSphericDamage(&pCRG->eSrc,4.f*spells[i].caster_level,30.f*spells[i].caster_level,DAMAGE_AREA,DAMAGE_TYPE_FIRE | DAMAGE_TYPE_MAGICAL,spells[i].caster);
+				spells[i].tolive=0;
+				ARX_SOUND_PlaySFX(SND_SPELL_RUNE_OF_GUARDING_END, &sphere.origin);
+			}
+		}
+	}
+}
+
+void RepelUndeadSpellUpdate(size_t i)
+{
+	if(spells[i].pSpellFx) {
+		spells[i].pSpellFx->Update(framedelay);
+		spells[i].pSpellFx->Render();					
+
+		if (spells[i].target == 0)
+			ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
+	}
+}
+
+void PoisonProjectileSpellUpdate(size_t i)
+{
+	if(spells[i].pSpellFx) {
+		spells[i].pSpellFx->Update(framedelay);
+		spells[i].pSpellFx->Render();
+	}
+}
+
+void LevitateSpellUpdate(size_t i)
+{
+	CLevitate *pLevitate=(CLevitate *)spells[i].pSpellFx;
+	Vec3f target;
+
+	if(spells[i].target == 0) {
+		target.x=player.pos.x;
+		target.y=player.pos.y+150.f;
+		target.z=player.pos.z;
+		player.levitate = true;
+	} else {
+		target.x = entities[spells[i].caster]->pos.x;
+		target.y = entities[spells[i].caster]->pos.y;
+		target.z = entities[spells[i].caster]->pos.z;
+	}
+
+	pLevitate->ChangePos(&target);
+		
+	CSpellFx *pCSpellFX = spells[i].pSpellFx;
+
+	if(pCSpellFX) {
+		pCSpellFX->Update(framedelay);
+		pCSpellFX->Render();
+	}
+	ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
 }
 
 void ARX_SPELLS_Update_Update(size_t i, unsigned long tim) {
@@ -2386,135 +2525,41 @@ void ARX_SPELLS_Update_Update(size_t i, unsigned long tim) {
 		//****************************************************************************
 		// LEVEL 4 SPELLS
 		case SPELL_BLESS: {
-			if(spells[i].pSpellFx) {
-				CBless * pBless=(CBless *)spells[i].pSpellFx;
-
-				if(pBless) {
-					if(ValidIONum(spells[i].target)) {
-						pBless->eSrc = entities[spells[i].target]->pos;
-						Anglef angle = Anglef::ZERO;
-
-						if(spells[i].target == 0)
-							angle.setPitch(player.angle.getPitch());
-						else 
-							angle.setPitch(entities[spells[i].target]->angle.getPitch());
-
-						pBless->Set_Angle(angle);
-					}
-				}
-
-				spells[i].pSpellFx->Update(framedelay);
-				spells[i].pSpellFx->Render();
-			}
+			BlessSpellUpdate(i);
 			break;
 		}
 		case SPELL_CURSE: {
-			if(spells[i].pSpellFx) {
-				CCurse * curse=(CCurse *)spells[i].pSpellFx;
-				Vec3f target = Vec3f_ZERO;
-					
-				if(spells[i].target >= 0 && entities[spells[i].target]) {
-					target = entities[spells[i].target]->pos;
-	
-					if(spells[i].target == 0)
-						target.y -= 200.f;
-					else
-						target.y += entities[spells[i].target]->physics.cyl.height - 30.f;
-				}
-				
-				curse->Update(checked_range_cast<unsigned long>(framedelay));
-				
-				curse->eTarget = target;
-				curse->Render();
-			}
+			CurseSpellUpdate(i);
 			break;
 		}
 		case SPELL_FIRE_PROTECTION: {
-			spells[i].pSpellFx->Update(framedelay);
-			spells[i].pSpellFx->Render();
-			ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
+			FireProtectionSpellUpdate(i);
 			break;
 		}
 		case SPELL_COLD_PROTECTION: {
-			spells[i].pSpellFx->Update(framedelay);
-			spells[i].pSpellFx->Render();
-			ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
+			ColdProtectionSpellUpdate(i);
 			break;
 		}
 		//****************************************************************************
 		// LEVEL 5 SPELLS
 		case SPELL_CURE_POISON: {
-			if(spells[i].pSpellFx) {
-				spells[i].pSpellFx->Update(framedelay);
-				spells[i].pSpellFx->Render();					
-			}
+			CurePoisonSpellUpdate(i);
 			break;
 		}
 		case SPELL_RUNE_OF_GUARDING: {
-			if(spells[i].pSpellFx) {
-				spells[i].pSpellFx->Update(framedelay);
-				spells[i].pSpellFx->Render();
-				CRuneOfGuarding * pCRG=(CRuneOfGuarding *)spells[i].pSpellFx;
-
-				if (pCRG)
-				{
-					EERIE_SPHERE sphere;
-					sphere.origin = pCRG->eSrc;
-					sphere.radius=std::max(spells[i].caster_level*15.f,50.f);
-
-					if (CheckAnythingInSphere(&sphere,spells[i].caster,CAS_NO_SAME_GROUP | CAS_NO_BACKGROUND_COL | CAS_NO_ITEM_COL| CAS_NO_FIX_COL | CAS_NO_DEAD_COL))
-					{
-						ARX_BOOMS_Add(pCRG->eSrc);
-						LaunchFireballBoom(&pCRG->eSrc,(float)spells[i].caster_level);
-						DoSphericDamage(&pCRG->eSrc,4.f*spells[i].caster_level,30.f*spells[i].caster_level,DAMAGE_AREA,DAMAGE_TYPE_FIRE | DAMAGE_TYPE_MAGICAL,spells[i].caster);
-						spells[i].tolive=0;
-						ARX_SOUND_PlaySFX(SND_SPELL_RUNE_OF_GUARDING_END, &sphere.origin);
-					}
-				}
-			}
+			RuneOfGuardingSpellUpdate(i);
 			break;
 		}
 		case SPELL_REPEL_UNDEAD: {
-			if(spells[i].pSpellFx) {
-				spells[i].pSpellFx->Update(framedelay);
-				spells[i].pSpellFx->Render();					
-
-				if (spells[i].target == 0)
-					ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
-			}
+			RepelUndeadSpellUpdate(i);
 			break;
 		}
 		case SPELL_POISON_PROJECTILE: {
-			if(spells[i].pSpellFx) {
-				spells[i].pSpellFx->Update(framedelay);
-				spells[i].pSpellFx->Render();
-			}
+			PoisonProjectileSpellUpdate(i);
 			break;
 		}
 		case SPELL_LEVITATE: {
-			CLevitate *pLevitate=(CLevitate *)spells[i].pSpellFx;
-			Vec3f target;
-
-			if(spells[i].target == 0) {
-				target.x=player.pos.x;
-				target.y=player.pos.y+150.f;
-				target.z=player.pos.z;
-				player.levitate = true;
-			} else {
-				target.x = entities[spells[i].caster]->pos.x;
-				target.y = entities[spells[i].caster]->pos.y;
-				target.z = entities[spells[i].caster]->pos.z;
-			}
-
-			pLevitate->ChangePos(&target);
-				
-			CSpellFx *pCSpellFX = spells[i].pSpellFx;
-
-			if(pCSpellFX) {
-				pCSpellFX->Update(framedelay);
-				pCSpellFX->Render();
-			}
-			ARX_SOUND_RefreshPosition(spells[i].snd_loop, entities[spells[i].target]->pos);
+			LevitateSpellUpdate(i);
 			break;
 		}
 		//****************************************************************************
