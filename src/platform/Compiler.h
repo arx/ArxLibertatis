5d783f1d9d0ff61291ed70841dc3d4e92410ffa0
Daniel Scharrer <daniel@constexpr.org>
2014-05-16 16:32:17 +0200

    Update compiler version macros
    - Calculate the MSVC version string from _MSC_VER
    - Recognize icc and ekopath

--- src/platform/Compiler.h
+++ src/platform/Compiler.h
@@ -25,30 +25,40 @@
 #ifndef ARX_PLATFORM_COMPILER_H
 #define ARX_PLATFORM_COMPILER_H
 
-#if defined(__clang__)
+#include <ostream>
+
+#include "platform/Platform.h" // needed for ARX_STR
+
+#if defined(__INTEL_COMPILER)
+	#define ARX_COMPILER_NAME           "Intel C++"
+	struct arx_icc_vername { };
+	std::ostream & operator<<(std::ostream & os, const arx_icc_vername & /* tag */) {
+		return os << ARX_COMPILER_NAME << ' ' << (__INTEL_COMPILER / 100) << '.'
+		          << (__INTEL_COMPILER % 100);
+	}
+	#define ARX_COMPILER_VERNAME        arx_icc_vername()
+#elif defined(__PATHSCALE__)
+	#define ARX_COMPILER_NAME           "EKOPath"
+	#define ARX_COMPILER_VERNAME        ARX_COMPILER_NAME " " __PATHSCALE__
+#elif defined(__clang_version__)
 	#define ARX_COMPILER_NAME           "Clang"
 	#define ARX_COMPILER_VERNAME        ARX_COMPILER_NAME " " __clang_version__
 #elif defined(__MINGW32__)
-	#define ARX_COMPILER_NAME           "MinGW32"
+	#define ARX_COMPILER_NAME           "MinGW"
 	#define ARX_COMPILER_VERNAME        ARX_COMPILER_NAME " " \
 	                                    ARX_STR(__MINGW32_MAJOR_VERSION) \
-	                                    "." ARX_STR(__MINGW32_MINOR_VERSION)
+	                                    "." ARX_STR(__MINGW32_MINOR_VERSION) \
+	                                    " (GCC " __VERSION__ ")"
 #elif defined(__GNUC__)
 	#define ARX_COMPILER_NAME           "GCC"
 	#define ARX_COMPILER_VERNAME        ARX_COMPILER_NAME " " __VERSION__
 #elif defined(_MSC_VER)
-	#if _MSC_VER < 1500 || _MSC_VER >= 1900
-		#define ARX_COMPILER_NAME         "MSVC"
-	#elif _MSC_VER < 1600
-		#define ARX_COMPILER_NAME         "VC9"
-	#elif _MSC_VER < 1700
-		#define ARX_COMPILER_NAME         "VC10"
-	#elif _MSC_VER < 1800
-		#define ARX_COMPILER_NAME         "VC11"
-	#elif _MSC_VER < 1900
-		#define ARX_COMPILER_NAME         "VC12"
-	#endif
-	#define ARX_COMPILER_VERNAME        ARX_COMPILER_NAME " " ARX_STR(_MSC_VER)
+	#define ARX_COMPILER_NAME           "MSVC"
+	struct arx_msvc_vername { };
+	std::ostream & operator<<(std::ostream & os, const arx_msvc_vername & /* tag */) {
+		return os << ARX_COMPILER_NAME << ' ' << (_MSC_VER / 100 - 6) << '.' << (_MSC_VER % 100);
+	}
+	#define ARX_COMPILER_VERNAME        arx_msvc_vername()
 #else
 	#define ARX_COMPILER_NAME           "Unknown"
 	#define ARX_COMPILER_VERNAME        ARX_COMPILER_NAME
