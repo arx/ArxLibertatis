0f56196ca0aaeec9d308c0035cd40991e0e4832f
Daniel Scharrer <daniel@constexpr.org>
2014-05-30 00:54:09 +0200

    Process.h: Use \brief for multi-line docs

--- src/platform/Process.h
+++ src/platform/Process.h
@@ -27,61 +27,137 @@
 namespace platform {
 
 /*!
- * Start another executable and wait for it to finish.
+ * \brief Start a program and wait for it to finish
+ *
  * The executable's standard output/error is discarded.
  *
  * @param exe  the program to run, either an absolute path or a program name in the PATH.
- * @param args program arguments. The first arguments should be the program name/path and
- *             the last argument should be NULL.
+ * @param args program arguments. The first argument should be the program name/path and
+ *             the last argument must be NULL.
  *
  * @return the programs exit code or a negative value on error.
  */
 int run(const char * exe, const char * const args[]);
+
+/*!
+ * \brief Start a program and wait for it to finish
+ *
+ * The executable's standard output/error is discarded.
+ *
+ * @param args program arguments. The first argument is the program to run, either an
+ *             absolute path or a program name in the PATH. The last argument must be NULL.
+ *
+ * @return the programs exit code or a negative value on error.
+ */
 inline int run(const char * const args[]) {
 	return run(args[0], args);
 }
+
+/*!
+ * \brief Start a program and wait for it to finish
+ *
+ * The executable's standard output/error is discarded.
+ *
+ * @param exe  the program to run, either an absolute path or a program name in the PATH.
+ * @param args program arguments. The first argument should be the program name/path and
+ *             the last argument must be NULL.
+ *
+ * @return the programs exit code or a negative value on error.
+ */
 inline int run(const std::string & exe, const char * const args[]) {
 	return run(exe.c_str(), args);
 }
 
 /*!
- * Start another executable without waiting for it to finish.
- * The executable's standard output/error is discarded.
+ * \brief Start a program without waiting for it to finish
+ *
+ * The program's standard output/error is discarded.
  *
  * @param exe  the program to run, either an absolute path or a program name in the PATH.
- * @param args program arguments. The first arguments should be the program name/path and
- *             the last argument should be NULL.
+ * @param args program arguments. The first argument should be the program name/path and
+ *             the last argument must be NULL.
  */
 void runAsync(const char * exe, const char * const args[]);
+
+/*!
+ * \brief Start a program without waiting for it to finish
+ *
+ * The program's standard output/error is discarded.
+ *
+ * @param args program arguments. The first argument is the program to run, either an
+ *             absolute path or a program name in the PATH. The last argument must be NULL.
+ */
 inline void runAsync(const char * const args[]) {
 	return runAsync(args[0], args);
 }
+
+/*!
+ * \brief Start a program without waiting for it to finish
+ *
+ * The program's standard output/error is discarded.
+ *
+ * @param exe  the program to run, either an absolute path or a program name in the PATH.
+ * @param args program arguments. The first argument should be the program name/path and
+ *             the last argument must be NULL.
+ */
 inline void runAsync(const std::string & exe, const char * const args[]) {
 	return runAsync(exe.c_str(), args);
 }
 
 #if ARX_PLATFORM != ARX_PLATFORM_WIN32
+
 /*!
- * Start another executable without waiting for it to finish.
- * The executable's standard output is retuned, standard error is discarded.
+ * \brief Start a program and get its standard output
+ *
+ * The program's standard standard error is discarded.
  *
  * @param exe  the program to run, either an absolute path or a program name in the PATH.
- * @param args program arguments. The first arguments should be the program name/path and
- *             the last argument should be NULL.
+ * @param args program arguments. The first argument should be the program name/path and
+ *             the last argument must be NULL.
  * @param unlocalized true if localization environment varuables should be reset.
+ *
+ * @return the program's standard output, or an empty string if there was an error.
  */
 std::string getOutputOf(const char * exe, const char * const args[],
                         bool unlocalized = false);
+
+/*!
+ * \brief Start a program and get its standard output
+ *
+ * The program's standard standard error is discarded.
+ *
+ * @param args program arguments. The first argument is the program to run, either an
+ *             absolute path or a program name in the PATH. The last argument must be NULL.
+ * @param unlocalized true if localization environment varuables should be reset.
+ *
+ * @return the program's standard output, or an empty string if there was an error.
+ */
 inline std::string getOutputOf(const char * const args[], bool unlocalized = false) {
 	return getOutputOf(args[0], args, unlocalized);
 }
+
+/*!
+ * \brief Start a program and get its standard output
+ *
+ * The program's standard standard error is discarded.
+ *
+ * @param exe  the program to run, either an absolute path or a program name in the PATH.
+ * @param args program arguments. The first argument should be the program name/path and
+ *             the last argument must be NULL.
+ * @param unlocalized true if localization environment varuables should be reset.
+ *
+ * @return the program's standard output, or an empty string if there was an error.
+ */
 inline std::string getOutputOf(const std::string & exe, const char * const args[],
                                bool unlocalized = false) {
 	return getOutputOf(exe.c_str(), args, unlocalized);
 }
+
 #endif
 
 /*!
+ * \brief Run a helper executable
+ *
  * Equivalent to
  * @code
  *  fs::path exe = getHelperExecutable(name);
