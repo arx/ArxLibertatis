de471b10964914c319d435739e374690dc60894b
Daniel Scharrer <daniel@constexpr.org>
2014-05-30 02:25:07 +0200

    Add doxygen comments for dialog functions

--- src/platform/Dialog.h
+++ src/platform/Dialog.h
@@ -32,11 +32,93 @@ enum DialogType {
 	DialogOkCancel
 };
 
+/*!
+ * \brief Show an informative message box
+ *
+ * \param message text of the message to display.
+ * \param title   title for the dialog window.
+ *
+ * A simple markup syntax is supported on some systems. On other systems, the message is
+ * displayed as-is:
+ *
+ *  * Lines staring with " * " are turned into bulleted lists.
+ *
+ *  * URLs starting with "http://" or "https://" are turned into links.
+ *
+ *  * Text surrounded by quotes is made bold.
+ *
+ * This function does not retun until the user closes the dialog.
+ *
+ * \note this may not be supported on all platforms. If dialogs are not supported, the
+ *       message is printed to stdout and the function retuns immediately.
+ */
 void showInfoDialog(const std::string & message, const std::string & title = "Information");
+
+/*!
+ * \brief Show a warning message box
+ *
+ * \param message text of the message to display.
+ * \param title   title for the dialog window.
+ *
+ * A simple markup syntax is supported on some systems. On other systems, the message is
+ * displayed as-is. \see \ref showInfoDialog for the markup syntax.
+ *
+ * This function does not retun until the user closes the dialog.
+ *
+ * \note this may not be supported on all platforms. If dialogs are not supported, the
+ *       message is printed to stdout and the function retuns immediately.
+ */
 void showWarningDialog(const std::string & message, const std::string & title = "Warning");
+
+
+/*!
+ * \brief Show an error message box
+ *
+ * \param message text of the message to display.
+ * \param title   title for the dialog window.
+ *
+ * A simple markup syntax is supported on some systems. On other systems, the message is
+ * displayed as-is. \see \ref showInfoDialog for the markup syntax.
+ *
+ * This function does not retun until the user closes the dialog.
+ *
+ * \note this may not be supported on all platforms. If dialogs are not supported, the
+ *       message is printed to stdout and the function retuns immediately.
+ */
 void showErrorDialog(const std::string & message, const std::string & title = "Error");
 
+/*!
+ * \brief Show a dialog asking a Yes/No question
+ *
+ * \param question text of the question to display.
+ * \param title    title for the dialog window.
+ *
+ * A simple markup syntax is supported on some systems. On other systems, the message is
+ * displayed as-is. \see \ref showInfoDialog for the markup syntax.
+ *
+ * \note this may not be supported on all platforms. If dialogs are not supported, the
+ *       message is printed to stdout and the function immediately returns \c true.
+ *
+ * \return \c true if the user clicked "Yes", or \c false if the user clicked "No" or
+ *         closed the dialog.
+ */
 bool askYesNo(const std::string & question, const std::string & title = "Yes/No?");
+
+/*!
+ * \brief Show a dialog asking a Ok/Cancel question
+ *
+ * \param question text of the question to display.
+ * \param title    title for the dialog window.
+ *
+ * A simple markup syntax is supported on some systems. On other systems, the message is
+ * displayed as-is. \see \ref showInfoDialog for the markup syntax.
+ *
+ * \note this may not be supported on all platforms. If dialogs are not supported, the
+ *       message is printed to stdout and the function immediately returns \c true.
+ *
+ * \return \c true if the user clicked "Ok", or \c false if the user clicked "Cancel" or
+ *         closed the dialog.
+ */
 bool askOkCancel(const std::string & question, const std::string & title = "Ok/Cancel?");
 
 } // namespace platform
