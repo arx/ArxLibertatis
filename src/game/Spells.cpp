arx/ArxLibertatis

commit 2c20ae48d808d1bcc3346bb8bbabe681e923c0b6

Extract method

src/game/Spells.cpp
@@ -197,6 +197,27 @@ bool SpellManager::ExistAnyInstanceForThisCaster(SpellType typ, EntityHandle cas
 	return false;
 }
 
+SpellBase * SpellManager::getSpellOnTarget(const Entity * target, SpellType type)
+{
+	if(!target)
+		return NULL;
+	
+	for(size_t i = 0; i < MAX_SPELLS; i++) {
+		SpellBase * spell = spells[SpellHandle(i)];
+		if(!spell->m_exist)
+			continue;
+		
+		if(spell->m_type != type)
+			continue;
+		
+		if(std::find(spell->m_targets.begin(), spell->m_targets.end(), target->index()) != spell->m_targets.end()) {
+			return spell;
+		}
+	}
+	
+	return NULL;
+}
+
 
 bool GetSpellPosition(Vec3f * pos, SpellBase * spell)
 {
@@ -347,26 +368,7 @@ bool GetSpellPosition(Vec3f * pos, SpellBase * spell)
 	return false;
 }
 
-SpellBase * ARX_SPELLS_GetSpellOn(const Entity * io, SpellType spellid)
-{
-	if(!io)
-		return NULL;
-	
-	for(size_t i = 0; i < MAX_SPELLS; i++) {
-		SpellBase * spell = spells[SpellHandle(i)];
-		if(!spell->m_exist)
-			continue;
-		
-		if(spell->m_type != spellid)
-			continue;
-		
-		if(std::find(spell->m_targets.begin(), spell->m_targets.end(), io->index()) != spell->m_targets.end()) {
-			return spell;
-		}
-	}
-	
-	return NULL;
-}
+
 
 void ARX_SPELLS_RemoveAllSpellsOn(Entity *io) {
 	
@@ -986,7 +988,7 @@ void ARX_SPELLS_LaunchSpellTarget(Entity * io) {
 float ARX_SPELLS_ApplyFireProtection(Entity * io,float damages)
 {
 	if(io) {
-		SpellBase * spell = ARX_SPELLS_GetSpellOn(io, SPELL_FIRE_PROTECTION);
+		SpellBase * spell = spells.getSpellOnTarget(io, SPELL_FIRE_PROTECTION);
 		if(spell) {
 			float modif = 1.f - (spell->m_caster_level * ( 1.0f / 10 ));
 
@@ -1008,7 +1010,7 @@ float ARX_SPELLS_ApplyFireProtection(Entity * io,float damages)
 
 float ARX_SPELLS_ApplyColdProtection(Entity * io,float damages)
 {
-	SpellBase * spell = ARX_SPELLS_GetSpellOn(io, SPELL_COLD_PROTECTION);
+	SpellBase * spell = spells.getSpellOnTarget(io, SPELL_COLD_PROTECTION);
 	if(spell) {
 		float modif = 1.f - (spell->m_caster_level * ( 1.0f / 10 ));
 

src/animation/AnimationRender.cpp
@@ -240,7 +240,7 @@ float Cedric_GetInvisibility(Entity *io) {
 			invisibility -= 1.f;
 
 		if(io != entities.player() && invisibility > 0.f && !EXTERNALVIEW) {
-			SpellBase * spell = ARX_SPELLS_GetSpellOn(io, SPELL_INVISIBILITY);
+			SpellBase * spell = spells.getSpellOnTarget(io, SPELL_INVISIBILITY);
 
 			if(spell) {
 				if(player.m_skillFull.intuition > spell->m_caster_level * 10) {
@@ -374,10 +374,10 @@ void Cedric_ApplyLightingFirstPartRefactor(Entity *io) {
 						if(io->sfx_flag & SFX_TYPE_INCINERATE) {
 							io->sfx_flag &= ~SFX_TYPE_INCINERATE;
 							io->sfx_flag &= ~SFX_TYPE_YLSIDE_DEATH;
-							SpellBase * spell = ARX_SPELLS_GetSpellOn(io, SPELL_INCINERATE);
+							SpellBase * spell = spells.getSpellOnTarget(io, SPELL_INCINERATE);
 
 							if(!spell)
-								spell = ARX_SPELLS_GetSpellOn(io, SPELL_MASS_INCINERATE);
+								spell = spells.getSpellOnTarget(io, SPELL_MASS_INCINERATE);
 
 							if(spell) {
 								spell->m_tolive = 0;

src/core/ArxGame.cpp
@@ -1278,12 +1278,12 @@ void ArxGame::updateLevel() {
 
 		SpellBase * spell;
 		
-		spell = ARX_SPELLS_GetSpellOn(entity, SPELL_SPEED);
+		spell = spells.getSpellOnTarget(entity, SPELL_SPEED);
 		if(spell) {
 			speedModifier += spell->m_caster_level * 0.1f;
 		}
 		
-		spell = ARX_SPELLS_GetSpellOn(entity, SPELL_SLOW_DOWN);
+		spell = spells.getSpellOnTarget(entity, SPELL_SLOW_DOWN);
 		if(spell) {
 			speedModifier -= spell->m_caster_level * 0.05f;
 		}

src/game/Damage.cpp
@@ -766,7 +766,7 @@ float ARX_DAMAGES_DamageNPC(Entity * io, float dmg, EntityHandle source, long fl
 		SendIOScriptEvent(io, SM_OUCH, tex);
 		io->dmg_sum = 0.f;
 		
-		SpellBase * spell = ARX_SPELLS_GetSpellOn(io, SPELL_CONFUSE);
+		SpellBase * spell = spells.getSpellOnTarget(io, SPELL_CONFUSE);
 		if(spell)
 			spell->m_tolive = 0;
 	}

src/game/Equipment.cpp
@@ -557,7 +557,7 @@ float ARX_EQUIPMENT_ComputeDamages(Entity * io_source, Entity * io_target, float
 		
 		damages = io_source->_npcdata->damages * ratioaim * (rnd() * ( 1.0f / 2 ) + 0.5f);
 
-		SpellBase * spell = ARX_SPELLS_GetSpellOn(io_source, SPELL_CURSE);
+		SpellBase * spell = spells.getSpellOnTarget(io_source, SPELL_CURSE);
 		if(spell) {
 			damages *= (1 - spell->m_caster_level * 0.05f);
 		}
@@ -584,7 +584,7 @@ float ARX_EQUIPMENT_ComputeDamages(Entity * io_source, Entity * io_target, float
 		ac = ARX_INTERACTIVE_GetArmorClass(io_target);
 		absorb = io_target->_npcdata->absorb;
 		
-		SpellBase * spell = ARX_SPELLS_GetSpellOn(io_target, SPELL_CURSE);
+		SpellBase * spell = spells.getSpellOnTarget(io_target, SPELL_CURSE);
 		if(spell) {
 			float modif = (1 - spell->m_caster_level * 0.05f);
 			ac *= modif;

src/game/NPC.cpp
@@ -2801,7 +2801,7 @@ static void ManageNPCMovement(Entity * io)
 
 	CollisionFlags levitate = 0;
 
-	if(ARX_SPELLS_GetSpellOn(io, SPELL_LEVITATE)) {
+	if(spells.getSpellOnTarget(io, SPELL_LEVITATE)) {
 		levitate = CFLAG_LEVITATE;
 		io->physics.targetpos.y = io->pos.y + io->move.y + ForcedMove.y;
 	} else { // Gravity 'simulation'

src/game/Player.cpp
@@ -520,17 +520,17 @@ void ARX_PLAYER_ComputePlayerFullStats() {
 		
 		SpellBase * spell;
 		
-		spell = ARX_SPELLS_GetSpellOn(entities.player(), SPELL_ARMOR);
+		spell = spells.getSpellOnTarget(entities.player(), SPELL_ARMOR);
 		if(spell) {
 			player.m_miscMod.armorClass += spell->m_caster_level;
 		}
 		
-		spell = ARX_SPELLS_GetSpellOn(entities.player(), SPELL_LOWER_ARMOR);
+		spell = spells.getSpellOnTarget(entities.player(), SPELL_LOWER_ARMOR);
 		if(spell) {
 			player.m_miscMod.armorClass -= spell->m_caster_level;
 		}
 		
-		spell = ARX_SPELLS_GetSpellOn(entities.player(), SPELL_CURSE);
+		spell = spells.getSpellOnTarget(entities.player(), SPELL_CURSE);
 		if(spell) {
 			player.m_attributeMod.strength -= spell->m_caster_level;
 			player.m_attributeMod.constitution -= spell->m_caster_level;
@@ -538,7 +538,7 @@ void ARX_PLAYER_ComputePlayerFullStats() {
 			player.m_attributeMod.mind -= spell->m_caster_level;
 		}
 	
-		spell = ARX_SPELLS_GetSpellOn(entities.player(), SPELL_BLESS);
+		spell = spells.getSpellOnTarget(entities.player(), SPELL_BLESS);
 		if(spell) {
 			player.m_attributeMod.strength += spell->m_caster_level;
 			player.m_attributeMod.dexterity += spell->m_caster_level;
@@ -1667,7 +1667,7 @@ void ARX_PLAYER_Manage_Visual() {
 			ChangeMoveAnim = alist[ANIM_MEDITATION];
 			ChangeMA_Loop = true;
 			goto makechanges;
-		} else if(ARX_SPELLS_GetSpellOn(io, SPELL_LEVITATE)) {
+		} else if(spells.getSpellOnTarget(io, SPELL_LEVITATE)) {
 			ChangeMoveAnim = alist[ANIM_LEVITATE];
 			ChangeMA_Loop = true;
 			goto makechanges;
@@ -1851,7 +1851,7 @@ void ForcePlayerLookAtIO(Entity * io) {
  */
 void ARX_PLAYER_Frame_Update()
 {
-	if(ARX_SPELLS_GetSpellOn(entities.player(), SPELL_PARALYSE)) {
+	if(spells.getSpellOnTarget(entities.player(), SPELL_PARALYSE)) {
 		PLAYER_PARALYSED = 1;
 	} else {
 		entities.player()->ioflags &= ~IO_FREEZESCRIPT;
@@ -1908,7 +1908,7 @@ void ARX_PLAYER_Frame_Update()
 	player.TRAP_DETECT = player.m_skillFull.mecanism;
 	player.TRAP_SECRET = player.m_skillFull.intuition;
 
-	if(ARX_SPELLS_GetSpellOn(entities.player(), SPELL_DETECT_TRAP))
+	if(spells.getSpellOnTarget(entities.player(), SPELL_DETECT_TRAP))
 		player.TRAP_DETECT = 100.f;
 
 	ModeLight |= MODE_DEPTHCUEING;
@@ -1921,7 +1921,7 @@ void ARX_PLAYER_Frame_Update()
  */
 static void ARX_PLAYER_MakeStepNoise() {
 	
-	if(ARX_SPELLS_GetSpellOn(entities.player(), SPELL_LEVITATE)) {
+	if(spells.getSpellOnTarget(entities.player(), SPELL_LEVITATE)) {
 		return;
 	}
 	
@@ -2101,7 +2101,7 @@ void PlayerMovementIterate(float DeltaTime) {
 				if(anything < 0.f) {
 					player.physics.cyl.height = old;
 					
-					SpellBase * spell = ARX_SPELLS_GetSpellOn(entities.player(), SPELL_LEVITATE);
+					SpellBase * spell = spells.getSpellOnTarget(entities.player(), SPELL_LEVITATE);
 					if(spell) {
 						spell->m_tolive = 0;
 					}

src/game/Spells.h
@@ -76,6 +76,8 @@ public:
 	
 	void RequestEndOfInstanceForThisCaster(SpellType typ, EntityHandle caster);
 	bool ExistAnyInstanceForThisCaster(SpellType typ, EntityHandle caster);
+	
+	SpellBase * getSpellOnTarget(const Entity * target, SpellType type);
 		
 private:
 	SpellBase m_spells[MAX_SPELLS];
@@ -98,7 +100,6 @@ void ARX_SPELLS_Update();
 void ARX_SPELLS_ManageMagic();
 
 void ARX_SPELLS_RemoveAllSpellsOn(Entity * io);
-SpellBase * ARX_SPELLS_GetSpellOn(const Entity * io, SpellType spellid);
 
 void ARX_SPELLS_CancelSpellTarget();
 void ARX_SPELLS_LaunchSpellTarget(Entity * io);

commit 1204e9058cdf0f6ddb7811a356df69c50c34123f

Fix spells on player broken in 2c20ae48

src/game/Spells.cpp
@@ -232,7 +232,7 @@ bool SpellManager::ExistAnyInstanceForThisCaster(SpellType typ, EntityHandle cas
 
 SpellBase * SpellManager::getSpellOnTarget(EntityHandle target, SpellType type)
 {
-	if(!target)
+	if(target == InvalidEntityHandle)
 		return NULL;
 	
 	for(size_t i = 0; i < MAX_SPELLS; i++) {