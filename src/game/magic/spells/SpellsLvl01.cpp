488523eba1e033f26fafea563f472c4afad273fb
Eli2 <FabianRoth64@googlemail.com>
2014-07-05 22:08:07 +0200

    Indentation

--- src/game/magic/spells/SpellsLvl01.cpp
+++ src/game/magic/spells/SpellsLvl01.cpp
@@ -100,60 +100,60 @@ void MagicMissileSpell::Launch()
 	CMultiMagicMissile * effect = new CMultiMagicMissile(number, mrCheat);
 	effect->SetDuration(6000ul);
 	
-	{		
-		m_hand_group = GetActionPointIdx(entities[m_caster]->obj, "primary_attach");
+	{
+	m_hand_group = GetActionPointIdx(entities[m_caster]->obj, "primary_attach");
+	
+	if(m_hand_group != -1) {
+		Entity * caster = entities[m_caster];
+		long group = m_hand_group;
+		m_hand_pos = caster->obj->vertexlist3[group].v;
+	}
+	
+	Vec3f aePos;
+	float afAlpha, afBeta;
+	if(m_caster == PlayerEntityHandle) {
+		afBeta = player.angle.getPitch();
+		afAlpha = player.angle.getYaw();
+		Vec3f vector;
+		vector.x = -std::sin(radians(afBeta)) * std::cos(radians(afAlpha)) * 60.f;
+		vector.y = std::sin(radians(afAlpha)) * 60.f;
+		vector.z = std::cos(radians(afBeta)) * std::cos(radians(afAlpha)) * 60.f;
 		
 		if(m_hand_group != -1) {
-			Entity * caster = entities[m_caster];
-			long group = m_hand_group;
-			m_hand_pos = caster->obj->vertexlist3[group].v;
-		}
-		
-		Vec3f aePos;
-		float afAlpha, afBeta;
-		if(m_caster == PlayerEntityHandle) {
-			afBeta = player.angle.getPitch();
-			afAlpha = player.angle.getYaw();
-			Vec3f vector;
-			vector.x = -std::sin(radians(afBeta)) * std::cos(radians(afAlpha)) * 60.f;
-			vector.y = std::sin(radians(afAlpha)) * 60.f;
-			vector.z = std::cos(radians(afBeta)) * std::cos(radians(afAlpha)) * 60.f;
-			
-			if(m_hand_group != -1) {
-				aePos = m_hand_pos + vector;
-			} else {
-				aePos.x = player.pos.x - std::sin(radians(afBeta)) + vector.x; 
-				aePos.y = player.pos.y + vector.y; //;
-				aePos.z = player.pos.z + std::cos(radians(afBeta)) + vector.z; 
-			}
+			aePos = m_hand_pos + vector;
 		} else {
-			afAlpha = 0;
-			afBeta = entities[m_caster]->angle.getPitch();
-			Vec3f vector;
-			vector.x = -std::sin(radians(afBeta)) * std::cos(radians(afAlpha)) * 60;
-			vector.y =  std::sin(radians(afAlpha)) * 60;
-			vector.z =  std::cos(radians(afBeta)) * std::cos(radians(afAlpha)) * 60;
-			
-			if(m_hand_group != -1) {
-				aePos = m_hand_pos + vector;
-			} else {
-				aePos = entities[m_caster]->pos + vector;
-			}
-			
-			Entity * io = entities[m_caster];
-			
-			if(ValidIONum(io->targetinfo)) {
-				Vec3f * p1 = &m_caster_pos;
-				Vec3f * p2 = &entities[io->targetinfo]->pos;
-				afAlpha = -(degrees(getAngle(p1->y, p1->z, p2->y, p2->z + glm::distance(Vec2f(p2->x, p2->z), Vec2f(p1->x, p1->z))))); //alpha entre orgn et dest;
-			} else if (ValidIONum(m_target)) {
-				Vec3f * p1 = &m_caster_pos;
-				Vec3f * p2 = &entities[m_target]->pos;
-				afAlpha = -(degrees(getAngle(p1->y, p1->z, p2->y, p2->z + glm::distance(Vec2f(p2->x, p2->z), Vec2f(p1->x, p1->z))))); //alpha entre orgn et dest;
-			}
+			aePos.x = player.pos.x - std::sin(radians(afBeta)) + vector.x;
+			aePos.y = player.pos.y + vector.y; //;
+			aePos.z = player.pos.z + std::cos(radians(afBeta)) + vector.z;
+		}
+	} else {
+		afAlpha = 0;
+		afBeta = entities[m_caster]->angle.getPitch();
+		Vec3f vector;
+		vector.x = -std::sin(radians(afBeta)) * std::cos(radians(afAlpha)) * 60;
+		vector.y =  std::sin(radians(afAlpha)) * 60;
+		vector.z =  std::cos(radians(afBeta)) * std::cos(radians(afAlpha)) * 60;
+		
+		if(m_hand_group != -1) {
+			aePos = m_hand_pos + vector;
+		} else {
+			aePos = entities[m_caster]->pos + vector;
 		}
 		
-		effect->Create(aePos, afAlpha, afBeta);
+		Entity * io = entities[m_caster];
+		
+		if(ValidIONum(io->targetinfo)) {
+			Vec3f * p1 = &m_caster_pos;
+			Vec3f * p2 = &entities[io->targetinfo]->pos;
+			afAlpha = -(degrees(getAngle(p1->y, p1->z, p2->y, p2->z + glm::distance(Vec2f(p2->x, p2->z), Vec2f(p1->x, p1->z))))); //alpha entre orgn et dest;
+		} else if (ValidIONum(m_target)) {
+			Vec3f * p1 = &m_caster_pos;
+			Vec3f * p2 = &entities[m_target]->pos;
+			afAlpha = -(degrees(getAngle(p1->y, p1->z, p2->y, p2->z + glm::distance(Vec2f(p2->x, p2->z), Vec2f(p1->x, p1->z))))); //alpha entre orgn et dest;
+		}
+	}
+	
+	effect->Create(aePos, afAlpha, afBeta);
 	}
 	
 	m_pSpellFx = effect;
