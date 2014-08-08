13f31f1a5805f7a8aa192ed75c0b4c59d8627235
Eli2 <FabianRoth64@googlemail.com>
2014-08-09 00:05:38 +0200

    Remove unused declarations

--- src/core/Core.h
+++ src/core/Core.h
@@ -119,10 +119,8 @@ extern long		CHANGE_LEVEL_ICON;
 void SetEditMode(long ed, const bool stop_sound = true);
 
 void SendGameReadyMsg();
-void DanaeSwitchFullScreen();
 void ARX_SetAntiAliasing();
 void DANAE_StartNewQuest();
-bool HandleGameFlowTransitions();
 void DanaeRestoreFullScreen();
 bool AdjustUI();
 
