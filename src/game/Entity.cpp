b84077afdc3de8b46eed75e98e3aa7b53207d406
Daniel Scharrer <daniel@constexpr.org>
2014-09-27 18:22:35 +0200

    Use new EntityId constructor in Entity

--- src/game/Entity.cpp
+++ src/game/Entity.cpp
@@ -75,9 +75,9 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 extern Entity * pIOChangeWeapon;
 
 Entity::Entity(const res::path & classPath, EntityInstance instance)
-	: m_index(size_t(-1)),
-	  m_id(classPath.filename(), instance),
-	  m_classPath(classPath)
+	: m_index(size_t(-1))
+	, m_id(classPath, instance)
+	, m_classPath(classPath)
 {
 	
 	m_index = entities.add(this);
