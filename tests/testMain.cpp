958b1d7793128d920624d76b490e77850610f6bc
Eli2 <FabianRoth64@googlemail.com>
2014-10-19 05:03:34 +0200

    Remove broken tests

--- tests/testMain.cpp
+++ tests/testMain.cpp
@@ -23,7 +23,6 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include "graphics/ColorTest.h"
-#include "graphics/GraphicsUtilityTest.h"
 #include "math/LegacyMathTest.h"
 
 int main(int argc, char *argv[]) {
