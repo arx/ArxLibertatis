6fed9cd33eb0e101ecab4791c1ad9ae1f8f05ce2
Daniel Scharrer <daniel@constexpr.org>
2014-12-21 12:39:40 +0100

    Remove unused function prototype

--- src/graphics/data/Mesh.h
+++ src/graphics/data/Mesh.h
@@ -171,7 +171,6 @@ Vec2f getWaterFxUvOffset(const Vec3f & odtv, float power);
 long EERIERTPPoly(EERIEPOLY *ep);
 
 float PtIn2DPolyProj(EERIE_3DOBJ * obj,EERIE_FACE * ef, float x, float z);
-float PtIn2DPolyProjV2(EERIE_3DOBJ * obj,EERIE_FACE * ef, float x, float z);
 
 long GetVertexPos(Entity * io,long id,Vec3f * pos);
 long CountBkgVertex();
