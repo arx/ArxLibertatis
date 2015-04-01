4d74c5c23bd94ddc509623070503a1ea0b3a2793
Eli2 <FabianRoth64@googlemail.com>
2015-04-01 22:28:08 +0200

    Replace componentwise operations

--- src/script/Script.cpp
+++ src/script/Script.cpp
@@ -1794,10 +1794,9 @@ static long Manage_Specific_RAT_Timer(SCR_TIMER * st) {
 	if (ARX_INTERACTIVE_ConvertToValidPosForIO(io, &target))
 	{
 		ARX_INTERACTIVE_Teleport(io, target);
-		Vec3f pos;
-		pos.x = io->pos.x;
-		pos.y = io->pos.y + io->physics.cyl.height * ( 1.0f / 2 );
-		pos.z = io->pos.z;
+		Vec3f pos = io->pos;
+		pos.y += io->physics.cyl.height * ( 1.0f / 2 );
+		
 		ARX_PARTICLES_Add_Smoke(&pos, 3, 20);
 		AddRandomSmoke(io, 20);
 		MakeCoolFx(io->pos);
