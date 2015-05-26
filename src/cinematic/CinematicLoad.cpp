fd8b73d29b0b1c2014508202851dded2b6fe340a
Daniel Scharrer <daniel@constexpr.org>
2015-05-26 09:38:38 +0200

    Fix cinematic light effect with wide screens
    Work around defects in cinematics that were authored for 4:3 screens.

--- src/cinematic/CinematicLoad.cpp
+++ src/cinematic/CinematicLoad.cpp
@@ -314,6 +314,24 @@ bool parseCinematic(Cinematic * c, const char * data, size_t size) {
 			k.force = 1;
 		}
 		
+		// The cinematics were authored for 4:3 - if the light only fades off
+		// outside of that region, it should never fade off.
+		// This fixes ugly transitions when fallin and fallout are close together.
+		{
+			
+			float f = std::min(k.light.fallin, k.light.fallout);
+			
+			float d0 = glm::distance(Vec2f(k.light.pos), Vec2f(-320.f, -240.f));
+			float d1 = glm::distance(Vec2f(k.light.pos), Vec2f( 320.f, -240.f));
+			float d2 = glm::distance(Vec2f(k.light.pos), Vec2f( 320.f,  240.f));
+			float d3 = glm::distance(Vec2f(k.light.pos), Vec2f(-320.f,  240.f));
+			
+			if(f > std::max(std::max(d0, d1), std::max(d2, d3))) {
+				k.light.fallin = k.light.fallout = std::numeric_limits<float>::max() / 3;
+			}
+			
+		}
+		
 		AddKeyLoad(k);
 		
 		LogDebug(" - " << i << ": frame " << k.frame << " image: " << k.numbitmap);
