d0134c2bbf0cc5b7ae2fae0d24a76e541d93294c
Daniel Scharrer <daniel@constexpr.org>
2015-06-23 18:14:17 +0200

    Fix alignment of Cinematic on Windows

--- src/cinematic/Cinematic.h
+++ src/cinematic/Cinematic.h
@@ -47,10 +47,11 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include <stddef.h>
 #include <vector>
 
+#include "game/Camera.h"
 #include "graphics/Color.h"
 #include "math/RandomFlicker.h"
 #include "math/Vector.h"
-#include "game/Camera.h"
+#include "platform/Alignment.h"
 
 struct C_KEY;
 struct CinematicGrid;
@@ -141,9 +142,14 @@ public:
 
 private:
 	EERIE_CAMERA	m_camera;
+public:
+	
+	ARX_USE_ALIGNED_NEW(Cinematic) // for m_camera
 };
 
 void DrawGrille(CinematicBitmap * bitmap, Color col, int fx, CinematicLight * light,
                 Vec3f * posgrillesuiv, float angzgrillesuiv, const CinematicFadeOut & fade);
+
+ARX_USE_ALIGNED_ALLOCATOR(Cinematic) // for m_camera
 
 #endif // ARX_CINEMATIC_CINEMATIC_H
