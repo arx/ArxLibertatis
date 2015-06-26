cc71bdf46595d3f7d1b76cd99117c4a6973fbf2c
Eli2 <FabianRoth64@googlemail.com>
2015-06-27 01:58:06 +0200

    Replace pointer with reference

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1389,12 +1389,12 @@ void ARX_PLAYER_Manage_Visual() {
 	
 	ANIM_USE & ause0 = io->animlayer[0];
 	ANIM_USE & ause1 = io->animlayer[1];
-	ANIM_USE * ause2 = &io->animlayer[2];
+	ANIM_USE & ause2 = io->animlayer[2];
 	ANIM_USE * ause3 = &io->animlayer[3];
 	
 	ause0.next_anim = NULL;
 	ause1.next_anim = NULL;
-	ause2->next_anim = NULL;
+	ause2.next_anim = NULL;
 	ause3->next_anim = NULL;
 	
 	ANIM_HANDLE ** alist = io->anims;
