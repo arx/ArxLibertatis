62b5bf9407e49c590621e327d9db4be9fd245d46
Eli2 <FabianRoth64@googlemail.com>
2015-07-19 01:14:45 +0200

    Scale player gold text position

--- src/gui/Hud.cpp
+++ src/gui/Hud.cpp
@@ -658,7 +658,7 @@ public:
 		
 		if(m_isSelected) {
 			Vec2f numberPos = m_rect.topLeft();
-			numberPos += Vec2f(- INTERFACE_RATIO(30), + INTERFACE_RATIO(10 - 25));
+			numberPos += Vec2f(-30 * m_scale, -15 * m_scale);
 			
 			ARX_INTERFACE_DrawNumber(numberPos, player.gold, 6, Color::white, m_scale);
 		}
