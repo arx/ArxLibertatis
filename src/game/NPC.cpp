14e270701f2187410d8c552cce4dc601af63c52d
Eli2 <FabianRoth64@googlemail.com>
2015-08-16 19:33:33 +0200

    Rename variable

--- src/game/NPC.cpp
+++ src/game/NPC.cpp
@@ -1466,10 +1466,10 @@ static void ARX_NPC_Manage_Fight(Entity * io) {
 
 static void ARX_NPC_Manage_Anims_End(Entity * io) {
 	
-	AnimLayer * ause = &io->animlayer[0];
-	if((ause->flags & EA_ANIMEND) && ause->cur_anim) {
+	AnimLayer * layer0 = &io->animlayer[0];
+	if((layer0->flags & EA_ANIMEND) && layer0->cur_anim) {
 		
-		if(ause->flags & EA_FORCEPLAY) {
+		if(layer0->flags & EA_FORCEPLAY) {
 			bool startAtBeginning = (io->_npcdata->behavior & BEHAVIOUR_FRIENDLY) != 0;
 			changeAnimation(io, ANIM_DEFAULT, 0, startAtBeginning);
 		}
