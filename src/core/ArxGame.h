ad119cd0dedf6f170d8bfdcb8772c27c70319e94
dreamer.dead <dreamer.dead@gmail.com>
2015-11-01 11:26:05 +0300

    Cleanup includes in ArxGame.{h,cpp} files
    Remove unused includes.
    Sort and group includes in .cpp file.
    Rename ArxGame::wasResized member variable.

--- src/core/ArxGame.h
+++ src/core/ArxGame.h
@@ -44,14 +44,11 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #ifndef ARX_CORE_ARXGAME_H
 #define ARX_CORE_ARXGAME_H
 
-#include <string>
-
 #include "core/Application.h"
-#include "graphics/Renderer.h"
-#include "window/Window.h"
-#include "window/RenderWindow.h"
 
-class Font;
+#include "graphics/Renderer.h"
+
+#include "window/Window.h"
 
 class ArxGame : public Application, public Window::Listener, public Renderer::Listener {
 	
@@ -71,7 +68,7 @@ protected:
 	
 	void doFrame();
 	void update();
-	void render();	
+	void render();
 
 	void manageKeyMouse();
 	void manageEditorControls();
@@ -80,7 +77,7 @@ protected:
 
 	void update2DFX();
 	void goFor2DFX();
-		
+	
 public:
 	
 	ArxGame();
@@ -91,20 +88,20 @@ public:
 private:
 	void updateTime();
 	void updateInput();
-
+	
 	// Camera stuff
 	void updateFirstPersonCamera();
 	void speechControlledCinematic();
 	void handlePlayerDeath();
 	void updateActiveCamera();
-
+	
 	bool isInMenu() const;
-
+	
 	void renderMenu();
-
+	
 	void updateLevel();
 	void renderLevel();
-
+	
 	
 	virtual void onWindowGotFocus(const Window & window);
 	virtual void onWindowLostFocus(const Window & window);
@@ -112,7 +109,7 @@ private:
 	virtual void onDestroyWindow(const Window & window);
 	virtual void onToggleFullscreen(const Window & window);
 	
-	bool wasResized;
+	bool m_wasResized;
 	
 	void onRendererInit(Renderer &);
 	void onRendererShutdown(Renderer &);
