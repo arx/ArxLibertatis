72e6fadf81cb1d80b0defc573b321e6d70314c53
Eli2 <FabianRoth64@googlemail.com>
2016-01-02 20:10:44 +0100

    Add temporary variable

--- src/graphics/data/MeshManipulation.cpp
+++ src/graphics/data/MeshManipulation.cpp
@@ -546,9 +546,11 @@ static EERIE_3DOBJ * CreateIntermediaryMesh(const EERIE_3DOBJ * obj1, const EERI
 
 	// Re-Creating sel_head
 	if(tw == TWEAK_HEAD) {
-		for(size_t l = 0; l < obj2->selections[sel_head2.handleData()].selected.size(); l++) {
+		const EERIE_SELECTIONS & sel = obj2->selections[sel_head2.handleData()];
+		
+		for(size_t l = 0; l < sel.selected.size(); l++) {
 			EERIE_VERTEX temp;
-			temp.v = obj2vertexlist2[obj2->selections[sel_head2.handleData()].selected[l]].v;
+			temp.v = obj2vertexlist2[sel.selected[l]].v;
 			long t = GetEquivalentVertex(work, &temp);
 
 			if(t != -1) {
