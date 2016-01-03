c45e796c4b43714462917f3b77b3da2ea9a2004d
Daniel Scharrer <daniel@constexpr.org>
2016-01-03 12:44:30 +0100

    HTTPClientWindows: Avoid unnecessary memset

--- tools/crashreporter/tbg/HTTPClientWindows.cpp
+++ tools/crashreporter/tbg/HTTPClientWindows.cpp
@@ -157,13 +157,13 @@ Response * WinHTTPSession::receive(HINTERNET wrequest, const Request & request,
 		                    WINHTTP_NO_OUTPUT_BUFFER, &urlSize, WINHTTP_NO_HEADER_INDEX);
 		if(GetLastError() ==  ERROR_INSUFFICIENT_BUFFER && urlSize % sizeof(WCHAR) == 0) {
 			platform::WideString redirect;
-			redirect.resize(urlSize / sizeof(WCHAR));
+			redirect.allocate(urlSize / sizeof(WCHAR));
 			if(WinHttpQueryHeaders(wrequest, WINHTTP_QUERY_LOCATION, WINHTTP_HEADER_NAME_BY_INDEX,
 			                       redirect.data(), &urlSize, WINHTTP_NO_HEADER_INDEX)) {
 				redirect.resize(urlSize / sizeof(WCHAR));
 				platform::WideString base(request.url());
 				platform::WideString wurl;
-				wurl.resize(2 * (base.size() + redirect.size()));
+				wurl.allocate(2 * (base.size() + redirect.size()));
 				urlSize = wurl.size();
 				if(InternetCombineUrlW(base, redirect, wurl.data(), &urlSize, ICU_BROWSER_MODE)) {
 					wurl.resize(urlSize);
