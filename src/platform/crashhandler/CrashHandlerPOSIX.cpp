cc4f8f32c4be5ae808686e469df1cac891136ddf
Daniel Scharrer <daniel@constexpr.org>
2016-01-10 22:15:27 +0100

    CrashHandlerPOSIX: Fix unused variable warning on non-x86 Linux
    This implements register printing on ARM-Linux.

--- src/platform/crashhandler/CrashHandlerPOSIX.cpp
+++ src/platform/crashhandler/CrashHandlerPOSIX.cpp
@@ -449,6 +449,15 @@ void CrashHandlerPOSIX::handleCrash(int signal, void * info, void * context) {
 		m_pCrashInfo->hasAddress = true;
 		m_pCrashInfo->stack = ctx->uc_mcontext.gregs[REG_RSP];
 		m_pCrashInfo->hasStack = true;
+		#elif ARX_ARCH == ARX_ARCH_ARM
+		m_pCrashInfo->address =  ctx->uc_mcontext.arm_pc;
+		m_pCrashInfo->hasAddress = true;
+		m_pCrashInfo->stack =  ctx->uc_mcontext.arm_sp;
+		m_pCrashInfo->hasStack = true;
+		m_pCrashInfo->frame = ctx->uc_mcontext.arm_fp;
+		m_pCrashInfo->hasFrame = true;
+		#else
+		ARX_UNUSED(ctx);
 		#endif
 	}
 	#else
