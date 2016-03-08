012bfb5021540f6dd2f21f886110d05dc76a5393
Eli2 <FabianRoth64@googlemail.com>
2016-03-08 03:55:01 +0100

    Use const unsigned long

--- src/scene/ChangeLevel.cpp
+++ src/scene/ChangeLevel.cpp
@@ -1114,7 +1114,7 @@ static long ARX_CHANGELEVEL_Push_IO(const Entity * io, long level) {
 	memcpy(dat, &ais, sizeof(ARX_CHANGELEVEL_IO_SAVE));
 	pos += sizeof(ARX_CHANGELEVEL_IO_SAVE);
 
-	long timm = arxtime.now_ul();
+	const unsigned long timm = arxtime.now_ul();
 
 	for(int i = 0; i < MAX_TIMER_SCRIPT; i++) {
 		if(scr_timer[i].exist) {
