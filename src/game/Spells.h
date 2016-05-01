7ffab59de7608235c4213cd2e1d3d4908dec74fa
XoD <xoddark@gmail.com>
2016-05-01 17:11:17 +0200

    Clean: remove space or tab at eol in Spells.

--- src/game/Spells.h
+++ src/game/Spells.h
@@ -21,22 +21,22 @@
 ARX FATALIS GPL Source Code
 Copyright (C) 1999-2010 Arkane Studios SA, a ZeniMax Media company.
 
-This file is part of the Arx Fatalis GPL Source Code ('Arx Fatalis Source Code'). 
+This file is part of the Arx Fatalis GPL Source Code ('Arx Fatalis Source Code').
 
-Arx Fatalis Source Code is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
+Arx Fatalis Source Code is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-Arx Fatalis Source Code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+Arx Fatalis Source Code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Arx Fatalis Source Code.  If not, see 
+You should have received a copy of the GNU General Public License along with Arx Fatalis Source Code.  If not, see
 <http://www.gnu.org/licenses/>.
 
-In addition, the Arx Fatalis Source Code is also subject to certain additional terms. You should have received a copy of these 
-additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the Arx 
+In addition, the Arx Fatalis Source Code is also subject to certain additional terms. You should have received a copy of these
+additional terms immediately following the terms and conditions of the GNU General Public License which accompanied the Arx
 Fatalis Source Code. If not, please request a copy in writing from Arkane Studios at the address below.
 
-If you have questions concerning this license or the applicable additional terms, you may contact in writing Arkane Studios, c/o 
+If you have questions concerning this license or the applicable additional terms, you may contact in writing Arkane Studios, c/o
 ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 ===========================================================================
 */
@@ -68,35 +68,35 @@ class TextureContainer;
 const size_t MAX_SPELLS = 20;
 
 class SpellManager {
-	
+
 public:
 	void init();
 	void clearAll();
-	
+
 	SpellBase * operator[] (const SpellHandle handle);
-	
+
 	void endSpell(SpellBase * spell);
-	
+
 	void endByCaster(EntityHandle caster);
 	void endByCaster(EntityHandle caster, SpellType type);
-	
+
 	void endByTarget(EntityHandle target, SpellType type);
 	void endByType(SpellType type);
-	
+
 	bool ExistAnyInstanceForThisCaster(SpellType typ, EntityHandle caster);
-	
+
 	SpellBase * getSpellByCaster(EntityHandle caster, SpellType type);
 	SpellBase * getSpellOnTarget(EntityHandle target, SpellType type);
-	
+
 	void replaceCaster(EntityHandle oldCaster, EntityHandle newCaster);
 	void removeTarget(Entity *io);
-	
+
 	bool hasFreeSlot();
 	void addSpell(SpellBase * spell);
 	void freeSlot(SpellBase * spell);
-	
+
 	SpellHandle create();
-	
+
 private:
 	SpellBase * m_spells[MAX_SPELLS];
 };
