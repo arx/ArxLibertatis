3498a858483bdb657eb67ced4eaaebb36947f266
Eli2 <FabianRoth64@googlemail.com>
2016-05-21 03:13:21 +0200

    Reorder calculations

--- src/cinematic/CinematicKeyframer.cpp
+++ src/cinematic/CinematicKeyframer.cpp
@@ -605,18 +605,19 @@ consequences on light :
 			const float f3 = t3 - t2;
 
 			const float tempx = next->pos.x;
-			const float p0x = 0.5f * (tempx - kprec->pos.x);
-			const float p1x = 0.5f * (ksuivsuiv->pos.x - current->pos.x);
-			c->pos.x = f0 * current->pos.x + f1 * tempx + f2 * p0x + f3 * p1x;
-
 			const float tempy = next->pos.y;
-			const float p0y = 0.5f * (tempy - kprec->pos.y);
-			const float p1y = 0.5f * (ksuivsuiv->pos.y - current->pos.y);
-			c->pos.y = f0 * current->pos.y + f1 * tempy + f2 * p0y + f3 * p1y;
-
 			const float tempz = next->pos.z;
+			
+			const float p0x = 0.5f * (tempx - kprec->pos.x);
+			const float p0y = 0.5f * (tempy - kprec->pos.y);
 			const float p0z = 0.5f * (tempz - kprec->pos.z);
+			
+			const float p1x = 0.5f * (ksuivsuiv->pos.x - current->pos.x);
+			const float p1y = 0.5f * (ksuivsuiv->pos.y - current->pos.y);
 			const float p1z = 0.5f * (ksuivsuiv->pos.z - current->pos.z);
+			
+			c->pos.x = f0 * current->pos.x + f1 * tempx + f2 * p0x + f3 * p1x;
+			c->pos.y = f0 * current->pos.y + f1 * tempy + f2 * p0y + f3 * p1y;
 			c->pos.z = f0 * current->pos.z + f1 * tempz + f2 * p0z + f3 * p1z;
 
 			c->angz = current->angz + a * GetAngleInterpolation(current->angz, next->angz);
