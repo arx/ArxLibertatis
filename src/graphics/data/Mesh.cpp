80158c08296eb331488543a8f7c2764a081013ab
Daniel Scharrer <daniel@constexpr.org>
2016-06-04 21:25:22 +0200

    Combine dot products

--- src/graphics/data/Mesh.cpp
+++ src/graphics/data/Mesh.cpp
@@ -423,9 +423,7 @@ bool GetTruePolyY(const EERIEPOLY * ep, const Vec3f & pos, float * ret) {
 		return false;
 	}
 	
-	float y = glm::dot(ep->v[0].p, n);
-	
-	y = (y - glm::dot(n, Vec3f(pos.x, 0.f, pos.z))) / n.y;
+	float y = glm::dot(ep->v[0].p - Vec3f(pos.x, 0.f, pos.z), n) / n.y;
 	
 	// Perhaps we can remove the clamp... (need to test)
 	*ret = glm::clamp(y, ep->min.y, ep->max.y);
