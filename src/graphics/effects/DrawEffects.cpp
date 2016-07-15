990ed7ec895693dc9f85a3436c0ef42b2d11f19c
Eli2 <FabianRoth64@googlemail.com>
2016-07-15 16:11:56 +0200

    Indentation

--- src/graphics/effects/DrawEffects.cpp
+++ src/graphics/effects/DrawEffects.cpp
@@ -402,15 +402,15 @@ void ARXDRAW_DrawPolyBoom() {
 				float fCalc = pb.timecreation - g_framedelay;
 				pb.timecreation = checked_range_cast<unsigned long>(fCalc);
 			}
-
+			
 			if(pb.timecreation - g_framedelay > 0) {
 				float fCalc =  pb.timecreation - g_framedelay;
 				pb.timecreation = checked_range_cast<unsigned long>(fCalc);
 			}
 		}
-
+		
 		float t = (float)pb.timecreation + (float)pb.tolive - (float)now;
-
+		
 		if(t <= 0) {
 			std::swap(polyboom[i], polyboom.back());
 			polyboom.pop_back();
@@ -423,10 +423,10 @@ void ARXDRAW_DrawPolyBoom() {
 	mat.setDepthBias(8);
 	mat.setLayer(RenderMaterial::Decal);
 	mat.setWrapMode(TextureStage::WrapClamp);
-
+	
 	std::vector<POLYBOOM>::iterator itr = polyboom.begin();
 	while (itr != polyboom.end()) {
-
+		
 		POLYBOOM & pb = *itr;
 		
 		float t = (float)pb.timecreation + (float)pb.tolive - (float)now;
@@ -436,116 +436,116 @@ void ARXDRAW_DrawPolyBoom() {
 		
 		switch(typp) {
 			
-		case 0: { // Scorch mark
-			
-			float tt = t / (float)pb.tolive * 0.8f;
-			ColorRGBA col = (player.m_improve ? (Color3f::red * (tt*.5f)) : Color3f::gray(tt)).toRGB();
-			
-			TexturedVertexUntransformed ltv[4];
-			IncrementPolyWithNormalOutput(pb.ep,ltv);
-			
-			for(long k = 0; k < pb.nbvert; k++) {
-				ltv[k].p = ltv[k].p;
-				ltv[k].uv.x=pb.u[k];
-				ltv[k].uv.y=pb.v[k];
-				ltv[k].color = col;
-			}
-			
-			if(player.m_improve) {
-				mat.setBlendType(RenderMaterial::Additive);
-			} else {
-				mat.setBlendType(RenderMaterial::Subtractive);
-			}
-			mat.setTexture(Boom);
-			
-			drawTriangle(mat, &ltv[0]);
-			if(pb.nbvert & 4) {
-				drawTriangle(mat, &ltv[1]);
-			}
-			
-			break;
-		}
-		
-		case 1: { // Blood
-			
-			float div = 1.f / (float)pb.tolive;
-			float tt = t * div;
-			float tr = std::max(1.f, tt * 2 - 0.5f);
-			ColorRGBA col = (pb.rgb * tt).toRGB(glm::clamp(tt * 1.5f, 0.f, 1.f) * 255);
-			
-			TexturedVertexUntransformed ltv[4];
-			IncrementPolyWithNormalOutput(pb.ep, ltv);
-			
-			for(long k = 0; k < pb.nbvert; k++) {
-				ltv[k].p = ltv[k].p;
-				ltv[k].uv.x=(pb.u[k]-0.5f)*(tr)+0.5f;
-				ltv[k].uv.y=(pb.v[k]-0.5f)*(tr)+0.5f;
-				ltv[k].color = col;
-			}
-			
-			mat.setBlendType(RenderMaterial::Subtractive2);
-			mat.setTexture(pb.tc);
-			
-			drawTriangle(mat, &ltv[0]);
-			if(pb.nbvert & 4) {
-				drawTriangle(mat, &ltv[1]);
-			}
-			
-			break;
-		}
-		
-		case 2: { // Water
-			
-			float div = 1.f / (float)pb.tolive;
-			float tt = t * div;
-			float tr = std::max(1.f, tt * 2 - 0.5f);
-			float ttt = tt * 0.5f;
-			ColorRGBA col = (pb.rgb * ttt).toRGB();
-			
-			TexturedVertexUntransformed ltv[4];
-			IncrementPolyWithNormalOutput(pb.ep,ltv);
-			
-			for(long k = 0; k < pb.nbvert; k++) {
-				ltv[k].p = ltv[k].p;
-				ltv[k].uv.x=(pb.u[k]-0.5f)*(tr)+0.5f;
-				ltv[k].uv.y=(pb.v[k]-0.5f)*(tr)+0.5f;
-				ltv[k].color=col;
-			}
-
-			if (	(ltv[0].uv.x<0.f)
-				&&	(ltv[1].uv.x<0.f)
-				&&	(ltv[2].uv.x<0.f)
-				&&	(ltv[3].uv.x<0.f) )
+			case 0: { // Scorch mark
+				
+				float tt = t / (float)pb.tolive * 0.8f;
+				ColorRGBA col = (player.m_improve ? (Color3f::red * (tt*.5f)) : Color3f::gray(tt)).toRGB();
+				
+				TexturedVertexUntransformed ltv[4];
+				IncrementPolyWithNormalOutput(pb.ep,ltv);
+				
+				for(long k = 0; k < pb.nbvert; k++) {
+					ltv[k].p = ltv[k].p;
+					ltv[k].uv.x=pb.u[k];
+					ltv[k].uv.y=pb.v[k];
+					ltv[k].color = col;
+				}
+				
+				if(player.m_improve) {
+					mat.setBlendType(RenderMaterial::Additive);
+				} else {
+					mat.setBlendType(RenderMaterial::Subtractive);
+				}
+				mat.setTexture(Boom);
+				
+				drawTriangle(mat, &ltv[0]);
+				if(pb.nbvert & 4) {
+					drawTriangle(mat, &ltv[1]);
+				}
+				
 				break;
-
-			if (	(ltv[0].uv.y<0.f)
-				&&	(ltv[1].uv.y<0.f)
-				&&	(ltv[2].uv.y<0.f)
-				&&	(ltv[3].uv.y<0.f) )
-				break;
-
-			if (	(ltv[0].uv.x>1.f)
-				&&	(ltv[1].uv.x>1.f)
-				&&	(ltv[2].uv.x>1.f)
-				&&	(ltv[3].uv.x>1.f) )
-				break;
-
-			if (	(ltv[0].uv.y>1.f)
-				&&	(ltv[1].uv.y>1.f)
-				&&	(ltv[2].uv.y>1.f)
-				&&	(ltv[3].uv.y>1.f) )
-				break;
-			
-			mat.setBlendType(RenderMaterial::Screen);
-			mat.setTexture(pb.tc);
-			
-			drawTriangle(mat, &ltv[0]);
-			if(pb.nbvert & 4) {
-				drawTriangle(mat, &ltv[1]);
 			}
 			
-			break;
-		}
+			case 1: { // Blood
+				
+				float div = 1.f / (float)pb.tolive;
+				float tt = t * div;
+				float tr = std::max(1.f, tt * 2 - 0.5f);
+				ColorRGBA col = (pb.rgb * tt).toRGB(glm::clamp(tt * 1.5f, 0.f, 1.f) * 255);
+				
+				TexturedVertexUntransformed ltv[4];
+				IncrementPolyWithNormalOutput(pb.ep, ltv);
+				
+				for(long k = 0; k < pb.nbvert; k++) {
+					ltv[k].p = ltv[k].p;
+					ltv[k].uv.x=(pb.u[k]-0.5f)*(tr)+0.5f;
+					ltv[k].uv.y=(pb.v[k]-0.5f)*(tr)+0.5f;
+					ltv[k].color = col;
+				}
+				
+				mat.setBlendType(RenderMaterial::Subtractive2);
+				mat.setTexture(pb.tc);
+				
+				drawTriangle(mat, &ltv[0]);
+				if(pb.nbvert & 4) {
+					drawTriangle(mat, &ltv[1]);
+				}
+				
+				break;
+			}
+			
+			case 2: { // Water
+				
+				float div = 1.f / (float)pb.tolive;
+				float tt = t * div;
+				float tr = std::max(1.f, tt * 2 - 0.5f);
+				float ttt = tt * 0.5f;
+				ColorRGBA col = (pb.rgb * ttt).toRGB();
+				
+				TexturedVertexUntransformed ltv[4];
+				IncrementPolyWithNormalOutput(pb.ep,ltv);
+				
+				for(long k = 0; k < pb.nbvert; k++) {
+					ltv[k].p = ltv[k].p;
+					ltv[k].uv.x=(pb.u[k]-0.5f)*(tr)+0.5f;
+					ltv[k].uv.y=(pb.v[k]-0.5f)*(tr)+0.5f;
+					ltv[k].color=col;
+				}
+				
+				if (	(ltv[0].uv.x<0.f)
+					&&	(ltv[1].uv.x<0.f)
+					&&	(ltv[2].uv.x<0.f)
+					&&	(ltv[3].uv.x<0.f) )
+					break;
+				
+				if (	(ltv[0].uv.y<0.f)
+					&&	(ltv[1].uv.y<0.f)
+					&&	(ltv[2].uv.y<0.f)
+					&&	(ltv[3].uv.y<0.f) )
+					break;
+				
+				if (	(ltv[0].uv.x>1.f)
+					&&	(ltv[1].uv.x>1.f)
+					&&	(ltv[2].uv.x>1.f)
+					&&	(ltv[3].uv.x>1.f) )
+					break;
+				
+				if (	(ltv[0].uv.y>1.f)
+					&&	(ltv[1].uv.y>1.f)
+					&&	(ltv[2].uv.y>1.f)
+					&&	(ltv[3].uv.y>1.f) )
+					break;
+				
+				mat.setBlendType(RenderMaterial::Screen);
+				mat.setTexture(pb.tc);
+				
+				drawTriangle(mat, &ltv[0]);
+				if(pb.nbvert & 4) {
+					drawTriangle(mat, &ltv[1]);
+				}
+				
+				break;
+			}
 		}
 		
 		++itr;
