0e47a83036dd4ff0775f74de76768ee67e75b508
bsxf47 <bsxf47@gmail.com>
2016-10-18 20:24:17 +0200

    Prevent adding duplicate mouse positions

--- src/game/magic/SpellRecognition.cpp
+++ src/game/magic/SpellRecognition.cpp
@@ -189,6 +189,11 @@ void spellRecognitionPointsReset() {
 
 // Adds a 2D point to currently drawn spell symbol
 void ARX_SPELLS_AddPoint(const Vec2s & pos) {
+	
+	if(plist.size() && Vec2f(pos) == plist.back()) {
+		return;
+	}
+
 	if(plist.size() == MAX_POINTS) {
 		plist.pop_back();
 	}
