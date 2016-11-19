903a44bc4815ec5864358881b400e339b9826dd6
Daniel Scharrer <daniel@constexpr.org>
2016-11-19 20:01:24 +0100

    LegacyMathTest: Fix warnings for functions wihout prototypes

--- tests/math/LegacyMathTest.cpp
+++ tests/math/LegacyMathTest.cpp
@@ -264,7 +264,7 @@ void LegacyMathTest::cameraRotationTest() {
 }
 
 // TODO copy-paste
-Vec2s inventorySizeFromTextureSize(Vec2i size) {
+static Vec2s inventorySizeFromTextureSize(Vec2i size) {
 	return Vec2s(glm::clamp((size + Vec2i(31, 31)) / Vec2i(32, 32), Vec2i(1, 1), Vec2i(3, 3)));
 }
 
