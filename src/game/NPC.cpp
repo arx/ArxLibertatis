arx/ArxLibertatis

commit c913b52676931c020526b5069d717250970a8344

Replace uses of distSqr() with glm::distance2()
Or use closerThan() / fartherThan() if applicable.

src/game/NPC.cpp
@@ -57,6 +57,8 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 
 #include <boost/algorithm/string/predicate.hpp>
 
+#include <glm/gtx/norm.hpp>
+
 #include "animation/Animation.h"
 
 #include "ai/Paths.h"
@@ -459,7 +461,7 @@ long ARX_NPC_GetNextAttainableNodeIncrement(Entity * io)
 	if(!(io->ioflags & IO_NPC) || (io->_npcdata->behavior & BEHAVIOUR_WANDER_AROUND))
 		return 0;
 
-	float dists = distSqr(io->pos, ACTIVECAM->orgTrans.pos);
+	float dists = glm::distance2(io->pos, ACTIVECAM->orgTrans.pos);
 
 	if (dists > square(ACTIVECAM->cdepth) * square(1.0f / 2))
 		return 0;
@@ -506,7 +508,7 @@ long ARX_NPC_GetNextAttainableNodeIncrement(Entity * io)
 		if(io->physics.startpos == io->physics.targetpos
 		        || ((ARX_COLLISION_Move_Cylinder(&phys, io, 40, CFLAG_JUST_TEST | CFLAG_NPC))))
 		{
-			if(distSqr(phys.cyl.origin, ACTIVEBKG->anchors[pos].pos) < square(30.f)) {
+			if(closerThan(phys.cyl.origin, ACTIVEBKG->anchors[pos].pos, 30.f)) {
 				return l_try;
 			}
 		}
@@ -528,7 +530,7 @@ static long AnchorData_GetNearest(Vec3f * pos, EERIE_CYLINDER * cyl, long except
 			continue;
 
 		if(eb->anchors[i].nblinked) {
-			float d = distSqr(eb->anchors[i].pos, *pos);
+			float d = glm::distance2(eb->anchors[i].pos, *pos);
 
 			if ((d < distmax) && (eb->anchors[i].height <= cyl->height)
 			        && (eb->anchors[i].radius >= cyl->radius)
@@ -633,9 +635,9 @@ bool ARX_NPC_LaunchPathfind(Entity * io, long target)
 	
 	io->_npcdata->pathfind.truetarget = target;
 	
-	if ((distSqr(pos1, ACTIVECAM->orgTrans.pos) < square(ACTIVECAM->cdepth) * square(1.0f / 2))
+	if ((closerThan(pos1, ACTIVECAM->orgTrans.pos, ACTIVECAM->cdepth) * square(1.0f / 2))
 	        &&	(EEfabs(pos1.y - pos2.y) < 50.f)
-	        && (distSqr(pos1, pos2) < square(520)) && (io->_npcdata->behavior & BEHAVIOUR_MOVE_TO)
+	        && (closerThan(pos1, pos2, 520)) && (io->_npcdata->behavior & BEHAVIOUR_MOVE_TO)
 	        && (!(io->_npcdata->behavior & BEHAVIOUR_SNEAK))
 	        && (!(io->_npcdata->behavior & BEHAVIOUR_FLEE))
 	   )
@@ -651,7 +653,7 @@ bool ARX_NPC_LaunchPathfind(Entity * io, long target)
 		if(io->physics.startpos == io->physics.targetpos
 		        || ((ARX_COLLISION_Move_Cylinder(&phys, io, 40, CFLAG_JUST_TEST | CFLAG_NPC | CFLAG_NO_HEIGHT_MOD)) ))
 		{
-			if(distSqr(phys.cyl.origin, pos2) < square(100.f)) {
+			if(closerThan(phys.cyl.origin, pos2, 100.f)) {
 				io->_npcdata->pathfind.pathwait = 0;
 				return false;
 			}
@@ -1767,7 +1769,7 @@ void ARX_NPC_TryToCutSomething(Entity * target, Vec3f * pos)
 			}
 
 			if(out < 3) {
-				float dist = distSqr(*pos, target->obj->vertexlist3[target->obj->selections[i].selected[0]].v);
+				float dist = glm::distance2(*pos, target->obj->vertexlist3[target->obj->selections[i].selected[0]].v);
 
 				if(dist < mindistSqr) {
 					mindistSqr = dist;
@@ -2119,9 +2121,9 @@ void ARX_NPC_Manage_Anims(Entity * io, float TOLERANCE)
 	float tdist = std::numeric_limits<float>::max();
 
 	if(io->_npcdata->pathfind.listnb && ValidIONum(io->_npcdata->pathfind.truetarget)) {
-		tdist = distSqr(io->pos, entities[io->_npcdata->pathfind.truetarget]->pos);
+		tdist = glm::distance2(io->pos, entities[io->_npcdata->pathfind.truetarget]->pos);
 	} else if(ValidIONum(io->targetinfo)) {
-		tdist = distSqr(io->pos, entities[io->targetinfo]->pos);
+		tdist = glm::distance2(io->pos, entities[io->targetinfo]->pos);
 	}
 
 	Entity * ioo = io->_npcdata->weapon;
@@ -2745,7 +2747,7 @@ static void ManageNPCMovement(Entity * io)
 
 
 	// look around if finished fleeing or being looking around !
-	if((io->_npcdata->behavior & BEHAVIOUR_LOOK_AROUND) && distSqr(io->pos, io->target) > square(150.f)) {
+	if((io->_npcdata->behavior & BEHAVIOUR_LOOK_AROUND) && fartherThan(io->pos, io->target, 150.f)) {
 		if(!io->_npcdata->ex_rotate) {
 			ARX_NPC_CreateExRotateData(io);
 		} else { // already created
@@ -3384,7 +3386,7 @@ Entity * ARX_NPC_GetFirstNPCInSight(Entity * ioo)
 		return NULL;
 
 	// Basic Clipping to avoid performance loss
-	if(distSqr(ACTIVECAM->orgTrans.pos, ioo->pos) > square(2500)) {
+	if(fartherThan(ACTIVECAM->orgTrans.pos, ioo->pos, 2500)) {
 		return NULL;
 	}
 
@@ -3399,7 +3401,7 @@ Entity * ARX_NPC_GetFirstNPCInSight(Entity * ioo)
 				|| io->show != SHOW_FLAG_IN_SCENE)
 			continue;
 
-		float dist_io = distSqr(io->pos, ioo->pos);
+		float dist_io = glm::distance2(io->pos, ioo->pos);
 
 		if(dist_io > found_dist || dist_io > square(1800))
 			continue; // too far
@@ -3468,7 +3470,7 @@ Entity * ARX_NPC_GetFirstNPCInSight(Entity * ioo)
 						found_dist = dist_io;
 					}
 					continue;
-				} else if(distSqr(ppos, dest) < square(25.f)) {
+				} else if(closerThan(ppos, dest, 25.f)) {
 					if(found_dist > dist_io) {
 						found_io = io;
 						found_dist = dist_io;
@@ -3515,7 +3517,7 @@ void CheckNPC(Entity * io)
 void CheckNPCEx(Entity * io) {
 	
 	// Distance Between Player and IO
-	float ds = distSqr(io->pos, player.basePosition());
+	float ds = glm::distance2(io->pos, player.basePosition());
 	
 	// Start as not visible
 	long Visible = 0;
@@ -3557,7 +3559,7 @@ void CheckNPCEx(Entity * io) {
 					Vec3f ppos;
 					// Check for Geometrical Visibility
 					if(IO_Visible(&orgn, &dest, NULL, &ppos)
-					   || distSqr(ppos, dest) < square(25.f)) {
+					   || closerThan(ppos, dest, 25.f)) {
 						Visible = 1;
 					}
 				}

src/ai/PathFinder.cpp
@@ -46,6 +46,8 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include <limits>
 #include <algorithm>
 
+#include <glm/gtx/norm.hpp>
+
 #include "graphics/GraphicsTypes.h"
 #include "graphics/Math.h"
 #include "graphics/data/Mesh.h"
@@ -401,7 +403,7 @@ PathFinder::NodeId PathFinder::getNearestNode(const Vec3f & pos) const {
 	float distance = std::numeric_limits<float>::max();
 	
 	for(size_t i = 0; i < map_s; i++) {
-		float dist = distSqr(map_d[i].pos, pos);
+		float dist = glm::distance2(map_d[i].pos, pos);
 		if(dist < distance && map_d[i].nblinked) {
 			best = i;
 			distance = dist;

src/animation/AnimationRender.cpp
@@ -541,7 +541,7 @@ bool Cedric_IO_Visible(const Vec3f & pos) {
 
 	if(ACTIVEBKG) {
 		//TODO maybe readd this
-		//if(distSqr(io->pos, ACTIVECAM->orgTrans.pos) > square(ACTIVECAM->cdepth) * square(0.6f))
+		//if(fartherThan(io->pos, ACTIVECAM->orgTrans.pos, ACTIVECAM->cdepth * 0.6f))
 		//	return false;
 
 		long xx = pos.x * ACTIVEBKG->Xmul;
@@ -1707,7 +1707,7 @@ void EERIEDrawAnimQuatRender(EERIE_3DOBJ *eobj, const Vec3f & pos, Entity *io, b
 	bool isFightingNpc = io &&
 						 (io->ioflags & IO_NPC) &&
 						 (io->_npcdata->behavior & BEHAVIOUR_FIGHT) &&
-						 distSqr(io->pos, player.pos) < square(240.f);
+						 closerThan(io->pos, player.pos, 240.f);
 
 	if(!isFightingNpc && ARX_SCENE_PORTAL_ClipIO(io, pos))
 		return;

src/game/Damage.cpp
@@ -1225,17 +1225,15 @@ bool ARX_DAMAGES_TryToDoDamage(Vec3f * pos, float dmg, float radius, long source
 			float rad = radius + 5.f;
 
 			if(io->ioflags & IO_FIX) {
-				threshold = square(510);
+				threshold = 510;
 				rad += 10.f;
 			} else if(io->ioflags & IO_NPC) {
-				threshold = square(250);
+				threshold = 250;
 			} else {
-				threshold = square(350);
+				threshold = 350;
 			}
 
-			if(distSqr(*pos, io->pos) < threshold
-			   && SphereInIO(io, pos, rad)
-			) {
+			if(closerThan(*pos, io->pos, threshold) && SphereInIO(io, pos, rad)) {
 				if(io->ioflags & IO_NPC) {
 					if(ValidIONum(source))
 						ARX_EQUIPMENT_ComputeDamages(entities[source], io, 1.f);
@@ -1268,7 +1266,7 @@ void CheckForIgnition(Vec3f * pos, float radius, bool mode, long flag) {
 				if((el->extras & EXTRAS_FIREPLACE) && (flag & 2))
 					continue;
 
-				if(distSqr(*pos, el->pos) <= square(radius)) {
+				if(!fartherThan(*pos, el->pos, radius)) {
 					if(mode) {
 						if (!(el->extras & EXTRAS_NO_IGNIT))
 							el->status = 1;
@@ -1285,7 +1283,7 @@ void CheckForIgnition(Vec3f * pos, float radius, bool mode, long flag) {
 
 		if(io && io->show == 1 && io->obj && !(io->ioflags & IO_UNDERWATER) && io->obj->fastaccess.fire >= 0) {
 			
-			if(distSqr(*pos, io->obj->vertexlist3[io->obj->fastaccess.fire].v) < square(radius)) {
+			if(closerThan(*pos, io->obj->vertexlist3[io->obj->fastaccess.fire].v, radius)) {
 
 				if(mode && io->ignition <= 0 && io->obj->fastaccess.fire >= 0) {
 					io->ignition = 1;

src/game/Spells.cpp
@@ -58,6 +58,8 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <glm/gtx/norm.hpp>
+
 #include "animation/Animation.h"
 
 #include "core/Application.h"
@@ -2330,7 +2332,7 @@ long TemporaryGetSpellTarget(const Vec3f * from) {
 	long found = 0;
 	for(size_t i = 1; i < entities.size(); i++) {
 		if(entities[i] && entities[i]->ioflags & IO_NPC) {
-			float dist = distSqr(*from, entities[i]->pos);
+			float dist = glm::distance2(*from, entities[i]->pos);
 			if(dist < mindist) {
 				found = i;
 				mindist = dist;

src/graphics/Math.h
@@ -54,6 +54,8 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include <boost/numeric/conversion/cast.hpp>
 #include <boost/static_assert.hpp>
 
+#include <glm/gtx/norm.hpp>
+
 using std::min;
 using std::max;
 
@@ -269,7 +271,7 @@ inline float square(float x) {
  * may use an approximative way of computing sqrt !
  */
 inline float fdist(const Vec3f & from, const Vec3f & to) {
-	return ffsqrt(distSqr(from, to));
+	return ffsqrt(glm::distance2(from, to));
 }
 
 /*!
@@ -277,7 +279,7 @@ inline float fdist(const Vec3f & from, const Vec3f & to) {
  * may use an approximative way of computing sqrt !
  */
 inline float fdist(const Vec2f & from, const Vec2f & to) {
-	return ffsqrt(distSqr(from, to));
+	return ffsqrt(glm::distance2(from, to));
 }
 
 inline bool PointInCylinder(const EERIE_CYLINDER * cyl, const Vec3f * pt) {

src/graphics/particle/ParticleEffects.cpp
@@ -48,6 +48,8 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 
 #include <algorithm>
 
+#include <glm/gtx/norm.hpp>
+
 #include "core/Application.h"
 #include "core/Config.h"
 #include "core/Core.h"
@@ -486,7 +488,7 @@ void ARX_PARTICLES_Spawn_Blood(Vec3f * pos, float dmgs, long source) {
 	long count = entities[source]->obj->nbgroups;
 	for(long i = 0; i < count; i += 2) {
 		long vertex = entities[source]->obj->grouplist[i].origin;
-		float dist = distSqr(*pos, entities[source]->obj->vertexlist3[vertex].v);
+		float dist = glm::distance2(*pos, entities[source]->obj->vertexlist3[vertex].v);
 		if(dist < nearest_dist) {
 			nearest_dist = dist;
 			nearest = i;
@@ -1464,7 +1466,7 @@ void TreatBackgroundActions() {
 			continue;
 		}
 		
-		float dist = distSqr(gl->pos,	ACTIVECAM->orgTrans.pos);
+		float dist = glm::distance2(gl->pos,	ACTIVECAM->orgTrans.pos);
 		if(dist > fZFar) {
 			// Out of treat range
 			ARX_SOUND_Stop(gl->sample);

src/math/Vector.h
@@ -27,18 +27,13 @@
 // TODO replace calls with their direct glm equivalents!
 
 template <class V>
-typename V::value_type distSqr(const V & a, const V & b) {
-	return glm::distance2(a, b);
-}
-
-template <class V>
 bool closerThan(const V & a, const V & b, typename V::value_type d) {
-	return distSqr(a, b) < (d * d);
+	return glm::distance2(a, b) < (d * d);
 }
 
 template <class V>
 bool fartherThan(const V & a, const V & b, typename V::value_type d) {
-	return distSqr(a, b) > (d * d);
+	return glm::distance2(a, b) > (d * d);
 }
 
 template <class V>

commit 5ed02cf73ce1c98203c179b2e7ecb31edcc3ea5a

Fix conversion of float to bool, broken in c913b52

src/game/NPC.cpp
@@ -626,7 +626,7 @@ bool ARX_NPC_LaunchPathfind(Entity * io, EntityHandle target)
 	
 	io->_npcdata->pathfind.truetarget = target;
 	
-	if ((closerThan(pos1, ACTIVECAM->orgTrans.pos, ACTIVECAM->cdepth) * square(1.0f / 2))
+	if(   closerThan(pos1, ACTIVECAM->orgTrans.pos, ACTIVECAM->cdepth * 0.5f)
 	        &&	(glm::abs(pos1.y - pos2.y) < 50.f)
 	        && (closerThan(pos1, pos2, 520)) && (io->_npcdata->behavior & BEHAVIOUR_MOVE_TO)
 	        && (!(io->_npcdata->behavior & BEHAVIOUR_SNEAK))