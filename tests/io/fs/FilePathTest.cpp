5a7b69baf04bbb6487f4fbbc8aca729d068b92cc
Daniel Scharrer <daniel@constexpr.org>
2016-12-13 17:38:09 +0100

    Port unused FilePath test to CppUnit

---
+++ tests/io/fs/FilePathTest.cpp
@@ -0,0 +1,257 @@
+/*
+ * Copyright 2016 Arx Libertatis Team (see the AUTHORS file)
+ *
+ * This file is part of Arx Libertatis.
+ *
+ * Arx Libertatis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Arx Libertatis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Arx Libertatis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tests/io/fs/FilePathTest.h"
+
+#include <string>
+
+#include "io/fs/FilePath.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FilePathTest);
+
+void FilePathTest::testPath(const char * input, const char * expected) {
+	fs::path path = input;
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(std::string(input), std::string(expected), path.string());
+}
+
+void FilePathTest::pathTest() {
+	
+	testPath(".", ".");
+	testPath("./", ".");
+	testPath(".////./././//././/", ".");
+	testPath("./a", "a");
+	testPath(".////./././//././/a", "a");
+	testPath("./..", "..");
+	testPath(".////./././//././/..", "..");
+	testPath("./a/..", ".");
+	
+	testPath("/a", "/a");
+	testPath("/a/b", "/a/b");
+	testPath("/a/b/c", "/a/b/c");
+	testPath("/a/", "/a");
+	testPath("/a/b", "/a/b");
+	testPath("/a/b/c/./", "/a/b/c");
+	
+	testPath("/", "/");
+	testPath("/..", "/..");
+	testPath("/../", "/..");
+	testPath("/..//", "/..");
+	testPath("/../..", "/../..");
+	testPath("/..//..", "/../..");
+	testPath("/../../", "/../..");
+	testPath("/..//../", "/../..");
+	testPath("/../..//", "/../..");
+	testPath("/..//..//", "/../..");
+	
+	testPath("/a", "/a");
+	testPath("/a/..", "/");
+	testPath("/a/../", "/");
+	testPath("/a/..//", "/");
+	testPath("/a/../..", "/..");
+	testPath("/a/..//..", "/..");
+	testPath("/a/../../", "/..");
+	testPath("/a/..//../", "/..");
+	testPath("/a/../..//", "/..");
+	testPath("/a/..//..//", "/..");
+	
+	testPath("/abcd", "/abcd");
+	testPath("/abcd/..", "/");
+	testPath("/abcd/../", "/");
+	testPath("/abcd/..//", "/");
+	testPath("/abcd/../..", "/..");
+	testPath("/abcd/..//..", "/..");
+	testPath("/abcd/../../", "/..");
+	testPath("/abcd/..//../", "/..");
+	testPath("/abcd/../..//", "/..");
+	testPath("/abcd/..//..//", "/..");
+	
+	testPath("/../a", "/../a");
+	testPath("/../a/", "/../a");
+	testPath("/../a//", "/../a");
+	testPath("/../a/..", "/..");
+	testPath("/../a//..", "/..");
+	testPath("/../a/../", "/..");
+	testPath("/../a//../", "/..");
+	testPath("/../a/..//", "/..");
+	testPath("/../a//..//", "/..");
+	
+	testPath("a", "a");
+	testPath("a/b", "a/b");
+	testPath("a/", "a");
+	testPath("a/b/", "a/b");
+	
+	testPath("", "");
+	testPath("..", "..");
+	testPath("../", "..");
+	testPath("..//", "..");
+	testPath("../..", "../..");
+	testPath("..//..", "../..");
+	testPath("../../", "../..");
+	testPath("..//../", "../..");
+	testPath("../..//", "../..");
+	testPath("..//..//", "../..");
+	
+	testPath("a", "a");
+	testPath("a/..", ".");
+	testPath("a/../", ".");
+	testPath("a/..//", ".");
+	testPath("a/../..", "..");
+	testPath("a/..//..", "..");
+	testPath("a/../../", "..");
+	testPath("a/..//../", "..");
+	testPath("a/../..//", "..");
+	testPath("a/..//..//", "..");
+	
+	testPath("abcd", "abcd");
+	testPath("abcd/..", ".");
+	testPath("abcd/../", ".");
+	testPath("abcd/..//", ".");
+	testPath("abcd/../..", "..");
+	testPath("abcd/..//..", "..");
+	testPath("abcd/../../", "..");
+	testPath("abcd/..//../", "..");
+	testPath("abcd/../..//", "..");
+	testPath("abcd/..//..//", "..");
+	
+	testPath("../a", "../a");
+	testPath("../a/", "../a");
+	testPath("../a//", "../a");
+	testPath("../a/..", "..");
+	testPath("../a//..", "..");
+	testPath("../a/../", "..");
+	testPath("../a//../", "..");
+	testPath("../a/..//", "..");
+	testPath("../a//..//", "..");
+	
+	testPath("/.", "/");
+	
+}
+
+void FilePathTest::testResolve(const char * left, const char * right, const char * expected) {
+	
+	std::string message = "\"" + std::string(left) + "\" / \"" + std::string(right) + "\"";
+	
+	fs::path result = fs::path(left) / right;
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(message, std::string(expected), result.string());
+	
+	fs::path temp = left;
+	temp /= right;
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(message, std::string(expected), temp.string());
+	
+}
+
+void FilePathTest::resolveTest() {
+	
+	testResolve(".", "a", "a");
+	testResolve(".", "a/b", "a/b");
+	testResolve(".", "..", "..");
+	testResolve(".", "a", "a");
+	testResolve("a", "..", ".");
+	testResolve("a/b", "..", "a");
+	testResolve("a/b", "../..", ".");
+	
+	testResolve(".", "", ".");
+	testResolve(".", "a/..", ".");
+	
+	testResolve("/a", "b", "/a/b");
+	testResolve("/a", "b/c", "/a/b/c");
+	testResolve("/a/b", "c", "/a/b/c");
+	
+	testResolve("..", "..", "../..");
+	testResolve("/..", "..", "/../..");
+	testResolve("../..", "..", "../../..");
+	testResolve("/../..", "..", "/../../..");
+	testResolve("..", "../..", "../../..");
+	testResolve("/..", "../..", "/../../..");
+	
+	testResolve("../a", "../..", "../..");
+	testResolve("/../a", "../..", "/../..");
+	
+	testResolve("/a", "..", "/");
+	testResolve("/a", "../..", "/..");
+	testResolve("/a/..", "..", "/..");
+	
+	testResolve("/abcd", "..", "/");
+	testResolve("/abcd", "../..", "/..");
+	testResolve("/abcd/..", "..", "/..");
+	
+	testResolve("/" , "../a", "/../a");
+	testResolve("/..", "a", "/../a");
+	testResolve("/", "../a/..", "/..");
+	testResolve("/..", "a/..", "/..");
+	testResolve("/../a", "..", "/..");
+	
+	testResolve(".", "/a", "/a");
+	testResolve(".", "/a/b", "/a/b");
+	testResolve(".", "/..", "/..");
+	testResolve(".", "/a", "/a");
+	testResolve("a", "/..", "/..");
+	testResolve("a/b", "/..", "/..");
+	testResolve("a/b", "/../..", "/../..");
+	
+	testResolve(".", "/", "/");
+	testResolve(".", "/a/..", "/");
+	
+	testResolve("/a", "/b", "/b");
+	testResolve("/a", "/b/c", "/b/c");
+	testResolve("/a/b", "/c", "/c");
+	
+	testResolve("..", "/..", "/..");
+	testResolve("/..", "/..", "/..");
+	
+	testResolve("/a", "/..", "/..");
+	testResolve("/a", "/../..", "/../..");
+	testResolve("/a/..", "/..", "/..");
+	
+	testResolve("/abcd", "/..", "/..");
+	testResolve("/abcd", "/../..", "/../..");
+	testResolve("/abcd/..", "/..", "/..");
+	
+	testResolve("/" , "/../a", "/../a");
+	testResolve("/..", "/a", "/a");
+	testResolve("/", "/../a/..", "/..");
+	testResolve("/..", "/a/..", "/");
+	testResolve("/../a", "/..", "/..");
+	
+}
+
+void FilePathTest::testParent(const char * input, const char * expected) {
+	
+	fs::path parent = fs::path(input).parent();
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(std::string(input), std::string(expected), parent.string());
+	
+	fs::path temp = input;
+	temp.up();
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(std::string(input), std::string(expected), temp.string());
+	
+}
+
+void FilePathTest::parentTest() {
+	
+	testParent("", "..");
+	testParent(".", "..");
+	testParent("a", ".");
+	testParent("a/b", "a");
+	testParent("/", "/..");
+	testParent("/a", "/");
+	testParent("/a/b", "/a");
+	testParent("..", "../..");
+	testParent("../..", "../../..");
+	
+}
---
+++ tests/io/fs/FilePathTest.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016 Arx Libertatis Team (see the AUTHORS file)
+ *
+ * This file is part of Arx Libertatis.
+ *
+ * Arx Libertatis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Arx Libertatis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Arx Libertatis.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARX_TESTS_IO_FS_FILEPATHTEST_H
+#define ARX_TESTS_IO_FS_FILEPATHTEST_H
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class FilePathTest : public CppUnit::TestFixture {
+	
+	CPPUNIT_TEST_SUITE(FilePathTest);
+	CPPUNIT_TEST(pathTest);
+	CPPUNIT_TEST(resolveTest);
+	CPPUNIT_TEST(parentTest);
+	CPPUNIT_TEST_SUITE_END();
+	
+public:
+	FilePathTest()
+		: CppUnit::TestFixture()
+	{}
+	
+	void testPath(const char * input, const char * expected);
+	void testResolve(const char * left, const char * right, const char * expected);
+	void testParent(const char * input, const char * expected);
+	
+	void pathTest();
+	void resolveTest();
+	void parentTest();
+};
+
+#endif // ARX_TESTS_IO_FS_FILEPATHTEST_H
+
