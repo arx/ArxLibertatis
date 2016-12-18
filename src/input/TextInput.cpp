7c154ab93103afed637eb8d2d34dd8183c4f048f
Daniel Scharrer <daniel@constexpr.org>
2016-12-18 19:11:39 +0100

    Add a text input helper class

--- src/input/TextInput.cpp
+++ src/input/TextInput.cpp
@@ -19,6 +19,12 @@
 
 #include "input/TextInput.h"
 
+#include <algorithm>
+
+#include "core/Application.h"
+#include "util/Unicode.h"
+#include "window/RenderWindow.h"
+
 bool TextInputHandler::keyPressed(Keyboard::Key key, KeyModifiers mod) {
 	
 	switch(key) {
@@ -95,4 +101,349 @@ bool TextInputHandler::keyPressed(Keyboard::Key key, KeyModifiers mod) {
 		
 	}
 	
+}
+
+void BasicTextInput::newText(const std::string & text) {
+	if(!text.empty() || !m_editText.empty()) {
+		m_text.insert(m_cursorPos, text);
+		m_cursorPos += text.length();
+		m_editText.clear();
+		m_editCursorPos = 0;
+		m_editCursorLength = 0;
+		textUpdated();
+	}
+}
+
+void BasicTextInput::setText(const std::string & text, size_t cursorPos) {
+	if(text != m_text) {
+		m_text = text;
+		m_cursorPos = std::min(cursorPos, text.length());
+		textUpdated();
+	}
+}
+
+void BasicTextInput::insert(const std::string & text) {
+	if(!text.empty()) {
+		m_text.insert(m_cursorPos, text);
+		m_cursorPos += text.length();
+		textUpdated();
+	}
+}
+
+void BasicTextInput::editingText(const std::string & composition, size_t cursorStart, size_t cursorLength) {
+	if(composition != m_editText || cursorStart != m_editCursorPos || cursorLength != m_editCursorLength) {
+		arx_assert(cursorStart <= composition.size());
+		arx_assert(cursorStart + cursorLength <= composition.size());
+		m_editText = composition;
+		m_editCursorPos = cursorStart;
+		m_editCursorLength = cursorLength;
+		editUpdated();
+	}
+}
+
+void BasicTextInput::clear() {
+	if(!m_text.empty()) {
+		m_text.clear();
+		m_cursorPos = 0;
+		textUpdated();
+	}
+}
+
+void BasicTextInput::moveLeft() {
+	if(m_cursorPos > 0) {
+		while(m_cursorPos > 0 && util::UTF8::isContinuationByte(m_text[m_cursorPos - 1])) {
+			m_cursorPos--;
+		}
+		m_cursorPos--;
+		cursorUpdated();
+	}
+}
+
+void BasicTextInput::moveRight() {
+	if(m_cursorPos < m_text.size()) {
+		m_cursorPos++;
+		while(m_cursorPos < m_text.size() && util::UTF8::isContinuationByte(m_text[m_cursorPos])) {
+			m_cursorPos++;
+		}
+		cursorUpdated();
+	}
+}
+
+void BasicTextInput::eraseLeft() {
+	if(m_cursorPos > 0) {
+		size_t count = 1;
+		while(m_cursorPos > 0 && util::UTF8::isContinuationByte(m_text[m_cursorPos - 1])) {
+			count++;
+			m_cursorPos--;
+		}
+		m_text.erase(m_cursorPos - 1, count);
+		m_cursorPos--;
+		textUpdated();
+	}
+}
+
+void BasicTextInput::eraseRight() {
+	if(m_cursorPos < m_text.size()) {
+		size_t end = m_cursorPos + 1;
+		while(end < m_text.size() && util::UTF8::isContinuationByte(m_text[end])) {
+			end++;
+		}
+		m_text.erase(m_cursorPos, end - m_cursorPos);
+		textUpdated();
+	}
+}
+
+bool BasicTextInput::isWordSeparator(char c) {
+	switch(c) {
+		case '\t': return true;
+		case ' ':  return true;
+		case ',':  return true;
+		case ';':  return true;
+		case ':':  return true;
+		case '.':  return true;
+		case '!':  return true;
+		case '+':  return true;
+		case '<':  return true;
+		case '>':  return true;
+		case '(':  return true;
+		case ')':  return true;
+		default:   return false;
+	}
+}
+
+size_t BasicTextInput::findWordLeft() const {
+	size_t newPos = m_cursorPos;
+	while(newPos > 0 && isWordSeparator(m_text[newPos - 1])) {
+		newPos--;
+	}
+	while(newPos > 0 && !isWordSeparator(m_text[newPos - 1])) {
+		newPos--;
+	}
+	return newPos;
+}
+
+size_t BasicTextInput::findWordRight() const {
+	size_t newPos = m_cursorPos;
+	while(newPos < m_text.size() && isWordSeparator(m_text[newPos])) {
+		newPos++;
+	}
+	while(newPos < m_text.size() && !isWordSeparator(m_text[newPos])) {
+		newPos++;
+	}
+	return newPos;
+}
+
+void BasicTextInput::moveWordLeft() {
+	if(m_cursorPos > 0) {
+		m_cursorPos = findWordLeft();
+		cursorUpdated();
+	}
+}
+
+void BasicTextInput::moveWordRight() {
+	if(m_cursorPos < m_text.size()) {
+		m_cursorPos = findWordRight();
+		cursorUpdated();
+	}
+}
+
+void BasicTextInput::eraseWordLeft() {
+	if(m_cursorPos > 0) {
+		size_t start = findWordLeft();
+		m_text.erase(start, m_cursorPos - start);
+		m_cursorPos = start;
+		textUpdated();
+	}
+}
+
+void BasicTextInput::eraseWordRight() {
+	if(m_cursorPos < m_text.size()) {
+		size_t end = findWordRight();
+		m_text.erase(m_cursorPos, end - m_cursorPos);
+		textUpdated();
+	}
+}
+
+void BasicTextInput::moveStart() {
+	if(m_cursorPos > 0) {
+		m_cursorPos = 0;
+		cursorUpdated();
+	}
+}
+
+void BasicTextInput::moveEnd() {
+	if(m_cursorPos < m_text.size()) {
+		m_cursorPos = m_text.size();
+		cursorUpdated();
+	}
+}
+
+void BasicTextInput::eraseStart() {
+	if(m_cursorPos > 0) {
+		m_text.erase(0, m_cursorPos);
+		m_cursorPos = 0;
+		textUpdated();
+	}
+}
+
+void BasicTextInput::eraseEnd() {
+	if(m_cursorPos < m_text.size()) {
+		m_text.erase(m_cursorPos);
+		textUpdated();
+	}
+}
+
+void BasicTextInput::paste(const std::string & text) {
+	std::string filtered = text;
+	std::replace(filtered.begin(), filtered.end(), '\n', ' ');
+	std::replace(filtered.begin(), filtered.end(), '\t', ' ');
+	insert(filtered);
+}
+
+bool BasicTextInput::keyPressed(Keyboard::Key key, KeyModifiers mod) {
+	
+	switch(key) {
+		
+		case Keyboard::Key_NumPad4: {
+			if(mod.num) {
+				break;
+			}
+			// fall-through
+		}
+		case Keyboard::Key_LeftArrow: {
+			if(mod.control) {
+				moveWordLeft();
+			} else {
+				moveLeft();
+			}
+			return true;
+		}
+		
+		case Keyboard::Key_NumPad6: {
+			if(mod.num) {
+				break;
+			}
+			// fall-through
+		}
+		case Keyboard::Key_RightArrow: {
+			if(mod.control) {
+				moveWordRight();
+			} else {
+				moveRight();
+			}
+			return true;
+		}
+		
+		case Keyboard::Key_NumPad7: {
+			if(mod.num) {
+				break;
+			}
+			// fall-through
+		}
+		case Keyboard::Key_Home: {
+			moveStart();
+			return true;
+		}
+		
+		case Keyboard::Key_NumPad1: {
+			if(mod.num) {
+				break;
+			}
+			// fall-through
+		}
+		case Keyboard::Key_End: {
+			moveEnd();
+			return true;
+		}
+		
+		case Keyboard::Key_Backspace: {
+			if(mod.control) {
+				eraseWordLeft();
+			} else {
+				eraseLeft();
+			}
+			return true;
+		}
+		
+		case Keyboard::Key_NumPoint: {
+			if(mod.num) {
+				break;
+			}
+			// fall-through
+		}
+		case Keyboard::Key_Delete: {
+			if(mod.control) {
+				eraseWordRight();
+			} else {
+				eraseRight();
+			}
+			return true;
+		}
+		
+		default: break;
+	}
+	
+	if(mod.control) {
+		switch(key) {
+			
+			case Keyboard::Key_A: {
+				moveStart();
+				return true;
+			}
+			
+			case Keyboard::Key_B: {
+				moveLeft();
+				return true;
+			}
+			
+			case Keyboard::Key_C: {
+				clear();
+				return true;
+			}
+			
+			case Keyboard::Key_D: {
+				eraseRight();
+				return true;
+			}
+			
+			case Keyboard::Key_E: {
+				moveEnd();
+				return true;
+			}
+			
+			case Keyboard::Key_F: {
+				moveRight();
+				return true;
+			}
+			
+			case Keyboard::Key_H: {
+				eraseLeft();
+				return true;
+			}
+			
+			case Keyboard::Key_K: {
+				eraseEnd();
+				return true;
+			}
+			
+			case Keyboard::Key_U: {
+				eraseStart();
+				return true;
+			}
+			
+			case Keyboard::Key_V: {
+				paste(mainApp->getWindow()->getClipboardText());
+				return true;
+			}
+			
+			case Keyboard::Key_W: {
+				eraseWordLeft();
+				return true;
+			}
+			
+			default: break;
+		}
+	}
+	
+	return TextInputHandler::keyPressed(key, mod);
 }
