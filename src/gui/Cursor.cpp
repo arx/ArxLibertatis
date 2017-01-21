b3336de28b785d46aa0df004677836076b003deb
bsxf47 <bsxf47@gmail.com>
2017-01-21 14:06:48 +0100

    Cursor: Use platform time for item highlighting

--- src/gui/Cursor.cpp
+++ src/gui/Cursor.cpp
@@ -444,7 +444,7 @@ static void ARX_INTERFACE_RenderCursorInternal(bool flag) {
 	
 	if(!SPECIAL_DRAGINTER_RENDER) {
 		if(FlyingOverIO || DRAGINTER) {
-			fHighLightAng += g_framedelay * 0.5f;
+			fHighLightAng += toMs(g_platformTime.lastFrameDuration()) * 0.5f;
 			
 			if(fHighLightAng > 90.f)
 				fHighLightAng = 90.f;
