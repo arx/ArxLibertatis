arx/ArxLibertatis

commit 13c35d76eb2a86a359f6a619cbb5702214a527ce

Move variable to smaller scope

src/cinematic/Cinematic.cpp
@@ -480,13 +480,13 @@ void Cinematic::Render(PlatformDuration frameDuration) {
 	
 	col.a = alpha;
 	
-	CinematicLight lightt, *l = NULL;
+	CinematicLight *l = NULL;
 	
 	static const float SPEEDINTENSITYRND = 60.f / 1000.f;
 	float FDIFF = float(toMs(frameDuration));
 	
 	if(m_light.intensity >= 0.f && m_lightd.intensity >= 0.f) {
-		lightt = m_light;
+		CinematicLight lightt = m_light;
 		
 		lightt.pos = lightt.pos * g_sizeRatio.y + Vec3f(g_size.center(), 0.f);
 		lightt.fallin *= g_sizeRatio.y;
@@ -527,7 +527,7 @@ void Cinematic::Render(PlatformDuration frameDuration) {
 		l = NULL;
 		
 		if(m_light.intensity >= 0.f && m_lightd.intensity >= 0.f) {
-			lightt = m_lightd;
+			CinematicLight lightt = m_lightd;
 			
 			lightt.pos = lightt.pos * g_sizeRatio.y + Vec3f(g_size.center(), 0.f);
 			lightt.fallin *= g_sizeRatio.y;

commit 6a39dcfc047f7ab9d749df2f3013d3b23264c784

Fix pointer to out of scope local, broken in 13c35d7

src/cinematic/Cinematic.cpp
@@ -480,13 +480,17 @@ void Cinematic::Render(PlatformDuration frameDuration) {
 	
 	col.a = alpha;
 	
-	CinematicLight *l = NULL;
+	
 	
 	static const float SPEEDINTENSITYRND = 60.f / 1000.f;
 	float FDIFF = float(toMs(frameDuration));
 	
+	{
+	CinematicLight *l = NULL;
+	CinematicLight lightt;
+	
 	if(m_light.intensity >= 0.f && m_lightd.intensity >= 0.f) {
-		CinematicLight lightt = m_light;
+		lightt = m_light;
 		
 		lightt.pos = lightt.pos * g_sizeRatio.y + Vec3f(g_size.center(), 0.f);
 		lightt.fallin *= g_sizeRatio.y;
@@ -500,6 +504,8 @@ void Cinematic::Render(PlatformDuration frameDuration) {
 	
 	if(tb->grid.m_nbvertexs)
 		DrawGrille(tb, col, fx, l, posgrille, angzgrille, fadegrille);
+	
+	}
 	
 	//PASS #2
 	if(force & 1) {
@@ -524,10 +530,12 @@ void Cinematic::Render(PlatformDuration frameDuration) {
 		alpha = 255 - alpha;
 		col.a = alpha;
 		
-		l = NULL;
+		{
+		CinematicLight *l = NULL;
+		CinematicLight lightt;
 		
 		if(m_light.intensity >= 0.f && m_lightd.intensity >= 0.f) {
-			CinematicLight lightt = m_lightd;
+			lightt = m_lightd;
 			
 			lightt.pos = lightt.pos * g_sizeRatio.y + Vec3f(g_size.center(), 0.f);
 			lightt.fallin *= g_sizeRatio.y;
@@ -541,6 +549,8 @@ void Cinematic::Render(PlatformDuration frameDuration) {
 		
 		if(tb->grid.m_nbvertexs)
 			DrawGrille(tb, col, fx, l, m_nextPosgrille, m_nextAngzgrille, m_nextFadegrille);
+		
+		}
 	}
 	
 	//effets qui continuent avec le temps