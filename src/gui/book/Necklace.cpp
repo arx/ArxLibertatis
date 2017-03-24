b6cd5f21013c8915b44bf13e3ae13e9b610c59df
bsxf47 <bsxf47@gmail.com>
2017-03-24 19:33:22 +0100

    Necklace: Shorten initialization

--- src/gui/book/Necklace.cpp
+++ src/gui/book/Necklace.cpp
@@ -146,8 +146,7 @@ void ARX_INTERFACE_ManageOpenedBook_Finish(const Vec2f & mousePos)
 		
 		EERIE_3DOBJ * rune = gui::necklace.runes[i];
 		
-		bookcam.center.x = (382 + tmpPos.x * 45 + BOOKDEC.x) * g_sizeRatio.x;
-		bookcam.center.y = (100 + tmpPos.y * 64 + BOOKDEC.y) * g_sizeRatio.y;
+		bookcam.center = (Vec2f(382, 100) + Vec2f(tmpPos) * Vec2f(45, 64) + BOOKDEC) * g_sizeRatio;
 		
 		SetActiveCamera(&bookcam);
 		PrepareCamera(&bookcam, g_size);
