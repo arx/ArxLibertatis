1795e142aeeec0b3d034804ae42e5d8c33b82765
bsxf47 <bsxf47@gmail.com>
2017-04-29 11:50:15 +0200

    RuneDraw: Use Vec2f

--- src/game/magic/RuneDraw.cpp
+++ src/game/magic/RuneDraw.cpp
@@ -147,7 +147,7 @@ void ARX_SPELLS_UpdateBookSymbolDraw(Rect rect) {
 		iMin = Vec2s(Vec2f(iMin) * scale);
 		iSize = Vec2s(Vec2f(iSize) * scale);
 
-		Vec2s pos = Vec2s(rect.center()) - iSize / Vec2s(2) - iMin;
+		Vec2f pos = Vec2s(rect.center()) - iSize / Vec2s(2) - iMin;
 
 		for(size_t j = 0; j < nbcomponents; j++) {
 
@@ -157,7 +157,7 @@ void ARX_SPELLS_UpdateBookSymbolDraw(Rect rect) {
 
 			if(timeRemaining < ti) {
 				float ratio = float(timeRemaining) * div_ti;
-				pos += Vec2s(Vec2f(ratio) * Vec2f(vect) * 0.5f);
+				pos += Vec2f(ratio) * Vec2f(vect) * 0.5f;
 				AddFlare(pos, 0.1f, 1, io, true);
 
 				break;
