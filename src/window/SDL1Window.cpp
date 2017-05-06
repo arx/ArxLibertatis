e14ba33d8901af8b8bfcc08ae9eed43d03ee49d6
Daniel Scharrer <daniel@constexpr.org>
2017-05-06 18:08:04 +0200

    SDLWindow: Log hw info when not enough texture units are available

--- src/window/SDL1Window.cpp
+++ src/window/SDL1Window.cpp
@@ -220,6 +220,7 @@ bool SDL1Window::initialize() {
 		glGetIntegerv(GL_MAX_TEXTURE_UNITS, &texunits);
 		if(glGetError() != GL_NO_ERROR || texunits < GLint(m_minTextureUnits)) {
 			if(lastTry) {
+				m_renderer->initialize(); // Log hardware information
 				LogError << "Not enough GL texture units available: have " << texunits
 				         << ", need at least " << m_minTextureUnits;
 				return false;
