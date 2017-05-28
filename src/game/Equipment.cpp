932bc17842b956b7a3622065fcfbb05ba31c9e30
Daniel Scharrer <daniel@constexpr.org>
2017-05-28 14:49:23 +0200

    Equipment: Make sparks from melee weapon hits framerate-independent
    This reduces the sparks to a level similar to before commit cdfc37f.

--- src/game/Equipment.cpp
+++ src/game/Equipment.cpp
@@ -803,9 +803,9 @@ bool ARX_EQUIPMENT_Strike_Check(Entity * io_source, Entity * io_weapon, float ra
 							ARX_PARTICLES_Spawn_Blood2(pos, dmgs, color, target);
 						} else {
 							if(target->ioflags & IO_ITEM)
-								ParticleSparkSpawn(pos, Random::getu(0, 3), SpawnSparkType_Default);
+								ParticleSparkSpawnContinous(pos, Random::getu(0, 3), SpawnSparkType_Default);
 							else
-								ParticleSparkSpawn(pos, Random::getu(0, 30), SpawnSparkType_Default);
+								ParticleSparkSpawnContinous(pos, Random::getu(0, 30), SpawnSparkType_Default);
 
 							ARX_NPC_SpawnAudibleSound(pos, io_source);
 
@@ -823,7 +823,7 @@ bool ARX_EQUIPMENT_Strike_Check(Entity * io_source, Entity * io_weapon, float ra
 						if(target->ioflags & IO_ITEM)
 							nb = 1;
 
-						ParticleSparkSpawn(pos, nb, SpawnSparkType_Default);
+						ParticleSparkSpawnContinous(pos, nb, SpawnSparkType_Default);
 						ARX_NPC_SpawnAudibleSound(pos, io_source);
 						target->spark_n_blood = SP_SPARKING;
 
@@ -840,7 +840,7 @@ bool ARX_EQUIPMENT_Strike_Check(Entity * io_source, Entity * io_weapon, float ra
 						if(target->ioflags & IO_ITEM)
 							nb = 1;
 
-						ParticleSparkSpawn(pos, nb, SpawnSparkType_Default);
+						ParticleSparkSpawnContinous(pos, nb, SpawnSparkType_Default);
 						ARX_NPC_SpawnAudibleSound(pos, io_source);
 						target->spark_n_blood = SP_SPARKING;
 
@@ -892,7 +892,7 @@ bool ARX_EQUIPMENT_Strike_Check(Entity * io_source, Entity * io_weapon, float ra
 				}
 			}
 
-			ParticleSparkSpawn(sphere.origin, Random::getu(0, 10), SpawnSparkType_Default);
+			ParticleSparkSpawnContinous(sphere.origin, Random::getu(0, 10), SpawnSparkType_Default);
 			ARX_NPC_SpawnAudibleSound(sphere.origin, io_source);
 		}
 	}
