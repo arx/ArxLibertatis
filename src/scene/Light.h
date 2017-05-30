16418c3265611645d0b04659189520eb877c7027
Daniel Scharrer <daniel@constexpr.org>
2017-05-30 23:54:34 +0200

    Light: Remove unused functions

--- src/scene/Light.h
+++ src/scene/Light.h
@@ -139,10 +139,6 @@ struct ColorMod {
 void RecalcLight(EERIE_LIGHT * el);
 
 void EERIE_LIGHT_GlobalInit();
-long EERIE_LIGHT_GetFree();
-long EERIE_LIGHT_Count();
-void EERIE_LIGHT_GlobalAdd(const EERIE_LIGHT * el);
-void EERIE_LIGHT_MoveAll(const Vec3f & trans);
 long EERIE_LIGHT_Create();
 void PrecalcIOLighting(const Vec3f & pos, float radius);
 
