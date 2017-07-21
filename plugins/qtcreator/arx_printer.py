71845331db533660ff615c9a5b189ad97fbe0f75
Eli2 <FabianRoth64@googlemail.com>
2017-07-21 21:33:59 +0200

    qtcreator, fix debug helpers

--- plugins/qtcreator/arx_printer.py
+++ plugins/qtcreator/arx_printer.py
@@ -1,51 +1,61 @@
 
 def qdump__Vec2f(d, value):
-    x = float(value['x'])
-    y = float(value['y'])
+    x = value['#1']['x'].value()
+    y = value['#2']['y'].value()
     valueStr = "{:10.4f} {:10.4f}".format(x, y)
+    
     d.putType("Vec2f")
     d.putValue(valueStr)
     d.putPlainChildren(value)
 
 def qdump__Vec3f(d, value):
-    x = float(value['x'])
-    y = float(value['y'])
-    z = float(value['z'])
+    x = value['#1']['x'].value()
+    y = value['#2']['y'].value()
+    z = value['#3']['z'].value()
     valueStr = "{:10.4f} {:10.4f} {:10.4f}".format(x, y, z)
+    
     d.putType("Vec3f")
     d.putValue(valueStr)
     d.putPlainChildren(value)
 
 def qdump__Anglef(d, value):
-    yaw = float(value['m_yaw'])
-    pitch = float(value['m_pitch'])
-    roll = float(value['m_roll'])
+    yaw   = value['m_yaw'].value()
+    pitch = value['m_pitch'].value()
+    roll  = value['m_roll'].value()
     valueStr = "{:10.4f} {:10.4f} {:10.4f}".format(yaw, pitch, roll)
+    
     d.putType("Anglef")
     d.putValue(valueStr)
     d.putPlainChildren(value)
 
 def qdump__Color3f(d, value):
-    r = float(value['r'])
-    g = float(value['g'])
-    b = float(value['b'])
+    r = value['r'].value()
+    g = value['g'].value()
+    b = value['b'].value()
     valueStr = "{:10.4f} {:10.4f} {:10.4f}".format(r, g, b)
+    
     d.putType("Color3f")
     d.putValue(valueStr)
     d.putPlainChildren(value)
 
 
 def qdump__Entity(d, value):
+    index = value['m_index'].value()
+    
     d.putType("Entity")
-    d.putValue(value['m_index'])
+    d.putValue("Idx: " + str(index))
     d.putPlainChildren(value)
 
 def qdump__EntityHandle(d, value):
+    val = value['t'].value()
+    
     d.putType("EntityHandle")
-    d.putValue(value['t'])
+    d.putValue(val)
     d.putPlainChildren(value)
 
 def qdump__LightHandle(d, value):
+    val = value['t'].value()
+    
     d.putType("LightHandle")
-    d.putValue(value['t'])
+    d.putValue(val)
     d.putPlainChildren(value)
