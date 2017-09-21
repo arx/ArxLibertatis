arx/ArxLibertatis

commit 2cd4fd7734e769cdcd0d24f7ab2a84259b0d7f33

GLTextureStage: Remove unused function

src/graphics/opengl/GLTextureStage.cpp
@@ -189,15 +189,6 @@ void GLTextureStage::setTexEnv(GLenum target, GLenum pname, GLint param) {
 	}
 }
 
-void GLTextureStage::setTexEnv(GLenum target, GLenum pname, GLfloat param) {
-
-	FloatStateCache::iterator it = m_stateCacheFloats.find(pname);
-	if(it == m_stateCacheFloats.end() || it->second != param) {
-		glTexEnvf(target, pname, param);
-		m_stateCacheFloats[pname] = param;
-	}
-}
-
 void GLTextureStage::setColorOp(TextureOp op) {
 	setOp(Color, op);
 }

src/graphics/opengl/GLTextureStage.h
@@ -82,7 +82,6 @@ private:
 	void setOp(OpType alpha, TextureOp op);
 
 	void setTexEnv(GLenum target, GLenum pname, GLint param);
-	void setTexEnv(GLenum target, GLenum pname, GLfloat param);
 
 	GLTexture2D * tex;
 	GLTexture2D * current;
@@ -93,9 +92,6 @@ private:
 
 	typedef std::map<GLenum, GLint> IntegerStateCache;
 	IntegerStateCache m_stateCacheIntegers;
-
-	typedef std::map<GLenum, GLfloat> FloatStateCache;
-	FloatStateCache m_stateCacheFloats;
 	
 	friend class GLTexture2D;
 };

commit 95ffc78382c4e16ff8fe221dd003121e0fdc0dec

[gcc] GLTextureStage: Fix non-integer mip biases
This was broken in commit 2cd4fd7.

src/graphics/opengl/GLTextureStage.cpp
@@ -203,7 +203,7 @@ void GLTextureStage::setMipMapLODBias(float bias) {
 		glActiveTexture(GL_TEXTURE0 + mStage);
 	}
 	
-	setTexEnv(GL_TEXTURE_FILTER_CONTROL, GL_TEXTURE_LOD_BIAS, bias);
+	glTexEnvf(GL_TEXTURE_FILTER_CONTROL, GL_TEXTURE_LOD_BIAS, bias);
 	
 	if(mStage != 0) {
 		glActiveTexture(GL_TEXTURE0);