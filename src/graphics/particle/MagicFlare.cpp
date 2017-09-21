17f34d4d532b78726363e921e31697af0e84ea50
Daniel Scharrer <daniel@constexpr.org>
2017-09-21 20:02:46 +0200

    [gcc] MagicFlare: Use PlatformDurationMsf()

--- src/graphics/particle/MagicFlare.cpp
+++ src/graphics/particle/MagicFlare.cpp
@@ -227,20 +227,20 @@ void AddFlare(const Vec2f & pos, float sm, short typ, Entity * io, bool bookDraw
 		if(zz < 0.2f) {
 			flare.type = 2;
 			flare.size = Random::getf(42.f, 84.f);
-			flare.tolive = PlatformDurationMs(Random::getf(800.f, 1600.f) * FLARE_MUL);
+			flare.tolive = PlatformDurationMsf(Random::getf(800.f, 1600.f) * FLARE_MUL);
 		} else if(zz < 0.5f) {
 			flare.type = 3;
 			flare.size = Random::getf(16.f, 68.f);
-			flare.tolive = PlatformDurationMs(Random::getf(800.f, 1600.f) * FLARE_MUL);
+			flare.tolive = PlatformDurationMsf(Random::getf(800.f, 1600.f) * FLARE_MUL);
 		} else {
 			flare.type = 1;
 			flare.size = Random::getf(32.f, 56.f) * sm;
-			flare.tolive = PlatformDurationMs(Random::getf(1700.f, 2200.f) * FLARE_MUL);
+			flare.tolive = PlatformDurationMsf(Random::getf(1700.f, 2200.f) * FLARE_MUL);
 		}
 	} else {
 		flare.type = (Random::getf() > 0.8f) ? 1 : 4;
 		flare.size = Random::getf(64.f, 102.f) * sm;
-		flare.tolive = PlatformDurationMs(Random::getf(1700.f, 2200.f) * FLARE_MUL);
+		flare.tolive = PlatformDurationMsf(Random::getf(1700.f, 2200.f) * FLARE_MUL);
 	}
 
 	flare.dynlight = LightHandle();
