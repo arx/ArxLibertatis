arx/ArxLibertatis

commit e54c1115836030753b2295480e6abf58c8e4dd46

Remove use of toMs

src/graphics/particle/Spark.cpp
@@ -63,7 +63,7 @@ long ParticleSparkCount() {
 
 void ParticleSparkSpawnContinous(const Vec3f & pos, unsigned rate, SpawnSparkType type) {
 	
-	float amount = float(rate) * toMs(g_platformTime.lastFrameDuration()) * (15.f / 1000.f);
+	float amount = float(rate) * (g_platformTime.lastFrameDuration() / PlatformDurationMsf(0.015f));
 	
 	unsigned count = unsigned(amount);
 	if(Random::getf() < (amount - float(count))) {

commit fdce12c41f70c80a2d5c9d34e1783e993affddb2

Fix excessive sparks, broken in e54c111

src/graphics/particle/Spark.cpp
@@ -63,7 +63,7 @@ long ParticleSparkCount() {
 
 void ParticleSparkSpawnContinous(const Vec3f & pos, unsigned rate, SpawnSparkType type) {
 	
-	float amount = float(rate) * (g_platformTime.lastFrameDuration() / PlatformDurationMsf(0.015f));
+	float amount = float(rate) * (g_platformTime.lastFrameDuration() / PlatformDurationMsf(66.666666f));
 	
 	unsigned count = unsigned(amount);
 	if(Random::getf() < (amount - float(count))) {