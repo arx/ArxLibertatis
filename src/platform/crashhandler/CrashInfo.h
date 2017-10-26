26f85819a7dd8cdbea81f8cfa2458852aebbfe6a
Daniel Scharrer <daniel@constexpr.org>
2017-10-26 21:39:37 +0200

    [style] CrashInfo: Add missing whitespace

--- src/platform/crashhandler/CrashInfo.h
+++ src/platform/crashhandler/CrashInfo.h
@@ -28,7 +28,7 @@
 #include "platform/Platform.h"
 #include "platform/Process.h"
 
-#pragma pack(push,1)
+#pragma pack(push, 1)
 
 struct CrashInfoBase {
 	
@@ -158,7 +158,7 @@ enum CrashType {
 	SIGNAL_UNKNOWN
 };
 
-#pragma pack(push,1)
+#pragma pack(push, 1)
 
 struct CrashInfo : public CrashInfoBase {
 	
