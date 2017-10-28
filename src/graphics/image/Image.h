64944c49c88ef27bd21cea73c2ae5f0728a40f3a
Daniel Scharrer <daniel@constexpr.org>
2017-10-28 13:37:16 +0200

    Image: Rename function

--- src/graphics/image/Image.h
+++ src/graphics/image/Image.h
@@ -115,7 +115,7 @@ public:
 	// destructively adjust image content
 	
 	//! blur using gaussian kernel
-	void Blur(size_t radius);
+	void blur(size_t radius);
 	
 	//! scales value and normalizes by max component value
 	void applyGamma(float gamma);
