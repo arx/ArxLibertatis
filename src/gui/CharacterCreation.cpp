83dc8f88f4b0b2f5e2fc4099addfe86934499865
Eli2 <FabianRoth64@googlemail.com>
2017-11-27 04:50:17 +0100

    CharacterCreation: indentation

--- src/gui/CharacterCreation.cpp
+++ src/gui/CharacterCreation.cpp
@@ -88,146 +88,146 @@ void CharacterCreation::render() {
 	arx_assert(BookBackground);
 	
 	{
-		UseRenderState state(render2D());
+	UseRenderState state(render2D());
+	
+	EERIEDrawBitmap(Rectf(Vec2f(0, 0), g_size.width(), g_size.height()), 0.9f, BookBackground, Color::white);
+	
+	g_playerBook.stats.manageNewQuest();
 		
-		EERIEDrawBitmap(Rectf(Vec2f(0, 0), g_size.width(), g_size.height()), 0.9f, BookBackground, Color::white);
+	bool DONE = (player.Skill_Redistribute == 0 && player.Attribute_Redistribute == 0);
+	
+	Vec2f pos;
+	pos.x = 0;
+	pos.y = 313 * g_sizeRatio.y + (g_size.height() - 313 * g_sizeRatio.y) * 0.70f;
+	
+	Vec2f size = g_sizeRatio;
+	size *= 100;
+	
+	Color color = Color::none;
+	
+	//---------------------------------------------------------------------
+	// Button QUICK GENERATION
+	pos.x = (g_size.width() - (513 * g_sizeRatio.x)) * 0.5f;
+	
+	const Rectf quickGenerateButtonMouseTestRect(
+		pos,
+		size.x,
+		size.y
+	);
+	
+	if(quickGenerateButtonMouseTestRect.contains(Vec2f(DANAEMouse))) {
+		SpecialCursor = CURSOR_INTERACTION_ON;
+		FLYING_OVER = BUTTON_QUICK_GENERATION;
 		
-		g_playerBook.stats.manageNewQuest();
-		
-			bool DONE = (player.Skill_Redistribute == 0 && player.Attribute_Redistribute == 0);
+		if(eeMousePressed1());
+		else if (eeMouseUp1())
+		{
+			m_cheatQuickGenButtonClickCount++;
+			int iSkin = player.skin;
+			ARX_SOUND_PlayMenu(SND_MENU_CLICK);
 			
-			Vec2f pos;
-			pos.x = 0;
-			pos.y = 313 * g_sizeRatio.y + (g_size.height() - 313 * g_sizeRatio.y) * 0.70f;
-			
-			Vec2f size = g_sizeRatio;
-			size *= 100;
-			
-			Color color = Color::none;
-			
-			//---------------------------------------------------------------------
-			// Button QUICK GENERATION
-			pos.x = (g_size.width() - (513 * g_sizeRatio.x)) * 0.5f;
-			
-			const Rectf quickGenerateButtonMouseTestRect(
-				pos,
-				size.x,
-				size.y
-			);
-			
-			if(quickGenerateButtonMouseTestRect.contains(Vec2f(DANAEMouse))) {
-				SpecialCursor = CURSOR_INTERACTION_ON;
-				FLYING_OVER = BUTTON_QUICK_GENERATION;
-				
-				if(eeMousePressed1());
-				else if (eeMouseUp1())
-				{
-					m_cheatQuickGenButtonClickCount++;
-					int iSkin = player.skin;
-					ARX_SOUND_PlayMenu(SND_MENU_CLICK);
-					
-					if(bQuickGenFirstClick) {
-						ARX_PLAYER_MakeAverageHero();
-						bQuickGenFirstClick = false;
-					} else {
-						ARX_PLAYER_QuickGeneration();
-					}
-					
-					player.skin = checked_range_cast<char>(iSkin);
-				}
-				
-				color = Color(255, 255, 255);
-			}
-			else
-				color = Color(232, 204, 143);
-			
-			pTextManage->AddText(hFontMenu, str_button_quickgen, Vec2i(pos), color);
-			
-			//---------------------------------------------------------------------
-			// Button SKIN
-			pos.x = g_size.width() * 0.5f;
-			
-			const Rectf skinButtonMouseTestRect(
-				pos,
-				size.x,
-				size.y
-			);
-			
-			if(skinButtonMouseTestRect.contains(Vec2f(DANAEMouse))) {
-				SpecialCursor = CURSOR_INTERACTION_ON;
-				FLYING_OVER = BUTTON_SKIN;
-				
-				if(eeMouseUp1()) {
-					m_cheatSkinButtonClickCount++;
-					ARX_SOUND_PlayMenu(SND_MENU_CLICK);
-					player.skin++;
-					
-					if(player.skin > 3)
-						player.skin = 0;
-					
-					ARX_PLAYER_Restore_Skin();
-				}
-				
-				color = Color(255, 255, 255);
-			}
-			else
-				color = Color(232, 204, 143);
-			
-			pTextManage->AddText(hFontMenu, str_button_skin, Vec2i(pos), color);
-			
-			//---------------------------------------------------------------------
-			// Button DONE
-			pos.x = g_size.width() - (g_size.width() - 513 * g_sizeRatio.x) * 0.5f - 40 * g_sizeRatio.x;
-			
-			const Rectf doneButtonMouseTestRect(
-				pos,
-				size.x,
-				size.y
-			);
-			
-			if(doneButtonMouseTestRect.contains(Vec2f(DANAEMouse))) {
-				if(DONE)
-					SpecialCursor = CURSOR_INTERACTION_ON;
-				
-				FLYING_OVER = BUTTON_DONE;
-				
-				if(DONE && eeMouseUp1()) {
-					if(m_cheatSkinButtonClickCount == 8 && m_cheatQuickGenButtonClickCount == 10) {
-						m_cheatSkinButtonClickCount = -2;
-					} else if(m_cheatSkinButtonClickCount == -1) {
-						ARX_PLAYER_MakeSpHero();
-						player.skin = 4;
-						ARX_PLAYER_Restore_Skin();
-						m_cheatSkinButtonClickCount = 0;
-						SP_HEAD = 1;
-					} else {
-						if(SP_HEAD) {
-							player.skin = 4;
-							ARX_PLAYER_Restore_Skin();
-							SP_HEAD = 0;
-						}
-						
-						ARX_SOUND_PlayMenu(SND_MENU_CLICK);
-						
-						MenuFader_start(Fade_In, Mode_InGame);
-					}
-				} else {
-					if(DONE)
-						color = Color(255, 255, 255);
-					else
-						color = Color(192, 192, 192);
-				}
+			if(bQuickGenFirstClick) {
+				ARX_PLAYER_MakeAverageHero();
+				bQuickGenFirstClick = false;
 			} else {
-				if(DONE)
-					color = Color(232, 204, 143);
-				else
-					color = Color(192, 192, 192);
+				ARX_PLAYER_QuickGeneration();
 			}
 			
-			if(m_cheatSkinButtonClickCount < 0)
-				color = Color(255, 0, 255);
+			player.skin = checked_range_cast<char>(iSkin);
+		}
+		
+		color = Color(255, 255, 255);
+	}
+	else
+		color = Color(232, 204, 143);
+	
+	pTextManage->AddText(hFontMenu, str_button_quickgen, Vec2i(pos), color);
+	
+	//---------------------------------------------------------------------
+	// Button SKIN
+	pos.x = g_size.width() * 0.5f;
+	
+	const Rectf skinButtonMouseTestRect(
+		pos,
+		size.x,
+		size.y
+	);
+	
+	if(skinButtonMouseTestRect.contains(Vec2f(DANAEMouse))) {
+		SpecialCursor = CURSOR_INTERACTION_ON;
+		FLYING_OVER = BUTTON_SKIN;
+		
+		if(eeMouseUp1()) {
+			m_cheatSkinButtonClickCount++;
+			ARX_SOUND_PlayMenu(SND_MENU_CLICK);
+			player.skin++;
 			
-			pTextManage->AddText(hFontMenu, str_button_done, Vec2i(pos), color);
+			if(player.skin > 3)
+				player.skin = 0;
+			
+			ARX_PLAYER_Restore_Skin();
+		}
+		
+		color = Color(255, 255, 255);
+	}
+	else
+		color = Color(232, 204, 143);
+	
+	pTextManage->AddText(hFontMenu, str_button_skin, Vec2i(pos), color);
+	
+	//---------------------------------------------------------------------
+	// Button DONE
+	pos.x = g_size.width() - (g_size.width() - 513 * g_sizeRatio.x) * 0.5f - 40 * g_sizeRatio.x;
+	
+	const Rectf doneButtonMouseTestRect(
+		pos,
+		size.x,
+		size.y
+	);
+	
+	if(doneButtonMouseTestRect.contains(Vec2f(DANAEMouse))) {
+		if(DONE)
+			SpecialCursor = CURSOR_INTERACTION_ON;
+		
+		FLYING_OVER = BUTTON_DONE;
+		
+		if(DONE && eeMouseUp1()) {
+			if(m_cheatSkinButtonClickCount == 8 && m_cheatQuickGenButtonClickCount == 10) {
+				m_cheatSkinButtonClickCount = -2;
+			} else if(m_cheatSkinButtonClickCount == -1) {
+				ARX_PLAYER_MakeSpHero();
+				player.skin = 4;
+				ARX_PLAYER_Restore_Skin();
+				m_cheatSkinButtonClickCount = 0;
+				SP_HEAD = 1;
+			} else {
+				if(SP_HEAD) {
+					player.skin = 4;
+					ARX_PLAYER_Restore_Skin();
+					SP_HEAD = 0;
+				}
+				
+				ARX_SOUND_PlayMenu(SND_MENU_CLICK);
+				
+				MenuFader_start(Fade_In, Mode_InGame);
+			}
+		} else {
+			if(DONE)
+				color = Color(255, 255, 255);
+			else
+				color = Color(192, 192, 192);
+		}
+	} else {
+		if(DONE)
+			color = Color(232, 204, 143);
+		else
+			color = Color(192, 192, 192);
+	}
+	
+	if(m_cheatSkinButtonClickCount < 0)
+		color = Color(255, 0, 255);
+	
+	pTextManage->AddText(hFontMenu, str_button_done, Vec2i(pos), color);
 	}
 	
 	EERIE_LIGHT * light = lightHandleGet(torchLightHandle);
