be994bfb1d23a96b3bb83820075f7b71a6c89750
Daniel Scharrer <daniel@constexpr.org>
2017-12-05 18:57:16 +0100

    [clang-tidy] SaveGame: Remove redundant static keywords

--- src/core/SaveGame.cpp
+++ src/core/SaveGame.cpp
@@ -32,10 +32,10 @@
 
 namespace {
 
-static const fs::path SAVEGAME_NAME = "gsave.sav";
-static const fs::path SAVEGAME_DIR = "save";
-static const fs::path SAVEGAME_THUMBNAIL = "gsave.bmp";
-static const std::string QUICKSAVE_ID = "ARX_QUICK_ARX";
+const fs::path SAVEGAME_NAME = "gsave.sav";
+const fs::path SAVEGAME_DIR = "save";
+const fs::path SAVEGAME_THUMBNAIL = "gsave.bmp";
+const std::string QUICKSAVE_ID = "ARX_QUICK_ARX";
 
 enum SaveGameChange {
 	SaveGameRemoved,
@@ -43,7 +43,7 @@ enum SaveGameChange {
 	SaveGameChanged
 };
 
-static int saveTimeCompare(const SaveGame & a, const SaveGame & b) {
+int saveTimeCompare(const SaveGame & a, const SaveGame & b) {
 	return (a.stime > b.stime);
 }
 
