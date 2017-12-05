753c2c7cf19a3e6d90b88f9b5bdae8c0f910f03a
Daniel Scharrer <daniel@constexpr.org>
2017-12-05 20:15:30 +0100

    TextureContainer: Don't use tabs for alignment

--- src/graphics/data/TextureContainer.h
+++ src/graphics/data/TextureContainer.h
@@ -41,7 +41,7 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 ===========================================================================
 */
 // Code: Cyril Meynier
-//       Sébastien Scieux	(JPEG & PNG)
+//       Sébastien Scieux (JPEG & PNG)
 //
 // Copyright (c) 1999 ARKANE Studios SA. All rights reserved
 
