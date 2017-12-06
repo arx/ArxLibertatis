arx/ArxLibertatis

commit dea72f7aca680441cbdf51bd8e278f336c9f2650

[clang-tidy] Projectile: Don't use float for loop variable

src/physics/Projectile.cpp
@@ -491,7 +491,8 @@ void ARX_THROWN_OBJECT_Manage(GameDuration timeDelta)
 					j = 200;
 					need_kill = 1;
 				} else {
-					for(float precision = 0.5f; precision <= 6.f; precision += 0.5f) {
+					for(size_t i = 1; i <= 12; i++) {
+						float precision = float(i) * 0.5f;
 						Sphere sphere;
 						sphere.origin = v0 + projectile.vector * precision * 4.5f;
 						sphere.radius = rad + 3.f;

commit f3eca39f4a4d4e96eee11d5bee6c2edea019de44

Projectile: Fix early loop termination
Was broken in commit dea72f7.

src/physics/Projectile.cpp
@@ -491,8 +491,8 @@ void ARX_THROWN_OBJECT_Manage(GameDuration timeDelta)
 					j = 200;
 					need_kill = 1;
 				} else {
-					for(size_t i = 1; i <= 12; i++) {
-						float precision = float(i) * 0.5f;
+					for(size_t k = 1; k <= 12; k++) {
+						float precision = float(k) * 0.5f;
 						Sphere sphere;
 						sphere.origin = v0 + projectile.vector * precision * 4.5f;
 						sphere.radius = rad + 3.f;
@@ -573,7 +573,7 @@ void ARX_THROWN_OBJECT_Manage(GameDuration timeDelta)
 									projectile.flags &= ~ATO_MOVING;
 									projectile.velocity = 0.f;
 									need_kill = 1;
-									precision = 500.f;
+									k = 1000;
 									j = 200;
 								}
 							}