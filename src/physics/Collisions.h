4bb8bf4dbfb1fcb875ae9cdd4a58da4135b298c7
Daniel Scharrer <daniel@constexpr.org>
2017-12-31 13:10:42 +0100

    Collisions: Add missing whitespace

--- src/physics/Collisions.h
+++ src/physics/Collisions.h
@@ -57,18 +57,18 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 
 // ARX_COLLISIONS flags (cylinder move)
 enum CollisionFlag {
-	CFLAG_LEVITATE          = (1<<0),
-	CFLAG_NO_INTERCOL       = (1<<1),
-	CFLAG_EASY_SLIDING      = (1<<3),
-	CFLAG_CLIMBING          = (1<<4),
-	CFLAG_JUST_TEST         = (1<<5),
-	CFLAG_NPC               = (1<<6),
-	CFLAG_PLAYER            = (1<<7),
-	CFLAG_RETURN_HEIGHT     = (1<<8),
-	CFLAG_EXTRA_PRECISION   = (1<<9),
-	CFLAG_COLLIDE_NOCOL     = (1<<12),
-	CFLAG_NO_NPC_COLLIDE    = (1<<13),
-	CFLAG_NO_HEIGHT_MOD     = (1<<14)
+	CFLAG_LEVITATE          = 1 << 0,
+	CFLAG_NO_INTERCOL       = 1 << 1,
+	CFLAG_EASY_SLIDING      = 1 << 3,
+	CFLAG_CLIMBING          = 1 << 4,
+	CFLAG_JUST_TEST         = 1 << 5,
+	CFLAG_NPC               = 1 << 6,
+	CFLAG_PLAYER            = 1 << 7,
+	CFLAG_RETURN_HEIGHT     = 1 << 8,
+	CFLAG_EXTRA_PRECISION   = 1 << 9,
+	CFLAG_COLLIDE_NOCOL     = 1 << 12,
+	CFLAG_NO_NPC_COLLIDE    = 1 << 13,
+	CFLAG_NO_HEIGHT_MOD     = 1 << 14
 };
 DECLARE_FLAGS(CollisionFlag, CollisionFlags)
 DECLARE_FLAGS_OPERATORS(CollisionFlags)
@@ -84,12 +84,12 @@ bool ARX_COLLISION_Move_Cylinder(IO_PHYSICS * ip, Entity * io, float MOVE_CYLIND
 float CheckAnythingInCylinder(const Cylinder & cyl, Entity * ioo, long flags = 0);
 
 enum CheckAnythingInSphereFlag {
-	 CAS_NO_NPC_COL        = (1<<0),
-	 CAS_NO_SAME_GROUP     = (1<<1),
-	 CAS_NO_BACKGROUND_COL = (1<<2),
-	 CAS_NO_ITEM_COL       = (1<<3),
-	 CAS_NO_FIX_COL        = (1<<4),
-	 CAS_NO_DEAD_COL       = (1<<5)
+	 CAS_NO_NPC_COL        = 1 << 0,
+	 CAS_NO_SAME_GROUP     = 1 << 1,
+	 CAS_NO_BACKGROUND_COL = 1 << 2,
+	 CAS_NO_ITEM_COL       = 1 << 3,
+	 CAS_NO_FIX_COL        = 1 << 4,
+	 CAS_NO_DEAD_COL       = 1 << 5
 };
 DECLARE_FLAGS(CheckAnythingInSphereFlag, CASFlags)
 DECLARE_FLAGS_OPERATORS(CASFlags)
