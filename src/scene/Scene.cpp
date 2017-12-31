arx/ArxLibertatis

commit a3680239ad8bbd0f6ff96463d39ac6f94215d6d4

Mesh: Don't flip y of untransformed vertices

src/scene/Scene.cpp
@@ -698,7 +698,6 @@ static EERIE_FRUSTRUM CreateScreenFrustrum() {
 	plane.b = matres[1][3] - matres[1][0];
 	plane.c = matres[2][3] - matres[2][0];
 	plane.d = matres[3][3] - matres[3][0];
-	plane.b = -plane.b;
 	
 	normalizePlane(plane);
 	}
@@ -710,7 +709,6 @@ static EERIE_FRUSTRUM CreateScreenFrustrum() {
 	plane.b = matres[1][3] + matres[1][0];
 	plane.c = matres[2][3] + matres[2][0];
 	plane.d = matres[3][3] + matres[3][0];
-	plane.b = -plane.b;
 	
 	normalizePlane(plane);
 	}
@@ -722,7 +720,6 @@ static EERIE_FRUSTRUM CreateScreenFrustrum() {
 	plane.b = matres[1][3] - matres[1][1];
 	plane.c = matres[2][3] - matres[2][1];
 	plane.d = matres[3][3] - matres[3][1];
-	plane.b = -plane.b;
 	
 	normalizePlane(plane);
 	}
@@ -734,7 +731,6 @@ static EERIE_FRUSTRUM CreateScreenFrustrum() {
 	plane.b = matres[1][3] + matres[1][1];
 	plane.c = matres[2][3] + matres[2][1];
 	plane.d = matres[3][3] + matres[3][1];
-	plane.b = -plane.b;
 	
 	normalizePlane(plane);
 	}
@@ -746,7 +742,6 @@ static EERIE_FRUSTRUM CreateScreenFrustrum() {
 	plane.b = matres[1][3] + matres[1][2];
 	plane.c = matres[2][3] + matres[2][2];
 	plane.d = matres[3][3] + matres[3][2];
-	plane.b = -plane.b;
 	
 	normalizePlane(plane);
 	}

src/game/Camera.cpp
@@ -67,7 +67,7 @@ static void EERIE_CreateMatriceProj(float width, float height, EERIE_CAMERA * ca
 	                               0,-1, 0, 0,
 	                               0, 0, 1, 0,
 	                               0, 0, 0, 1);
-	GRenderer->SetViewMatrix(flip_y * cam->orgTrans.worldToView * flip_y);
+	GRenderer->SetViewMatrix(flip_y * cam->orgTrans.worldToView);
 
 	cam->ProjectionMatrix[0][0] *= width * .5f;
 	cam->ProjectionMatrix[1][1] *= height * .5f;

src/graphics/data/Mesh.cpp
@@ -1500,7 +1500,7 @@ void ComputePortalVertexBuffer() {
 				}
 				
 				vertex->p.x = poly.v[0].p.x;
-				vertex->p.y = -(poly.v[0].p.y);
+				vertex->p.y = poly.v[0].p.y;
 				vertex->p.z = poly.v[0].p.z;
 				vertex->color = poly.v[0].color;
 				vertex->uv = poly.v[0].uv + texture->hd;
@@ -1508,7 +1508,7 @@ void ComputePortalVertexBuffer() {
 				poly.uslInd[0] = index++;
 				
 				vertex->p.x = poly.v[1].p.x;
-				vertex->p.y = -(poly.v[1].p.y);
+				vertex->p.y = poly.v[1].p.y;
 				vertex->p.z = poly.v[1].p.z;
 				vertex->color = poly.v[1].color;
 				vertex->uv = poly.v[1].uv + texture->hd;
@@ -1516,7 +1516,7 @@ void ComputePortalVertexBuffer() {
 				poly.uslInd[1] = index++;
 				
 				vertex->p.x = poly.v[2].p.x;
-				vertex->p.y = -(poly.v[2].p.y);
+				vertex->p.y = poly.v[2].p.y;
 				vertex->p.z = poly.v[2].p.z;
 				vertex->color = poly.v[2].color;
 				vertex->uv = poly.v[2].uv + texture->hd;
@@ -1525,7 +1525,7 @@ void ComputePortalVertexBuffer() {
 				
 				if(poly.type & POLY_QUAD) {
 					vertex->p.x = poly.v[3].p.x;
-					vertex->p.y = -(poly.v[3].p.y);
+					vertex->p.y = poly.v[3].p.y;
 					vertex->p.z = poly.v[3].p.z;
 					vertex->color = poly.v[3].color;
 					vertex->uv = poly.v[3].uv + texture->hd;

commit 397f999e5ce84c147755226ec594560cab1afe28

Scene: Fix lava and water animation
This was broken in commit a368023.

src/scene/Scene.cpp
@@ -852,9 +852,7 @@ static void RenderWater() {
 		}
 		
 		for(int j = 0; j < iNbVertex; ++j) {
-			pVertex->p.x = ep->v[j].p.x;
-			pVertex->p.y = -ep->v[j].p.y;
-			pVertex->p.z = ep->v[j].p.z;
+			pVertex->p = ep->v[j].p;
 			pVertex->color = Color(80, 80, 80, 255).toRGBA();
 			
 			for(int i = 0; i < FTVU_STEP_COUNT; ++i) {
@@ -960,9 +958,7 @@ static void RenderLava() {
 		}
 		
 		for(int j = 0; j < iNbVertex; ++j) {
-			pVertex->p.x = ep->v[j].p.x;
-			pVertex->p.y = -ep->v[j].p.y;
-			pVertex->p.z = ep->v[j].p.z;
+			pVertex->p = ep->v[j].p;
 			pVertex->color = Color(102, 102, 102, 255).toRGBA();
 			for(int i = 0; i < FTVU_STEP_COUNT; ++i) {
 				Vec2f uv = CalculateLavaDisplacement(ep, time, j, i);