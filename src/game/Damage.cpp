62b566746c83352e239864aea7eaf2be8edb91be
Daniel Scharrer <daniel@constexpr.org>
2018-01-08 17:08:14 +0100

    Damage: Use NULL sender when breaking weapons

--- src/game/Damage.cpp
+++ src/game/Damage.cpp
@@ -1476,7 +1476,7 @@ void ARX_DAMAGES_DurabilityLoss(Entity * io, float loss) {
 	io->durability -= loss;
 	
 	if(io->durability <= 0) {
-		SendIOScriptEvent(EVENT_SENDER, io, SM_BREAK);
+		SendIOScriptEvent(NULL, io, SM_BREAK);
 	}
 	
 }
