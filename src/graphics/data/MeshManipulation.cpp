arx/ArxLibertatis

commit 3d3ec1ffc46673ec31f1304691623ae94537eddb

MeshManipulation: Merge loops

src/graphics/data/MeshManipulation.cpp
@@ -105,17 +105,7 @@ void EERIE_MESH_TWEAK_Skin(EERIE_3DOBJ * obj, const res::path & s1, const res::p
 		
 		for(size_t i = 0; i < obj->texturecontainer.size(); i++) {
 			if(obj->originaltextures[i] == skintochange) {
-				skintochange = obj->texturecontainer[i]->m_texName;
-				break;
-			}
-		}
-		
-		TextureContainer * tex2 = TextureContainer::Find(skintochange);
-		if(tex2) {
-			for(size_t i = 0; i < obj->texturecontainer.size(); i++) {
-				if(obj->texturecontainer[i] == tex2) {
-					obj->texturecontainer[i] = tex;
-				}
+				obj->texturecontainer[i] = tex;
 			}
 		}
 		

commit a412b29c7266fda5d62c08c33fabaa4a4d9cb449

MeshManipulation: Fix texture replacement matching rules
This was broken in commit 3d3ec1f which changed the behavior to only
match against the untweaked texture name. Some scripts supply the
current tweaked texture name instead.

Fixes: issue #1112

src/graphics/data/MeshManipulation.cpp
@@ -104,8 +104,21 @@ void EERIE_MESH_TWEAK_Skin(EERIE_3DOBJ * obj, const res::path & s1, const res::p
 	
 	arx_assert(obj->originaltextures.size() == obj->texturecontainer.size());
 	
+	bool found = false;
+	
 	for(size_t i = 0; i < obj->texturecontainer.size(); i++) {
 		if(obj->originaltextures[i] == skintochange) {
+			obj->texturecontainer[i] = tex;
+			found = true;
+		}
+	}
+	
+	if(found) {
+		return;
+	}
+	
+	for(size_t i = 0; i < obj->texturecontainer.size(); i++) {
+		if(obj->texturecontainer[i]->m_texName == skintochange) {
 			obj->texturecontainer[i] = tex;
 		}
 	}