3db29d60f6e28f0e34e206b58f34bc4ea02c4c48
Daniel Scharrer <daniel@constexpr.org>
2018-01-13 21:43:50 +0100

    SpellsLvl09: Code style

--- src/game/magic/spells/SpellsLvl09.cpp
+++ src/game/magic/spells/SpellsLvl09.cpp
@@ -401,7 +401,7 @@ void NegateMagicSpell::LaunchAntiMagicField() {
 	for(size_t i = 0; i < MAX_SPELLS; i++) {
 		SpellBase * spell = spells[SpellHandle(i)];
 		
-		if(   !spell || this == spell || m_level < spell->m_level) {
+		if(!spell || this == spell || m_level < spell->m_level) {
 			continue;
 		}
 		
