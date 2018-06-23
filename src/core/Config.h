7f85f4c1f2a79788e7d05ba88b517ad3f974ce07
Daniel Scharrer <daniel@constexpr.org>
2018-06-23 06:22:47 +0200

    Config: Return if a binding was changed

--- src/core/Config.h
+++ src/core/Config.h
@@ -230,7 +230,7 @@ public:
 	
 public:
 	
-	void setActionKey(ControlAction actionId, size_t index, InputKeyId key);
+	bool setActionKey(ControlAction actionId, size_t index, InputKeyId key);
 	void setDefaultActionKeys();
 	
 	/*!
