18f14c6394f0b87b53fe2411f6b3e188624ae262
Daniel Scharrer <daniel@constexpr.org>
2018-06-24 13:21:24 +0200

    TextWidget: Make conversion to float explicit

--- src/gui/widget/TextWidget.cpp
+++ src/gui/widget/TextWidget.cpp
@@ -35,7 +35,7 @@ TextWidget::TextWidget(Font * font, const std::string & text, Vec2f pos)
 void TextWidget::setText(const std::string & text) {
 	m_text = text;
 	Vec2i textSize = m_font->getTextSize(m_text);
-	m_rect = Rectf(m_rect.topLeft(), textSize.x + 1, textSize.y + 1);
+	m_rect = Rectf(m_rect.topLeft(), float(textSize.x + 1), float(textSize.y + 1));
 }
 
 bool TextWidget::click() {
