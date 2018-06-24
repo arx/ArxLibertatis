c6039d59ca0fcaf62e64ae63b5bd3c551d873857
Daniel Scharrer <daniel@constexpr.org>
2018-06-24 15:06:14 +0200

    GameSound: Remove return value

--- src/scene/GameSound.h
+++ src/scene/GameSound.h
@@ -223,7 +223,8 @@ long ARX_SOUND_PlayCollision(const std::string & name1, const std::string & name
 audio::SourceId ARX_SOUND_PlayScript(const res::path & name, const Entity * io = NULL, float pitch = 1.0F, SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
 void ARX_SOUND_PlayAnim(audio::SourceId & sample_id, const Vec3f * position = NULL);
 audio::SourceId ARX_SOUND_PlayCinematic(const res::path & name, bool isSpeech);
-long ARX_SOUND_PlayMenu(audio::SourceId & sample_id, float pitch = 1.0F, SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
+void ARX_SOUND_PlayMenu(audio::SourceId & sample_id, float pitch = 1.0F,
+                        SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
 bool ARX_SOUND_IsPlaying(audio::SourceId & sample_id);
 GameDuration ARX_SOUND_GetDuration(audio::SampleId & sample_id);
 
