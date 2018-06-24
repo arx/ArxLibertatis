e64518eb40bfd6e6316fffee4c32d001955e2094
Daniel Scharrer <daniel@constexpr.org>
2018-06-24 13:50:31 +0200

    GameSound: Use correct type

--- src/scene/GameSound.h
+++ src/scene/GameSound.h
@@ -222,7 +222,7 @@ long ARX_SOUND_PlayCollision(const std::string & name1, const std::string & name
 
 long ARX_SOUND_PlayScript(const res::path & name, const Entity * io = NULL, float pitch = 1.0F, SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
 long ARX_SOUND_PlayAnim(audio::SourceId & sample_id, const Vec3f * position = NULL);
-long ARX_SOUND_PlayCinematic(const res::path & name, bool isSpeech);
+audio::SourceId ARX_SOUND_PlayCinematic(const res::path & name, bool isSpeech);
 long ARX_SOUND_PlayMenu(audio::SourceId & sample_id, float pitch = 1.0F, SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
 long ARX_SOUND_IsPlaying(audio::SourceId & sample_id);
 GameDuration ARX_SOUND_GetDuration(audio::SampleId & sample_id);
