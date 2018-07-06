3bbe7a7355cd3056375f1cf355ef02e31ba9a3b6
Daniel Scharrer <daniel@constexpr.org>
2018-07-07 01:23:16 +0200

    SpellRecognition: Remove return after ARX_DEAD_CODE()

--- src/game/magic/SpellRecognition.cpp
+++ src/game/magic/SpellRecognition.cpp
@@ -304,26 +304,15 @@ void RuneRecognitionAlt::findKeyPoints(std::vector<Vec2f> &in) {
  */
 char RuneRecognitionAlt::angle2arx(int dir) {
 	switch(dir) {
-		case 0:
-			return '6';
-		case 1:
-			return '9';
-		case 2:
-			return '8';
-		case 3:
-			return '7';
-		case 4:
-			return '4';
-		case 5:
-			return '1';
-		case 6:
-			return '2';
-		case 7:
-			return '3';
-		default: {
-			ARX_DEAD_CODE();
-			return '\0';
-		}
+		case 0: return '6';
+		case 1: return '9';
+		case 2: return '8';
+		case 3: return '7';
+		case 4: return '4';
+		case 5: return '1';
+		case 6: return '2';
+		case 7: return '3';
+		default: ARX_DEAD_CODE();
 	}
 }
 
@@ -332,26 +321,15 @@ char RuneRecognitionAlt::angle2arx(int dir) {
  */
 int RuneRecognitionAlt::arx2angle(char dir) {
 	switch(dir) {
-		case '6':
-			return 0;
-		case '9':
-			return 1;
-		case '8':
-			return 2;
-		case '7':
-			return 3;
-		case '4':
-			return 4;
-		case '1':
-			return 5;
-		case '2':
-			return 6;
-		case '3':
-			return 7;
-		default: {
-			ARX_DEAD_CODE();
-			return -1;
-		}
+		case '6': return 0;
+		case '9': return 1;
+		case '8': return 2;
+		case '7': return 3;
+		case '4': return 4;
+		case '1': return 5;
+		case '2': return 6;
+		case '3': return 7;
+		default: ARX_DEAD_CODE();
 	}
 }
 
