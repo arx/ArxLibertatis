arx/ArxLibertatis

commit 3b0a74632dc8bba0631fe8ba31bd7447a2947e54

Remove unnecessary inband error code

src/scene/GameSound.cpp
@@ -713,7 +713,7 @@ audio::SourcedSample ARX_SOUND_PlayScript(const res::path & name, const Entity *
 		if(loop != ARX_SOUND_PLAY_LOOPED) {
 			if (g_camera && fartherThan(g_camera->m_pos, channel.position, ARX_SOUND_REFUSE_DISTANCE)) {
 				// TODO the sample will never be freed!
-				return ARX_SOUND_TOO_FAR;
+				return audio::SourcedSample();
 			}
 		}
 	} else {

src/script/ScriptedConversation.cpp
@@ -133,7 +133,7 @@ public:
 			}
 			
 			if(unique) {
-				io->sound = (num == ARX_SOUND_TOO_FAR) ? audio::SourcedSample() : num;
+				io->sound = num;
 			}
 			
 			if(num == audio::SourcedSample()) {

commit 34e2610751743883fb5c664f7bad32fe98619930

Fix invalid failure, broken in 3b0a7463

src/scene/GameSound.cpp
@@ -692,7 +692,7 @@ long ARX_SOUND_PlayCollision(const std::string & name1, const std::string & name
 	return (long)(channel.pitch * length);
 }
 
-audio::SourcedSample ARX_SOUND_PlayScript(const res::path & name, const Entity * io,
+audio::SourcedSample ARX_SOUND_PlayScript(const res::path & name, bool & tooFar, const Entity * io,
                                      float pitch, SoundLoopMode loop) {
 	
 	if(!g_soundInitialized) {
@@ -716,6 +716,7 @@ audio::SourcedSample ARX_SOUND_PlayScript(const res::path & name, const Entity *
 		if(loop != ARX_SOUND_PLAY_LOOPED) {
 			if (g_camera && fartherThan(g_camera->m_pos, channel.position, ARX_SOUND_REFUSE_DISTANCE)) {
 				// TODO the sample will never be freed!
+				tooFar = true;
 				return audio::SourcedSample();
 			}
 		}

src/scene/GameSound.h
@@ -222,7 +222,7 @@ audio::SourcedSample ARX_SOUND_PlaySpeech(const res::path & name, bool * tooFar 
 long ARX_SOUND_PlayCollision(Material mat1, Material mat2, float volume, float power, const Vec3f & position, Entity * source);
 long ARX_SOUND_PlayCollision(const std::string & name1, const std::string & name2, float volume, float power, const Vec3f & position, Entity * source);
 
-audio::SourcedSample ARX_SOUND_PlayScript(const res::path & name, const Entity * io = NULL, float pitch = 1.0F, SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
+audio::SourcedSample ARX_SOUND_PlayScript(const res::path & name, bool &tooFar, const Entity * io = NULL, float pitch = 1.0F, SoundLoopMode loop = ARX_SOUND_PLAY_ONCE);
 void ARX_SOUND_PlayAnim(audio::SourcedSample & sample_id, const Vec3f * position = NULL);
 audio::SourcedSample ARX_SOUND_PlayCinematic(const res::path & name, bool isSpeech);
 bool ARX_SOUND_IsPlaying(audio::SourcedSample & sample_id);

src/script/ScriptedConversation.cpp
@@ -125,18 +125,19 @@ public:
 			}
 			
 			audio::SourcedSample num = audio::SourcedSample();
+			bool tooFar = false;
 			// TODO(broken-scripts) should be a flag instead of depending on the event
 			if(no_pos || SM_INVENTORYUSE == context.getMessage()) {
-				num = ARX_SOUND_PlayScript(sample, NULL, pitch, loop);
+				num = ARX_SOUND_PlayScript(sample, tooFar, NULL, pitch, loop);
 			} else {
-				num = ARX_SOUND_PlayScript(sample, io, pitch, loop);
+				num = ARX_SOUND_PlayScript(sample, tooFar, io, pitch, loop);
 			}
 			
 			if(unique) {
 				io->sound = num;
 			}
 			
-			if(num == audio::SourcedSample()) {
+			if(!tooFar && num == audio::SourcedSample()) {
 				ScriptWarning << "unable to load sound file " << sample;
 				return Failed;
 			}