d8b52babdd8f48b57c5ab3eda00fe0b314a848e5
Eli2 <FabianRoth64@googlemail.com>
2018-08-01 03:50:17 +0200

    DebugPanel: Remove parameter const qualifiers

--- src/gui/debug/DebugPanel.cpp
+++ src/gui/debug/DebugPanel.cpp
@@ -33,22 +33,22 @@
 namespace arx {
 namespace debug {
 
-std::stringstream &operator <<(std::stringstream &ss, const Vec2i value) {
+std::stringstream &operator <<(std::stringstream &ss, Vec2i value) {
 	ss << boost::str(boost::format("(%d, %d)") % value.x % value.y);
 	return ss;
 }
 
-std::stringstream &operator <<(std::stringstream &ss, const Vec3f value) {
+std::stringstream &operator <<(std::stringstream &ss, Vec3f value) {
 	ss << boost::str(boost::format("%4.2f %4.2f %4.2f") % value.x % value.y % value.z);
 	return ss;
 }
 
-std::stringstream &operator <<(std::stringstream &ss, const Anglef value) {
+std::stringstream &operator <<(std::stringstream &ss, Anglef value) {
 	ss << boost::str(boost::format("%4.2f %4.2f %4.2f") % value.getPitch() % value.getYaw() % value.getRoll());
 	return ss;
 }
 
-std::stringstream &operator <<(std::stringstream &ss, const ResourcePool value) {
+std::stringstream &operator <<(std::stringstream &ss, ResourcePool value) {
 	ss << boost::str(boost::format("%4.2f/%4.2f") % value.current % value.max);
 	return ss;
 }
