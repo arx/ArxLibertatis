arx/ArxLibertatis

commit a69e22bd8db62b66755956528a0c474ef7473768

Note: Don't rely on GLM default constructors

src/gui/Note.cpp
@@ -109,8 +109,8 @@ void Note::calculateLayout() {
 	Vec2f newTextStart;
 	Vec2f newTextEnd;
 	
-	Vec2f prevButtonOffset;
-	Vec2f nextButtonOffset;
+	Vec2f prevButtonOffset(0.f);
+	Vec2f nextButtonOffset(0.f);
 	
 	Vec2f scale = Vec2f(g_playerBook.getScale());
 	
@@ -167,7 +167,7 @@ void Note::calculateLayout() {
 			nextButtonOffset = Vec2f(-15.f, -6.f);
 		}
 		
-		case Undefined: break; // Cannot handle notes of undefined type.
+		case Undefined: return; // Cannot handle notes of undefined type.
 	}
 	
 	if(m_type == QuestBook) {

commit 635412c6c87bb4cd6b070434a5a983b02fc3afcc

Note: Add missing break statements
This fixes quest book rendering broken in commit a69e22b.

src/gui/Note.cpp
@@ -97,6 +97,7 @@ void Note::loadTextures() {
 			m_background = TextureContainer::LoadUI("graph/interface/book/questbook");
 			m_prevPage = TextureContainer::LoadUI("graph/interface/book/left_corner_original");
 			m_nextPage = TextureContainer::LoadUI("graph/interface/book/right_corner_original");
+			break;
 		}
 		
 		case Undefined: break; // Cannot handle notes of undefined type.
@@ -165,6 +166,7 @@ void Note::calculateLayout() {
 			m_maxPages = std::numeric_limits<size_t>::max();
 			prevButtonOffset = Vec2f(8.f, -6.f);
 			nextButtonOffset = Vec2f(-15.f, -6.f);
+			break;
 		}
 		
 		case Undefined: return; // Cannot handle notes of undefined type.