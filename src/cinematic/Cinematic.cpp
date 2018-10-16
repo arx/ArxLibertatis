arx/ArxLibertatis

commit 9cc2f481e27366d57055c5568d80e21c57086d66

Cinematic: Use color conversion constructor

src/cinematic/Cinematic.cpp
@@ -155,23 +155,15 @@ static Color CalculLight(CinematicLight * light, Vec2f pos, Color col) {
 	                     + (light->pos.y - pos.y) * (light->pos.y - pos.y));
 	
 	if(ra > light->fallout) {
-		return (col * LightRND);
+		return col * LightRND;
 	}
 	
-	Color3f color;
-	
-	if(ra < light->fallin) {
-		color = light->color * LightRND;
-	} else {
-		ra = (light->fallout - ra) / (light->fallout - light->fallin);
-		color = light->color * (LightRND * ra);
+	Color3f color = light->color * (LightRND / 255.f);
+	if(ra >= light->fallin) {
+		color = color * ((light->fallout - ra) / (light->fallout - light->fallin));
 	}
 	
-	Color in = col;
-	in.r = std::min(in.r + (int)color.r, 255);
-	in.g = std::min(in.g + (int)color.g, 255);
-	in.b = std::min(in.b + (int)color.b, 255);
-	return in;
+	return Color(Color3f(col) + color);
 }
 
 static Vec3f TransformLocalVertex(const Vec2f & vbase, const Vec3f & LocalPos, float LocalSin, float LocalCos) {

commit 3e8d61b233312b08dfc745fa240eca9983db20ed

Cinematic: Preserve vertex alpha in lighting calculation
This was broken in commit 9cc2f48.

src/cinematic/Cinematic.cpp
@@ -163,7 +163,7 @@ static Color CalculLight(CinematicLight * light, Vec2f pos, Color col) {
 		color = color * ((light->fallout - ra) / (light->fallout - light->fallin));
 	}
 	
-	return Color(Color3f(col) + color);
+	return Color(Color3f(col) + color, col.a);
 }
 
 static Vec3f TransformLocalVertex(const Vec2f & vbase, const Vec3f & LocalPos, float LocalSin, float LocalCos) {