6b647834d352d8b1e7f384a6bbc1a3ca438fd194
Daniel Scharrer <daniel@constexpr.org>
2019-01-04 16:00:28 +0100

    Update copyright years

--- src/io/log/ColorLogger.h
+++ src/io/log/ColorLogger.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013 Arx Libertatis Team (see the AUTHORS file)
+ * Copyright 2011-2017 Arx Libertatis Team (see the AUTHORS file)
  *
  * This file is part of Arx Libertatis.
  *
--- tests/util/StringTest.h
+++ tests/util/StringTest.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 Arx Libertatis Team (see the AUTHORS file)
+ * Copyright 2014-2017 Arx Libertatis Team (see the AUTHORS file)
  *
  * This file is part of Arx Libertatis.
  *
