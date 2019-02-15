d4d8869eb3a2f473c40c705396b24c59e3fe35bd
Eli2 <FabianRoth64@googlemail.com>
2019-02-16 00:52:57 +0100

    qtcreator: improve pretty printers

--- plugins/qtcreator/arx_printer.py
+++ plugins/qtcreator/arx_printer.py
@@ -1,3 +1,4 @@
+from dumper import *
 
 def arx_vec2_helper(value):
     return (value['#1']['x'].value(), value['#2']['y'].value())
@@ -30,7 +31,6 @@ def qdump__Anglef(d, value):
     pitch = value['m_pitch'].value()
     roll  = value['m_roll'].value()
     valueStr = "{:10.4f} {:10.4f} {:10.4f}".format(yaw, pitch, roll)
-    
     d.putType("Anglef")
     d.putValue(valueStr)
     d.putPlainChildren(value)
@@ -67,64 +67,30 @@ def qdump__Color3f(d, value):
 
 def qdump__Entity(d, value):
     index = value['m_index'].value()
-    
     d.putType("Entity")
     d.putValue("Idx: " + str(index))
     d.putPlainChildren(value)
 
 # HandleType ==================================================================
+#            src/util/HandleType.h
 
-def arx_HandleType_helper(d, v):
+def qdump__HandleType(d, v):
     handleType = v.type.stripTypedefs()
-    templateArgs = handleType.templateArguments()
-    # TODO how to do asserts ?
-    #d.check(len(templateArgs) == 3)
-    invalidValue = templateArgs[2]
+    invalidValue = handleType.templateArgument(2)
     value = v['t'].value()
-    d.putValue(value if (value != invalidValue) else 'Invalid ('+str(value)+')')
+    if value == invalidValue:
+        d.putValue('Invalid ('+str(value)+')')
+    else:
+        d.putValue(value)
     d.putPlainChildren(v)
-
-def qdump__MixerId(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__EnvId(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__AmbianceId(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__SavegameHandle(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__EntityHandle(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__SpellHandle(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__PrecastHandle(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__DamageHandle(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__ActionPoint(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__ObjSelection(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__ObjVertGroup(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__ObjVertHandle(d, v):
-    arx_HandleType_helper(d, v)
-def qdump__LightHandle(d, v):
-    arx_HandleType_helper(d, v)
 
 # Time Types ==================================================================
 #            src/core/TimeTypes.h
 
-def arx_TimeType_helper(d, v):
+def qdump__InstantType(d, v):
     d.putValue(v['t'].value())
     d.putPlainChildren(v)
 
-def qdump__GameInstant(d, v):
-    arx_TimeType_helper(d, v)
-def qdump__GameDuration(d, v):
-    arx_TimeType_helper(d, v)
-def qdump__PlatformInstant(d, v):
-    arx_TimeType_helper(d, v)
-def qdump__PlatformDuration(d, v):
-    arx_TimeType_helper(d, v)
-def qdump__AnimationDuration(d, v):
-    arx_TimeType_helper(d, v)
+def qdump__DurationType(d, v):
+    d.putValue(v['t'].value())
+    d.putPlainChildren(v)
