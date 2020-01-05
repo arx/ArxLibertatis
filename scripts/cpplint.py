8f464e29400e1094d5b3bf2e2f46d0cdb7a90776
Daniel Scharrer <daniel@constexpr.org>
2020-01-05 15:31:50 +0100

    cpplint: Don't warn on readdir
    readdir is threadsafe on glibc while readdir_r is fragile because
    we need to guess the size to allocate beforehand and thus marked
    deprecated.

--- scripts/cpplint.py
+++ scripts/cpplint.py
@@ -1252,7 +1252,6 @@ threading_list = (
     ('gmtime(', 'gmtime_r('),
     ('localtime(', 'localtime_r('),
     ('rand(', 'rand_r('),
-    ('readdir(', 'readdir_r('),
     ('strtok(', 'strtok_r('),
     ('ttyname(', 'ttyname_r('),
     )
