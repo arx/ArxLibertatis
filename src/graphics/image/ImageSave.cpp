7cf8a1e552cf08dfc90de885ec1909249ca0ffe8
Daniel Scharrer <daniel@constexpr.org>
2020-04-13 19:50:07 +0200

    ImageSave: Add alpha channel support

--- src/graphics/image/ImageSave.cpp
+++ src/graphics/image/ImageSave.cpp
@@ -41,6 +41,18 @@ void writeU32LE(fs::ofstream & f, u32 x) {
 	f.write(buffer, 4);
 }
 
+void writePadding(fs::ofstream & f, size_t size) {
+	
+	const char padding[] = { 0, 0, 0, 0 };
+	
+	while(size) {
+		size_t n = std::min(sizeof(padding), size);
+		f.write(padding, std::streamsize(n));
+		size -= n;
+	}
+	
+}
+
 } // anonymous namespace
 
 bool Image::save(const fs::path & filename) const {
@@ -58,47 +70,94 @@ bool Image::save(const fs::path & filename) const {
 		return false;
 	}
 	
-	size_t w = getWidth();
-	size_t h = getHeight();
-	size_t c = getNumChannels();
+	const size_t width = getWidth();
+	const size_t height = getHeight();
+	const size_t inputChannels = getNumChannels();
+	const size_t outputChannels = hasAlpha() ? 4 : 3;
 	
-	// File header
+	// BITMAPFILEHEADER
 	const char magic[] = { 'B', 'M' };
 	f.write(magic, sizeof(magic));
-	size_t scanline_pad = size_t((-int(w) * 3) & 3);
-	writeU32LE(f, u32(14 + 40 + (w * 3 + scanline_pad) * h));
+	const size_t fileHeaderSize = 14;
+	const size_t bitmapinfoheaderSize = 40;
+	const size_t colorspaceInfoSize = 48;
+	const size_t bitmapv4headerSize = bitmapinfoheaderSize + 16 + 4 + colorspaceInfoSize;
+	const size_t infoHeaderSize = hasAlpha() ? bitmapv4headerSize : bitmapinfoheaderSize;
+	const size_t scanlinePadding = ((width * outputChannels) % 4 == 0) ? 0 : 4 - (width * outputChannels) % 4;
+	const size_t dataOffset = fileHeaderSize + infoHeaderSize;
+	const size_t fileSize = dataOffset + (width * outputChannels + scanlinePadding) * height;
+	writeU32LE(f, u32(fileSize));
 	writeU16LE(f, 0);
 	writeU16LE(f, 0);
-	writeU32LE(f, 14 + 40);
+	writeU32LE(f, u32(dataOffset));
 	
-	// Bitmap header
-	writeU32LE(f, 40);
-	writeU32LE(f, u32(w));
-	writeU32LE(f, u32(h));
-	writeU16LE(f, 1);
-	writeU16LE(f, 24);
-	writeU32LE(f, 0);
-	writeU32LE(f, 0);
-	writeU32LE(f, 0);
-	writeU32LE(f, 0);
-	writeU32LE(f, 0);
-	writeU32LE(f, 0);
+	arx_assert(!f.good() || f.tellp() == std::streamoff(fileHeaderSize));
+	
+	// BITMAPINFOHEADER for BGR or BITMAPV3INFOHEADER for BGRA
+	const size_t planes = 1;
+	const size_t bpp = outputChannels * 8;
+	const size_t compression = hasAlpha() ? 3 /* BITFIELDS */ : 0 /* RGB */;
+	const size_t imageSize = 0;
+	const size_t resolutionX = 0;
+	const size_t resolutionY = 0;
+	const size_t usedColors = 0;
+	const size_t importantColors = 0;
+	writeU32LE(f, u32(infoHeaderSize));
+	writeU32LE(f, u32(width));
+	writeU32LE(f, u32(height));
+	writeU16LE(f, u16(planes));
+	writeU16LE(f, u16(bpp));
+	writeU32LE(f, u32(compression));
+	writeU32LE(f, u32(imageSize));
+	writeU32LE(f, u32(resolutionX));
+	writeU32LE(f, u32(resolutionY));
+	writeU32LE(f, u32(usedColors));
+	writeU32LE(f, u32(importantColors));
+	if(hasAlpha()) {
+		// Always store as BGRA
+		const size_t redMask   = 0x00ff0000;
+		const size_t greenMask = 0x0000ff00;
+		const size_t blueMask  = 0x000000ff;
+		const size_t alphaMask = 0xff000000;
+		writeU32LE(f, u32(redMask));
+		writeU32LE(f, u32(greenMask));
+		writeU32LE(f, u32(blueMask));
+		writeU32LE(f, u32(alphaMask));
+		// Could use BITMAPV3INFOHEADER and skip the remaining fields
+		// However this is an undocumented variant and not everything can read it
+		const size_t colorspaceType = 0x73524742; // sRGB
+		writeU32LE(f, u32(colorspaceType));
+		writePadding(f, colorspaceInfoSize);
+	}
+	
+	arx_assert(!f.good() || f.tellp() == std::streamoff(dataOffset));
+	
 	if(!f.good()) {
 		return false;
 	}
 	
-	for(size_t y = h; y != 0; y--) {
+	for(size_t y = height; y != 0; y--) {
 		
-		for(size_t x = 0; x < w; x++) {
-			const unsigned char * d = getData() + ((y - 1) * w + x) * c;
+		for(size_t x = 0; x < width; x++) {
+			const unsigned char * d = getData() + ((y - 1) * width + x) * inputChannels;
 			
-			char buffer[3];
+			char buffer[4];
 			switch(getFormat()) {
 				
-				case Format_L8:
-				case Format_A8:
+				case Format_L8: {
+					buffer[0] = buffer[1] = buffer[2] = char(d[0]);
+					break;
+				}
+				
+				case Format_A8: {
+					buffer[0] = buffer[1] = buffer[2] = 0;
+					buffer[3] = char(d[0]);
+					break;
+				}
+				
 				case Format_L8A8: {
 					buffer[0] = buffer[1] = buffer[2] = char(d[0]);
+					buffer[3] = char(d[1]);
 					break;
 				}
 				
@@ -117,20 +176,18 @@ bool Image::save(const fs::path & filename) const {
 				}
 				
 				case Format_R8G8B8A8: {
-					// Composite against pink background
-					const unsigned char bg[3] = { 255, 0, 255 };
-					for(size_t k = 0; k < 3; ++k) {
-						buffer[k] = char(u8(bg[k] + ((d[2 - k] - bg[k]) * d[3]) / 255));
-					}
+					buffer[0] = char(d[2]);
+					buffer[1] = char(d[1]);
+					buffer[2] = char(d[0]);
+					buffer[3] = char(d[3]);
 					break;
 				}
 				
 				case Format_B8G8R8A8: {
-					// Composite against pink background
-					const unsigned char bg[3] = { 255, 0, 255 };
-					for(size_t k = 0; k < 3; ++k) {
-						buffer[k] = char(u8(bg[k] + ((d[k] - bg[k]) * d[3]) / 255));
-					}
+					buffer[0] = char(d[0]);
+					buffer[1] = char(d[1]);
+					buffer[2] = char(d[2]);
+					buffer[3] = char(d[3]);
 					break;
 				}
 				
@@ -141,18 +198,14 @@ bool Image::save(const fs::path & filename) const {
 				
 			}
 			
-			if(!f.write(buffer, 3).good()) {
-				return false;
-			}
+			f.write(buffer, outputChannels);
 			
 		}
 		
-		if(scanline_pad > 0) {
-			const char padding[] = { 0, 0 };
-			arx_assert(scanline_pad <= sizeof(padding));
-			if(!f.write(padding, std::streamsize(scanline_pad)).good()) {
-				return false;
-			}
+		writePadding(f, scanlinePadding);
+		
+		if(!f.good()) {
+			return false;
 		}
 		
 	}
