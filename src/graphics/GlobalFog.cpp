arx/ArxLibertatis

commit 43b6961b01bb870fe70903c5d98fa2e6ed3c7534

GlobalFog: Code style

src/graphics/GlobalFog.cpp
@@ -78,17 +78,11 @@ void ARX_GLOBALMODS_Reset() {
 static float Approach(float current, float desired, float increment) {
 	
 	if(desired > current) {
-		current += increment;
-
-		if (desired < current)
-			current = desired;
-	} else if (desired < current) {
-		current -= increment;
-
-		if (desired > current)
-			current = desired;
+		current = std::min(current + increment, desired);
+	} else if(desired < current) {
+		current = std::min(current - increment, desired);
 	}
-
+	
 	return current;
 }
 
@@ -97,7 +91,7 @@ void ARX_GLOBALMODS_Apply() {
 	ARX_PROFILE_FUNC();
 	
 	float baseinc = g_framedelay;
-	float incdiv1000 = g_framedelay * ( 1.0f / 1000 );
+	float incdiv1000 = g_framedelay * 0.001f;
 	
 	GLOBAL_MODS & current = g_currentFogParameters;
 	GLOBAL_MODS & desired = g_desiredFogParameters;

commit d8fbcafc2cebb7de7d9378625c615fc66138fd46

GlobalFog: Fix fog parameter interpolation
This was broken in commit 43b6961.

Fixes: bug #1446

src/graphics/GlobalFog.cpp
@@ -80,7 +80,7 @@ static float Approach(float current, float desired, float increment) {
 	if(desired > current) {
 		current = std::min(current + increment, desired);
 	} else if(desired < current) {
-		current = std::min(current - increment, desired);
+		current = std::max(current - increment, desired);
 	}
 	
 	return current;