arx/ArxLibertatis

commit 010b39aa4d9c95f4858a7086c667f60a9f33dee4

MagicMissile: Simplify

src/graphics/effects/MagicMissile.cpp
@@ -164,33 +164,19 @@ void CMagicMissile::Render() {
 			newpos = arx::catmullRom(v1, v2, v3, v4, t);
 
 			if(!((fTrail - (i * iBezierPrecision + toto)) > iLength)) {
-				float c;
-
-				if(fTrail < iLength) {
-					c = 1.0f - ((fTrail - (i * iBezierPrecision + toto)) / fTrail);
+				
+				float fsize = 1.f - (fTrail - i * iBezierPrecision + toto) / std::min(fTrail, float(iLength));
+				float alpha = std::max(fsize - 0.2f, 0.2f);
+				Color color(m_trailColor * (glm::clamp(fsize + Random::getf(-0.1f, 0.1f), 0.f, 1.f) * alpha));
+				
+				if(fsize < 0.5f) {
+					fsize = fsize * 6.f;
 				} else {
-					c = 1.0f - ((fTrail - (i * iBezierPrecision + toto)) / (float)iLength);
+					fsize = (1.f - fsize + 0.5f) * 3.f;
 				}
-
-				float fsize = c;
-				float alpha = c - 0.2f;
-
-				if(alpha < 0.2f)
-					alpha = 0.2f;
-
-				c += Random::getf(-0.1f, 0.1f);
 				
-				c = glm::clamp(c, 0.f, 1.f);
-				
-				Color color(m_trailColor * (c * alpha));
-
-				if(fsize < 0.5f)
-					fsize = fsize * 2 * 3;
-				else
-					fsize = (1.0f - fsize + 0.5f) * 2 * (3 * 0.5f);
-
-				float fs = fsize * 6 + Random::getf(0.f, 0.3f);
-				float fe = fsize * 6 + Random::getf(0.f, 0.3f);
+				float fs = fsize * 6.f + Random::getf(0.f, 0.3f);
+				float fe = fsize * 6.f + Random::getf(0.f, 0.3f);
 				Draw3DLineTexNew(mat, lastpos, newpos, color, color, fs, fe);
 			}
 

commit 1ca332e657ad6ed650ccafbae178a573f4946fff

MagicMissile: Fix trail effect
This was broken in commit 010b39a.

src/graphics/effects/MagicMissile.cpp
@@ -164,7 +164,7 @@ void CMagicMissile::Render() {
 
 			if(!((fTrail - (i * iBezierPrecision + toto)) > iLength)) {
 				
-				float fsize = 1.f - (fTrail - i * iBezierPrecision + toto) / std::min(fTrail, float(iLength));
+				float fsize = 1.f - (fTrail - i * iBezierPrecision - toto) / std::min(fTrail, float(iLength));
 				float alpha = std::max(fsize - 0.2f, 0.2f);
 				Color color(m_trailColor * (glm::clamp(fsize + Random::getf(-0.1f, 0.1f), 0.f, 1.f) * alpha));
 				

src/graphics/effects/SpellEffects.cpp
@@ -68,7 +68,7 @@ void CSpellFx::SetDuration(GameDuration duration) {
 
 void Draw3DLineTexNew(const RenderMaterial & mat, Vec3f startPos, Vec3f endPos, Color startColor, Color endColor, float startSize, float endSize) {
 	
-	float fBeta = MAKEANGLE(player.angle.getYaw());
+	float fBeta = MAKEANGLE(g_camera->angle.getYaw());
 	float xxs = startSize * glm::cos(glm::radians(fBeta));
 	float xxe = endSize * glm::cos(glm::radians(fBeta));
 	float zzs = startSize;