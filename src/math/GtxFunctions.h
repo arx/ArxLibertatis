4bc5e7629fca14cdc6be3c2ce01b703ef16860cf
Daniel Scharrer <daniel@constexpr.org>
2020-06-25 08:55:29 +0200

    GtxFunctions: Increase epsilon for atan2() singularity check
    This is required for at least ARM (under qemu).

--- src/math/GtxFunctions.h
+++ src/math/GtxFunctions.h
@@ -226,7 +226,7 @@ GLM_FUNC_QUALIFIER float roll(glm::quat const & q)
 	float const y = 2.f * (q.x * q.y + q.w * q.z);
 	float const x = q.w * q.w + q.x * q.x - q.y * q.y - q.z * q.z;
 	
-	if(glm::all(glm::epsilonEqual(glm::vec2(x, y), glm::vec2(0), glm::epsilon<float>()))) // avoid atan2(0,0) - handle singularity
+	if(glm::all(glm::epsilonEqual(glm::vec2(x, y), glm::vec2(0), 4.f * glm::epsilon<float>()))) // avoid atan2(0,0) - handle singularity
 		return static_cast<float>(0.f);
 	
 	return static_cast<float>(glm::atan(y, x));
@@ -239,7 +239,7 @@ GLM_FUNC_QUALIFIER float pitch(glm::quat const & q)
 	float const y = 2.f * (q.y * q.z + q.w * q.x);
 	float const x = q.w * q.w - q.x * q.x - q.y * q.y + q.z * q.z;
 	
-	if(glm::all(glm::epsilonEqual(glm::vec2(x, y), glm::vec2(0), glm::epsilon<float>()))) // avoid atan2(0,0) - handle singularity - Matiis
+	if(glm::all(glm::epsilonEqual(glm::vec2(x, y), glm::vec2(0), 4.f * glm::epsilon<float>()))) // avoid atan2(0,0) - handle singularity - Matiis
 		return static_cast<float>(2.f * glm::atan(q.x, q.w));
 	
 	return static_cast<float>(glm::atan(y, x));
