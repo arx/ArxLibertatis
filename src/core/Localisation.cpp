cca472626b6c78e2ebc8821a9c57a83c99e72779
Daniel Scharrer <daniel@constexpr.org>
2020-07-04 21:15:43 +0200

    Localisation: Improve language detection
    With AF 1.22, utext_francais.ini is included with all language versions
    so just selecting the first language is no longer enough.
    
    Fixes: issue #1464

--- src/core/Localisation.cpp
+++ src/core/Localisation.cpp
@@ -25,6 +25,7 @@
 #include <cstdlib>
 #include <iterator>
 
+#include <boost/algorithm/string/case_conv.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/foreach.hpp>
 
@@ -33,8 +34,11 @@
 #include "io/resource/ResourcePath.h"
 #include "io/resource/PakReader.h"
 #include "io/IniReader.h"
+#include "io/fs/FileStream.h"
+#include "io/fs/SystemPaths.h"
 #include "io/log/Logger.h"
 
+#include "platform/Environment.h"
 #include "platform/Platform.h"
 
 #include "util/Unicode.h"
@@ -43,70 +47,122 @@ namespace {
 
 IniReader g_localisation;
 
-PakFile * autodetectLanguage() {
+static Language getLanguageInfo(const std::string & id) {
 	
-	PakDirectory * dir = g_resources->getDirectory("localisation");
-	if(!dir) {
-		LogCritical << "Missing 'localisation' directory. Is 'loc.pak' present?";
-		return NULL;
-	}
+	std::istringstream iss(g_resources->read("localisation/languages/" + id + ".ini"));
+	IniReader reader;
+	reader.read(iss);
 	
-	std::ostringstream languages;
-	PakFile * localisation = NULL;
+	Language result;
+	result.name = reader.getKey("language", "name", id);
+	result.locale = reader.getKey("language", "locale", id);
+	boost::to_lower(result.locale);
 	
-	PakDirectory::files_iterator file = dir->files_begin();
-	for(; file != dir->files_end(); ++file) {
-		
-		const std::string & name = file->first;
-		
-		const std::string prefix = "utext_";
-		const std::string suffix = ".ini";
-		if(!boost::starts_with(name, prefix) || !boost::ends_with(name, suffix)) {
-			// Not a localisation file.
-			continue;
-		}
-		
-		if(name.length() <= prefix.length() + suffix.length()) {
-			// Missing language name.
-			continue;
-		}
-		
-		// Extract the language name.
-		size_t length = name.length() - prefix.length() - suffix.length();
-		std::string language = name.substr(prefix.length(), length);
-		
-		if(!localisation) {
-			
-			localisation = file->second;
-			config.language = language;
-			
-		} else {
-			
-			if(!languages.tellp()) {
-				languages << config.language;
+	return result;
+}
+
+std::string selectPreferredLanguage(const Languages & languages) {
+	
+	std::vector<std::string> locales = platform::getPreferredLocales();
+	
+	// Select language based on system language
+	BOOST_FOREACH(std::string & locale, locales) {
+		BOOST_FOREACH(const Languages::value_type & language, languages) {
+			if(language.second.locale == locale) {
+				return language.first;
 			}
-			languages << ", " << language;
-			
-			// Prefer english if there are multiple localisations.
-			if(language == "english") {
-				localisation = file->second;
-				config.language = language;
+		}
+		BOOST_FOREACH(const Languages::value_type & language, languages) {
+			if(boost::starts_with(language.second.locale, locale)) {
+				return language.first;
 			}
 		}
 	}
 	
-	if(!localisation) {
+	return std::string();
+}
+
+std::string selectDefaultLanguage(const Languages & languages) {
+	
+	// Select language from AF config file
+	// For AF 1.22 this will match language selected by the user in the Steam / GOG / Bethesda launcher
+	const char * cfgFiles[] = { "cfg.ini", "cfg_default.ini" };
+	BOOST_FOREACH(const fs::path & datadir, fs::getDataDirs()) {
+		BOOST_FOREACH(const char * cfgFile, cfgFiles) {
+			fs::ifstream ifs(datadir / cfgFile);
+			if(ifs.is_open()) {
+				IniReader cfg;
+				cfg.read(ifs);
+				std::string language = cfg.getKey("language", "string", config.language);
+				boost::to_lower(language);
+				Languages::const_iterator it = languages.find(language);
+				if(it != languages.end()) {
+					return it->first;
+				}
+			}
+		}
+	}
+	
+	return std::string();
+}
+
+void autodetectTextLanguage() {
+	
+	Languages languages = getAvailableTextLanguages();
+	if(languages.empty()) {
 		LogCritical << "Could not find any localisation file. (localisation/utext_*.ini)";
-		return NULL;
+		config.language = std::string();
+		return;
 	}
 	
-	if(languages.tellp()) {
-		LogWarning << "Multiple localisations avalable: " << languages.rdbuf();
+	if(languages.size() == 1) {
+		// Only one language available so just use that
+		config.language = languages.begin()->first;
+		return;
 	}
 	
-	LogInfo << "Autodetected language: " << config.language;
+	std::ostringstream list;
+	BOOST_FOREACH(const Languages::value_type & language, languages) {
+		if(list.tellp()) {
+			list << ", ";
+		}
+		list << language.first;
+	}
+	LogInfo << "Multiple text languages available: " << list.str();
 	
-	return localisation;
+	config.language = selectPreferredLanguage(languages);
+	if(!config.language.empty()) {
+		return;
+	}
+	
+	config.language = selectDefaultLanguage(languages);
+	if(!config.language.empty()) {
+		return;
+	}
+	
+	// Otherwise, prefer english if english audio is available
+	Languages::iterator english = languages.find("english");
+	if(english != languages.end() && g_resources->getDirectory(res::path("speech") / english->first)) {
+		config.language = english->first;
+		return;
+	}
+	
+	// Otherwise, prefer the first language with audio available
+	BOOST_FOREACH(const Languages::value_type & language, languages) {
+		if(g_resources->getDirectory(res::path("speech") / language.first)) {
+			config.language = language.first;
+			return;
+		}
+	}
+	
+	// Otherwise, prefer english
+	if(english != languages.end()) {
+		config.language = english->first;
+		return;
+	}
+	
+	// Finally just select the first available language
+	config.language = languages.begin()->first;
 }
 
 void loadLocalisation(PakDirectory * dir, const std::string & name) {
@@ -176,31 +232,67 @@ void loadLocalisations() {
 
 } // anonymous namespace
 
+Languages getAvailableTextLanguages() {
+	
+	Languages result;
+	
+	PakDirectory * localisation = g_resources->getDirectory("localisation");
+	if(!localisation) {
+		LogCritical << "Missing 'localisation' directory. Is 'loc.pak' present?";
+		return result;
+	}
+	PakDirectory::files_iterator file = localisation->files_begin();
+	for(; file != localisation->files_end(); ++file) {
+		
+		const std::string & name = file->first;
+		
+		const std::string prefix = "utext_";
+		const std::string suffix = ".ini";
+		if(!boost::starts_with(name, prefix) || !boost::ends_with(name, suffix)) {
+			// Not a localisation file.
+			continue;
+		}
+		
+		if(name.length() <= prefix.length() + suffix.length()) {
+			// Missing language name.
+			continue;
+		}
+		
+		// Extract the language name.
+		size_t length = name.length() - prefix.length() - suffix.length();
+		std::string id = name.substr(prefix.length(), length);
+		
+		if(id.find_first_not_of("abcdefghijklmnopqrstuvwxyz_") != std::string::npos) {
+			LogWarning << "Ignoring localisation/" << name;
+			continue;
+		}
+		
+		result.insert(Languages::value_type(id, getLanguageInfo(id)));
+	}
+	
+	return result;
+}
+
 bool initLocalisation() {
 	
 	LogDebug("Starting localization");
 	
 	g_localisation.clear();
 	
-	PakFile * file;
-	
-	if(config.language.empty()) {
-		file = autodetectLanguage();
-	} else {
-		
-		LogInfo << "Using language from config file: " << config.language;
-		
-		// Attempt to load localisation for the configured language.
+	PakFile * file = NULL;
+	if(!config.language.empty()) {
+		LogInfo << "Using text language from config: " << config.language;
 		std::string filename = "localisation/utext_" + config.language + ".ini";
 		file = g_resources->getFile(filename);
-		
 		if(!file) {
-			LogWarning << "Localisation file " << filename << " not found, autodetecting language.";
-			/*
-			 * TODO we might want to keep the old config.language setting as there could be
-			 * localized audio for that language even if there is no localized text.
-			 */
-			file = autodetectLanguage();
+			LogWarning << "Localisation file " << filename << " not found";
+		}
+	}
+	if(!file) {
+		autodetectTextLanguage();
+		if(!config.language.empty()) {
+			LogInfo << "Autodetected text language: " << config.language;
+			file = g_resources->getFile("localisation/utext_" + config.language + ".ini");
 		}
 	}
 	
