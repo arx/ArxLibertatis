daea17dc66d161b68c3a25d7c7b04756e945efd4
Daniel Scharrer <daniel@constexpr.org>
2020-07-05 12:57:07 +0200

    ResourceSetup: Filter bad localisations from speech.pak

--- src/io/resource/ResourceSetup.cpp
+++ src/io/resource/ResourceSetup.cpp
@@ -78,10 +78,38 @@ struct ResourceEntry {
 
 bool addDefaultResources(PakReader * reader) {
 	
+	PakFilter speechFilter;
+	// French version has French audio copied in the deutsch and english directories
+	{
+		std::vector<std::string> & filter = speechFilter[util::md5::checksum("6d2cea5370477464b43353a637a18c0c")];
+		filter.push_back("speech/deutsch");
+		filter.push_back("speech/english");
+	}
+	// German version has German audio partially copied in the english and francais directories
+	{
+		std::vector<std::string> & filter = speechFilter[util::md5::checksum("499b08d1a365ebea74b23a8efc9a8302")];
+		filter.push_back("speech/english");
+		filter.push_back("speech/francais");
+	}
+	// Italian version has German audio partially copied in the francais directory
+	{
+		std::vector<std::string> & filter = speechFilter[util::md5::checksum("ca067587556a908be255e9244209fc47")];
+		filter.push_back("speech/francais");
+	}
+	// Spanish version has Spanish audio copied in the deustch (sic) and english directories
+	{
+		std::vector<std::string> & filter = speechFilter[util::md5::checksum("975be9f2c6a952b4d014dfaf0d6f1882")];
+		filter.push_back("speech/deustch");
+		filter.push_back("speech/english");
+	}
+	
 	const char * const directories[] = { "editor", "game", "graph", "localisation", "misc", "sfx", "speech" };
 	struct {
 		const char * name;
-	} const archives[] = { { "data" }, { "loc" }, { "data2" }, { "sfx" }, { "speech" } };
+		PakFilter * filter;
+	} const archives[] = {
+		{ "data", NULL }, { "loc", NULL }, { "data2", NULL }, { "sfx", NULL }, { "speech", &speechFilter }
+	};
 	
 	std::set<ResourceEntry> resources;
 	
@@ -126,7 +154,7 @@ bool addDefaultResources(PakReader * reader) {
 		if(entry.archive == std::numeric_limits<int>::max()) {
 			reader->addFiles(entry.path, entry.name); 
 		} else {
-			if(reader->addArchive(entry.path)) {
+			if(reader->addArchive(entry.path, archives[entry.archive].filter)) {
 				ok[entry.archive] = true;
 			}
 		}
