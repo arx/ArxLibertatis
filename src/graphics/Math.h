4e1c39b4a8dc9321288e96794c371b69e7789272
Daniel Scharrer <daniel@constexpr.org>
2020-07-05 17:49:19 +0200

    Math: Fix special case

--- src/graphics/Math.h
+++ src/graphics/Math.h
@@ -246,6 +246,9 @@ inline long PointInUnderCylinder(const Cylinder & cyl, const Vec3f & pt) {
 
 template <typename T>
 T positive_modulo(T a, T b) {
+	if(b == 1) {
+		return 0;
+	}
 	return (a % b) + T(a < 0) * b;
 }
 
