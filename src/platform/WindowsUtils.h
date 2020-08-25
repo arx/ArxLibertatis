1a0879c0432c92c87bb34855340b56755dfbda28
Daniel Scharrer <daniel@constexpr.org>
2020-08-25 08:43:14 +0200

    WindowsUtils: Make MinGW happy

--- src/platform/WindowsUtils.h
+++ src/platform/WindowsUtils.h
@@ -157,7 +157,7 @@ std::string getErrorString(DWORD error = GetLastError(), HMODULE module = NULL);
 
 template <typename FunctionType>
 FunctionType getProcAddress(HMODULE module, const char * symbol) {
-	return reinterpret_cast<FunctionType>(GetProcAddress(module, symbol));
+	return reinterpret_cast<FunctionType>(reinterpret_cast<void(*)()>(GetProcAddress(module, symbol)));
 }
 
 } // namespace
