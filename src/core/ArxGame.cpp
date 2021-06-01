arx/ArxLibertatis

commit edaa7e68cd418a03660bd84430a503de2837582f

Move entity speed modifier calculation to one place

src/core/ArxGame.cpp
@@ -1321,12 +1321,6 @@ void ArxGame::renderLevel() {
 		managePlayerControls();
 	}
 
-	ARX_PLAYER_Manage_Movement();
-
-	ARX_PLAYER_Manage_Visual();
-
-	g_miniMap.setActiveBackground(ACTIVEBKG);
-	g_miniMap.validatePlayerPos(CURRENTLEVEL, BLOCK_PLAYER_CONTROLS, Book_Mode);
 
 	for(size_t i = 0; i < entities.size(); i++) {
 		Entity *entity = entities[i];
@@ -1343,7 +1337,40 @@ void ArxGame::renderLevel() {
 				entity->special_color = Color3f::gray(float(iHighLight));
 			}
 		}
+
+		if(entity) {
+			float speedModifier = 0.f;
+
+			if(entity == entities.player()) {
+				if(cur_mr == 3)
+					speedModifier += 0.5f;
+
+				if(cur_rf == 3)
+					speedModifier += 1.5f;
+			}
+
+			long speedSpellIndex = ARX_SPELLS_GetSpellOn(entity, SPELL_SPEED);
+			if(speedSpellIndex > -1) {
+				speedModifier += spells[speedSpellIndex].caster_level * 0.1f;
+			}
+
+			long slowSpellIndex = ARX_SPELLS_GetSpellOn(entity, SPELL_SLOW_DOWN);
+			if(slowSpellIndex > -1) {
+				speedModifier -= spells[slowSpellIndex].caster_level * 0.05f;
+			}
+
+			entity->speed_modif = speedModifier;
+		}
 	}
+
+
+	ARX_PLAYER_Manage_Movement();
+
+	ARX_PLAYER_Manage_Visual();
+
+	g_miniMap.setActiveBackground(ACTIVEBKG);
+	g_miniMap.validatePlayerPos(CURRENTLEVEL, BLOCK_PLAYER_CONTROLS, Book_Mode);
+
 
 	if(entities.player() && entities.player()->animlayer[0].cur_anim) {
 		ManageNONCombatModeAnimations();

src/animation/Animation.cpp
@@ -511,14 +511,6 @@ void EERIEDrawAnimQuat(EERIE_3DOBJ *eobj, ANIM_USE *eanim, Anglef *angle, Vec3f 
 	if(io) {
 		float speedfactor = io->basespeed + io->speed_modif;
 
-		if(io == entities.player()) {
-			if(cur_mr==3)
-				speedfactor+=0.5f;
-
-			if(cur_rf==3)
-				speedfactor+=1.5f;
-		}
-
 		if(speedfactor < 0)
 			speedfactor = 0;
 

src/game/Player.cpp
@@ -2273,12 +2273,6 @@ void ARX_PLAYER_Manage_Movement() {
 	if(speedfactor < 0)
 		speedfactor = 0;
 
-	if(cur_mr == 3)
-		speedfactor += 0.5f;
-
-	if(cur_rf == 3)
-		speedfactor += 1.5f;
-
 	static float StoredTime = 0;
 	// Compute time things
 	float DeltaTime = StoredTime;

src/game/Spells.cpp
@@ -3551,13 +3551,6 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			
 			ARX_SPELLS_AddSpellOn(spells[i].target, i);
 			
-			if(spells[i].caster >= 0 && spells[i].target < long(entities.size())) {
-				Entity * t = entities[spells[i].target];
-				if(t) {
-					t->speed_modif += spells[i].caster_level * 0.1f;
-				}
-			}
-			
 			break;
 		}
 		case SPELL_DISPELL_ILLUSION: {
@@ -4327,10 +4320,6 @@ bool ARX_SPELLS_Launch(Spell typ, long source, SpellcastFlags flagss, long level
 			spells[i].tolive = effect->GetDuration();
 			
 			ARX_SPELLS_AddSpellOn(target, i);
-			
-			if(ValidIONum(target)) {
-				entities[target]->speed_modif -= spells[i].caster_level * 0.05f;
-			}
 			
 			break;
 		}
@@ -5451,11 +5440,6 @@ void ARX_SPELLS_Update()
 				case SPELL_SPEED:						
 					ARX_SPELLS_RemoveSpellOn(spells[i].target,i);
 
-					if(spells[i].target >= 0 && spells[i].target < long(entities.size())) {
-						if(entities[spells[i].target])
-							entities[spells[i].target]->speed_modif -= spells[i].caster_level * (1.0f/10);
-					}
-
 					if(spells[i].caster == 0)
 						ARX_SOUND_Stop(spells[i].snd_loop);
 
@@ -5551,11 +5535,6 @@ void ARX_SPELLS_Update()
 				break;
 				case SPELL_SLOW_DOWN:
 					ARX_SPELLS_RemoveSpellOn(spells[i].target,i);
-
-					if(spells[i].target >= 0 && spells[i].target < long(entities.size())) {
-						if(entities[spells[i].target])
-							entities[spells[i].target]->speed_modif += spells[i].caster_level * (1.0f/20);
-					}
 				break;				
 				//****************************************************************************
 				// LEVEL 7

commit d1412b3588e40deb6fc240853cc3070babd3dd11

ArxGame: Fix Speed and Slow down spells not stacking
Stacking was broken in commit edaa7e6.

src/core/ArxGame.cpp
@@ -1711,22 +1711,12 @@ void ArxGame::updateLevel() {
 		if(entity == entities.player()) {
 			if(cur_mr == 3)
 				speedModifier += 0.5f;
-
 			if(cur_rf == 3)
 				speedModifier += 1.5f;
 		}
-
-		SpellBase * spell;
 		
-		spell = spells.getSpellOnTarget(entity->index(), SPELL_SPEED);
-		if(spell) {
-			speedModifier += spell->m_level * 0.1f;
-		}
-		
-		spell = spells.getSpellOnTarget(entity->index(), SPELL_SLOW_DOWN);
-		if(spell) {
-			speedModifier -= spell->m_level * 0.05f;
-		}
+		speedModifier += spells.getTotalSpellCasterLevelOnTarget(entity->index(), SPELL_SPEED) * 0.1f;
+		speedModifier -= spells.getTotalSpellCasterLevelOnTarget(entity->index(), SPELL_SLOW_DOWN) * 0.05f;
 		
 		entity->speed_modif = speedModifier;
 	}