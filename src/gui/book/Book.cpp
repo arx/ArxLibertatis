arx/ArxLibertatis

commit 6ef8c421fa96c5ffb87c1aac116cf2889675dbce

Replace boost::lexical_cast<std::string>() with std::to_string()

src/gui/book/Book.cpp
@@ -968,42 +968,42 @@ void StatsPage::manageStats()
 	// Secondary Attributes
 	{
 		Vec2f pos = bookPos + Vec2f(227, 90) * scale;
-		std::string value = boost::lexical_cast<std::string>(std::ceil(player.Full_maxlife));
+		std::string value = std::to_string(std::ceil(player.Full_maxlife));
 		Color color = attributeModToColor(player.Full_maxlife, player.lifePool.max);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(227, 150) * scale;
-		std::string value = boost::lexical_cast<std::string>(std::ceil(player.Full_maxmana));
+		std::string value = std::to_string(std::ceil(player.Full_maxmana));
 		Color color = attributeModToColor(player.Full_maxmana, player.manaPool.max);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(227, 210) * scale;
-		std::string value = boost::lexical_cast<std::string>(std::ceil(player.m_miscFull.damages));
+		std::string value = std::to_string(std::ceil(player.m_miscFull.damages));
 		Color color = attributeModToColor(player.m_miscMod.damages);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(54, 90) * scale;
-		std::string value = boost::lexical_cast<std::string>(std::ceil(player.m_miscFull.armorClass));
+		std::string value = std::to_string(std::ceil(player.m_miscFull.armorClass));
 		Color color = attributeModToColor(player.m_miscMod.armorClass);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(54, 150) * scale;
-		std::string value = boost::lexical_cast<std::string>(std::ceil(player.m_miscFull.resistMagic));
+		std::string value = std::to_string(std::ceil(player.m_miscFull.resistMagic));
 		Color color = attributeModToColor(player.m_miscMod.resistMagic);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(54, 210) * scale;
-		std::string value = boost::lexical_cast<std::string>(std::ceil(player.m_miscFull.resistPoison));
+		std::string value = std::to_string(std::ceil(player.m_miscFull.resistPoison));
 		Color color = attributeModToColor(player.m_miscMod.resistPoison);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}

src/script/ScriptUtils.cpp
@@ -21,8 +21,6 @@
 
 #include <set>
 
-#include <boost/lexical_cast.hpp>
-
 #include "game/Entity.h"
 #include "graphics/data/Mesh.h"
 
@@ -62,17 +60,17 @@ std::string Context::getStringVar(const std::string & name) const {
 		std::string tv;
 		switch(getSystemVar(*this, name, tv, &fv, &lv)) {
 			case TYPE_TEXT: return tv;
-			case TYPE_LONG: return boost::lexical_cast<std::string>(lv);
-			default: return boost::lexical_cast<std::string>(fv);
+			case TYPE_LONG: return std::to_string(lv);
+			default: return std::to_string(fv);
 		}
 	} else if(name[0] == '#') {
-		return boost::lexical_cast<std::string>(GETVarValueLong(svar, name));
+		return std::to_string(GETVarValueLong(svar, name));
 	} else if(name[0] == '\xA7') {
-		return boost::lexical_cast<std::string>(GETVarValueLong(getEntity()->m_variables, name));
+		return std::to_string(GETVarValueLong(getEntity()->m_variables, name));
 	} else if(name[0] == '&') {
-		return boost::lexical_cast<std::string>(GETVarValueFloat(svar, name));
+		return std::to_string(GETVarValueFloat(svar, name));
 	} else if(name[0] == '@') {
-		return boost::lexical_cast<std::string>(GETVarValueFloat(getEntity()->m_variables, name));
+		return std::to_string(GETVarValueFloat(getEntity()->m_variables, name));
 	} else if(name[0] == '$') {
 		const SCRIPT_VAR * var = GetVarAddress(svar, name);
 		return var ? var->text : "void";

src/animation/Animation.cpp
@@ -53,8 +53,6 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include <cstring>
 #include <sstream>
 
-#include <boost/lexical_cast.hpp>
-
 #include "util/String.h"
 
 #include "animation/AnimationFormat.h"
@@ -525,7 +523,7 @@ ANIM_HANDLE * EERIE_ANIMMANAGER_Load_NoWarning(const res::path & path) {
 		res::path altpath;
 		do {
 			altpath = res::path(path);
-			altpath.append_basename(boost::lexical_cast<std::string>(pathcount++));
+			altpath.append_basename(std::to_string(pathcount++));
 		} while(EERIE_ANIMMANAGER_AddAltAnim(slot, altpath));
 		
 		return &slot;

src/graphics/DrawDebug.cpp
@@ -410,7 +410,7 @@ static void drawColorChart() {
 	for(size_t i = 0; i < std::size(distinctDebugColors); i++) {
 		drawLine(p, p + Vec2f(40, 0), 1.f, distinctDebugColors[i]);
 		
-		drawTextCentered(hFontDebug, p + Vec2f(-5, 0), boost::lexical_cast<std::string>(i), Color::white);
+		drawTextCentered(hFontDebug, p + Vec2f(-5, 0), std::to_string(i), Color::white);
 		
 		p += Vec2i(0, 12);
 	}

src/gui/MainMenu.cpp
@@ -809,7 +809,7 @@ public:
 				std::sort(rates.begin(), rates.end());
 				rates.erase(std::unique(rates.begin(), rates.end()), rates.end());
 				for(s32 refresh : rates) {
-					cb->addEntry(boost::lexical_cast<std::string>(refresh));
+					cb->addEntry(std::to_string(refresh));
 					if(config.video.fpsLimit == refresh) {
 						cb->selectLast();
 					}

src/gui/Speech.cpp
@@ -50,8 +50,6 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #include <cstdio>
 #include <algorithm>
 
-#include <boost/lexical_cast.hpp>
-
 #include "animation/Animation.h"
 
 #include "core/Config.h"
@@ -259,7 +257,7 @@ long ARX_SPEECH_AddSpeech(Entity * io, const std::string & data, long mood,
 		LogDebug(" -> " << variant << " / " << count);
 		
 		if(variant > 1) {
-			sample = data + boost::lexical_cast<std::string>(variant);
+			sample = data + std::to_string(variant);
 		} else {
 			sample = data;
 		}

src/gui/Text.cpp
@@ -263,9 +263,10 @@ static Font * createFont(const res::path & fontFace,
 	arx_assert(scaleFactor < 1000.f); // TODO better maximum
 	
 	try {
-		std::string szFontSize = boost::lexical_cast<std::string>(fontSize);
-		std::string configSize = getLocalised(configSizeKey, szFontSize);
-		fontSize = boost::lexical_cast<unsigned int>(configSize);
+		std::string configSize = getLocalised(configSizeKey, std::string());
+		if(!configSize.empty()) {
+			fontSize = boost::lexical_cast<unsigned int>(configSize);
+		}
 	} catch(const boost::bad_lexical_cast &) {
 		LogError << "Invalid font size for: " << configSizeKey;
 	}

src/gui/widget/SaveSlotWidget.cpp
@@ -23,8 +23,6 @@
 #include <iomanip>
 #include <ctime>
 
-#include <boost/lexical_cast.hpp>
-
 #include "core/Core.h"
 #include "core/Localisation.h"
 #include "graphics/data/TextureContainer.h"
@@ -111,7 +109,7 @@ SaveSlotWidget::SaveSlotWidget(SavegameHandle savegame, size_t i, Font * font, c
 				case 11: m_date = getLocalised("system_december"); break;
 				default: arx_unreachable();
 			}
-			m_date += " " + boost::lexical_cast<std::string>(t.tm_mday);
+			m_date += " " + std::to_string(t.tm_mday);
 		} else {
 			std::ostringstream oss;
 			oss << std::setfill('0') << (t.tm_year + 1900) << "-"

commit 5d7e5c3ac3526adb0621b7c7f129fec0766b1053

Fix excessive precision with float to string conversion
Was broken in commit 6ef8c42.

src/gui/book/Book.cpp
@@ -968,42 +968,42 @@ void StatsPage::manageStats()
 	// Secondary Attributes
 	{
 		Vec2f pos = bookPos + Vec2f(227, 90) * scale;
-		std::string value = std::to_string(std::ceil(player.Full_maxlife));
+		std::string value = std::to_string(long(std::ceil(player.Full_maxlife)));
 		Color color = attributeModToColor(player.Full_maxlife, player.lifePool.max);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(227, 150) * scale;
-		std::string value = std::to_string(std::ceil(player.Full_maxmana));
+		std::string value = std::to_string(long(std::ceil(player.Full_maxmana)));
 		Color color = attributeModToColor(player.Full_maxmana, player.manaPool.max);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(227, 210) * scale;
-		std::string value = std::to_string(std::ceil(player.m_miscFull.damages));
+		std::string value = std::to_string(long(std::ceil(player.m_miscFull.damages)));
 		Color color = attributeModToColor(player.m_miscMod.damages);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(54, 90) * scale;
-		std::string value = std::to_string(std::ceil(player.m_miscFull.armorClass));
+		std::string value = std::to_string(long(std::ceil(player.m_miscFull.armorClass)));
 		Color color = attributeModToColor(player.m_miscMod.armorClass);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(54, 150) * scale;
-		std::string value = std::to_string(std::ceil(player.m_miscFull.resistMagic));
+		std::string value = std::to_string(long(std::ceil(player.m_miscFull.resistMagic)));
 		Color color = attributeModToColor(player.m_miscMod.resistMagic);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}
 	
 	{
 		Vec2f pos = bookPos + Vec2f(54, 210) * scale;
-		std::string value = std::to_string(std::ceil(player.m_miscFull.resistPoison));
+		std::string value = std::to_string(long(std::ceil(player.m_miscFull.resistPoison)));
 		Color color = attributeModToColor(player.m_miscMod.resistPoison);
 		DrawBookTextCenter(hFontInBook, pos, value, color);
 	}

src/script/ScriptUtils.cpp
@@ -21,6 +21,8 @@
 
 #include <set>
 
+#include <boost/lexical_cast.hpp>
+
 #include "game/Entity.h"
 #include "graphics/data/Mesh.h"
 
@@ -68,9 +70,9 @@ std::string Context::getStringVar(const std::string & name) const {
 	} else if(name[0] == '\xA7') {
 		return std::to_string(GETVarValueLong(getEntity()->m_variables, name));
 	} else if(name[0] == '&') {
-		return std::to_string(GETVarValueFloat(svar, name));
+		return boost::lexical_cast<std::string>(GETVarValueFloat(svar, name));
 	} else if(name[0] == '@') {
-		return std::to_string(GETVarValueFloat(getEntity()->m_variables, name));
+		return boost::lexical_cast<std::string>(GETVarValueFloat(getEntity()->m_variables, name));
 	} else if(name[0] == '$') {
 		const SCRIPT_VAR * var = GetVarAddress(svar, name);
 		return var ? var->text : "void";