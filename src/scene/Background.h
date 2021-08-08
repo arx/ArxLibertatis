7598c01406a8dae290b4ca6f37204f3c011d3491
Daniel Scharrer <daniel@constexpr.org>
2021-08-08 10:12:43 +0200

    Background: Add helpers to iterate over tiles

--- src/scene/Background.h
+++ src/scene/Background.h
@@ -20,9 +20,14 @@
 #define ARX_SCENE_BACKGROUND_H
 
 #include <bitset>
+#include <utility>
+#include <vector>
 
 #include "ai/Anchors.h"
 #include "graphics/GraphicsTypes.h"
+#include "math/Types.h"
+#include "platform/Platform.h"
+#include "util/Range.h"
 
 struct BackgroundTileData {
 	
@@ -38,16 +43,61 @@ struct BackgroundTileData {
 
 static const short MAX_BKGX = 160;
 static const short MAX_BKGZ = 160;
-static const short BKG_SIZX = 100;
-static const short BKG_SIZZ = 100;
 
-static const Vec2f g_backgroundTileSize = Vec2f(BKG_SIZX, BKG_SIZZ);
+static const Vec2f g_backgroundTileSize = Vec2f(100, 100);
 
 struct BackgroundData {
 	
 private:
 	
 	std::bitset<MAX_BKGX * MAX_BKGZ> m_activeTiles;
+	
+	//! Tile data accessor
+	template <typename T>
+	class Tile : public Vec2s {
+		
+		T * m_base;
+		
+	public:
+		
+		Tile(T * base, Vec2s tile) : Vec2s(tile), m_base(base) { }
+		
+		[[nodiscard]] Vec2s index() const noexcept {
+			return *this;
+		}
+		
+		[[nodiscard]] bool valid() const noexcept {
+			return m_base->isTileValid(*this);
+		}
+		[[nodiscard]] explicit operator bool() const noexcept {
+			return valid();
+		}
+		
+		[[nodiscard]] bool active() const noexcept {
+			return m_base->isTileActive(*this);
+		}
+		void setActive() noexcept {
+			m_base->setTileActive(*this);
+		}
+		
+		[[nodiscard]] auto & data() const noexcept {
+			arx_assume(valid());
+			return m_base->m_tileData[x][y];
+		}
+		
+		[[nodiscard]] auto & polygons() const noexcept {
+			return data().polydata;
+		}
+		
+		[[nodiscard]] auto intersectingPolygons() const noexcept {
+			return util::dereference(data().polyin);
+		}
+		
+		[[nodiscard]] auto & maxY() const noexcept {
+			return data().maxy;
+		}
+		
+	};
 	
 public:
 	
@@ -57,29 +107,136 @@ public:
 	BackgroundTileData m_tileData[MAX_BKGX][MAX_BKGZ];
 	std::vector<ANCHOR_DATA> m_anchors;
 	
-	bool isTileActive(Vec2s tile) const {
-		return m_activeTiles.test(size_t(tile.x) * size_t(MAX_BKGZ) + size_t(tile.y));
+	[[nodiscard]] auto get(Vec2s tile) {
+		return Tile(this, tile);
+	}
+	[[nodiscard]] auto get(Vec2s tile) const {
+		return Tile(this, tile);
 	}
 	
-	void setTileActive(Vec2s tile) {
-		m_activeTiles.set(size_t(tile.x) * size_t(MAX_BKGZ) + size_t(tile.y));
+	[[nodiscard]] auto getTile(Vec3f pos) {
+		return get(getTileIndex(pos));
+	}
+	[[nodiscard]] auto getTile(Vec3f pos) const {
+		return get(getTileIndex(pos));
 	}
 	
-	void resetActiveTiles() {
-		m_activeTiles.reset();
-	}
-	
-	Vec2s getTile(const Vec3f & pos) const {
-		return Vec2s(s16(pos.x * m_mul.x), s16(pos.z * m_mul.y));
-	}
-	
-	bool isTileValid(Vec2s tile) const {
+	[[nodiscard]] bool isTileValid(Vec2s tile) const noexcept {
+		arx_assume(m_size.x <= MAX_BKGX && m_size.y <= MAX_BKGZ);
 		return tile.x >= 0 && tile.x < m_size.x && tile.y >= 0 && tile.y < m_size.y;
 	}
 	
-	bool isInActiveTile(const Vec3f & pos) const {
-		Vec2s tile = getTile(pos);
-		return isTileValid(tile) && isTileActive(tile);
+	[[nodiscard]] bool isTileActive(Vec2s tile) const noexcept {
+		arx_assume(isTileValid(tile));
+		return m_activeTiles.test(size_t(tile.x) * size_t(MAX_BKGZ) + size_t(tile.y));
+	}
+	
+	void setTileActive(Vec2s tile) noexcept {
+		arx_assume(isTileValid(tile));
+		m_activeTiles.set(size_t(tile.x) * size_t(MAX_BKGZ) + size_t(tile.y));
+	}
+	
+	void resetActiveTiles() noexcept {
+		m_activeTiles.reset();
+	}
+	
+	bool isInActiveTile(Vec3f pos) const noexcept {
+		auto tile = getTile(pos);
+		return tile.valid() && tile.active();
+	}
+	
+private:
+	
+	[[nodiscard]] Vec2s getTileIndex(const Vec3f & pos) const noexcept {
+		return Vec2s(s16(pos.x * m_mul.x), s16(pos.z * m_mul.y));
+	}
+	
+	//! Returns an iterable over all valid tile indices
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] util::GridRange<Vec2s, Iterator> indices() const noexcept {
+		return { Vec2s(0), m_size };
+	}
+	
+	//! Returns an iterable over all valid tile indices in [begin.x, end.x) × [begin.y, end.y)
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] util::GridRange<Vec2s, Iterator> indicesIn(Vec2s begin, Vec2s end) const noexcept {
+		typedef Vec2s::value_type T;
+		return { Vec2s(glm::clamp(begin.x, T(0), m_size.x), glm::clamp(begin.y, T(0), m_size.y)),
+		         Vec2s(glm::clamp(end.x, T(0), m_size.x), glm::clamp(end.y, T(0), m_size.y)) };
+	}
+	
+	//! Returns an iterable over all valid tile indices in around and including the given tile
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] util::GridRange<Vec2s, Iterator> indicesAround(Vec2s tile, u16 extend) const noexcept {
+		return indicesIn(tile - Vec2s(extend), tile + Vec2s(extend) + Vec2s(1));
+	}
+	
+	//! Returns an iterable over all valid tile indices in around the given position
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] util::GridRange<Vec2s, Iterator> indicesAround(Vec3f pos, float extend) const noexcept {
+		return indicesIn(getTileIndex(pos - Vec3f(extend)), getTileIndex(pos + Vec3f(extend)) + Vec2s(1));
+	}
+	
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesForIndices(util::GridRange<Vec2s, Iterator> range) noexcept {
+		return util::transform(std::move(range), [this](Vec2s index) {
+			arx_assume(isTileValid(index));
+			return get(index);
+		});
+	}
+	
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesForIndices(util::GridRange<Vec2s, Iterator> range) const noexcept {
+		return util::transform(std::move(range), [this](Vec2s index) {
+			arx_assume(isTileValid(index));
+			return get(index);
+		});
+	}
+	
+public:
+	
+	//! Returns an iterable over all valid tiles
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tiles() noexcept {
+		return tilesForIndices(indices<Iterator>());
+	}
+	//! Returns an iterable over all valid tiles
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tiles() const noexcept {
+		return tilesForIndices(indices<Iterator>());
+	}
+	
+	//! Returns an iterable over all valid tile indices in around and including the given tile index
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesIn(Vec2s begin, Vec2s end) noexcept {
+		return tilesForIndices(indicesIn<Iterator>(begin, end));
+	}
+	//! Returns an iterable over all valid tile indices in around and including the given tile index
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesIn(Vec2s begin, Vec2s end) const noexcept {
+		return tilesForIndices(indicesIn<Iterator>(begin, end));
+	}
+	
+	//! Returns an iterable over all valid tile indices in around and including the given tile index
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesAround(Vec2s tile, u16 extend) noexcept {
+		return tilesForIndices(indicesAround<Iterator>(tile, extend));
+	}
+	//! Returns an iterable over all valid tile indices in around and including the given tile index
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesAround(Vec2s tile, u16 extend) const noexcept {
+		return tilesForIndices(indicesAround<Iterator>(tile, extend));
+	}
+	
+	//! Returns an iterable over all valid tile indices in around the given position
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesAround(Vec3f pos, float extend) noexcept {
+		return tilesForIndices(indicesAround<Iterator>(pos, extend));
+	}
+	//! Returns an iterable over all valid tile indices in around the given position
+	template <template <typename T> typename Iterator = util::GridXYIterator>
+	[[nodiscard]] auto tilesAround(Vec3f pos, float extend) const noexcept {
+		return tilesForIndices(indicesAround<Iterator>(pos, extend));
 	}
 	
 	BackgroundData()
