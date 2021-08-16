7e74bfbd08f476cde6e0b4e49d43ac6426939c6b
Daniel Scharrer <daniel@constexpr.org>
2021-08-16 22:35:42 +0200

    FilesystemPOSIX: Fix build without fstatat()

--- src/io/fs/Filesystem.h
+++ src/io/fs/Filesystem.h
@@ -45,6 +45,10 @@
 
 #include "platform/Platform.h"
 
+#if !ARX_HAVE_DIRFD || !ARX_HAVE_FSTATAT || !ARX_HAVE_AT_SYMLINK_NOFOLLOW
+#include "io/fs/FilePath.h"
+#endif
+
 namespace fs {
 
 class path;
