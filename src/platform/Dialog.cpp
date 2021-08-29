a3b7e3d5df1b47f2f7a344b8f01bd8229febd755
Daniel Scharrer <daniel@constexpr.org>
2021-08-29 13:14:01 +0200

    Dialog: Fix formatting for HTTPS links in dialogs
    Noticed by js in irc.
    
    (cherry picked from commit a839fc2a9444560a5c895381480109a9d6abc818)

--- src/platform/Dialog.cpp
+++ src/platform/Dialog.cpp
@@ -175,7 +175,7 @@ static std::string formatAsHtml(const std::string & text, bool newline, bool ul 
 				oss << "http://";
 				i += 6;
 			} else if(!link && line.compare(i, 8, "https://", 8) == 0) {
-				oss << "<a href=";
+				oss << "<a href=\"";
 				link_start = oss.tellp(), link = true;
 				oss << "https://";
 				i += 7;
