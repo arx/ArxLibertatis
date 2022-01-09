arx/ArxLibertatis

commit 6b9293166ef11af18df6a2435444193e0bc9dca2

Projectile: Replace ValidIONum()

src/physics/Projectile.cpp
@@ -401,11 +401,11 @@ static void ARX_THROWN_OBJECT_ManageProjectile(size_t i, GameDuration timeDelta)
 			bool need_kill = false;
 			for(size_t jj = 0; jj < sphereContent.size(); jj++) {
 				
-				if(!ValidIONum(sphereContent[jj]) && sphereContent[jj] != projectile.source) {
+				Entity * entity = entities.get(sphereContent[jj]);
+				if(entity && entity->index() != projectile.source) {
 					continue;
 				}
-				
-				Entity & target = *entities[sphereContent[jj]];
+				Entity & target = *entity;
 				
 				if(target.ioflags & IO_NPC) {
 					

commit 3bfae45a8b3daae88ee0d49b1e0295cd0a233957

Projectile: Fix arrow-entity collision
Was broken in commit 6b92931.

src/physics/Projectile.cpp
@@ -395,7 +395,7 @@ static void ARX_THROWN_OBJECT_ManageProjectile(size_t i, GameDuration timeDelta)
 			for(size_t jj = 0; jj < sphereContent.size(); jj++) {
 				
 				arx_assert(sphereContent[jj]);
-				if(sphereContent[jj]->index() != projectile.source) {
+				if(sphereContent[jj]->index() == projectile.source) {
 					continue;
 				}
 				Entity & target = *sphereContent[jj];