arx/ArxLibertatis

commit 485cbf89009dba19388042aa8777f40ea73cdc7d

ScriptUtils: Use std::string_view

src/script/ScriptUtils.cpp
@@ -32,14 +32,15 @@ static bool isWhitespace(char c) {
 	return (static_cast<unsigned char>(c) <= 32 || c == '(' || c == ')');
 }
 
-std::string loadUnlocalized(const std::string & str) {
+std::string_view toLocalizationKey(std::string_view string) {
 	
 	// if the section name has the qualifying brackets "[]", cut them off
-	if(!str.empty() && str[0] == '[' && str[str.length() - 1] == ']') {
-		return str.substr(1, str.length() - 2);
+	if(!string.empty() && string.front() == '[' && string.back() == ']') {
+		string.remove_prefix(1);
+		string.remove_suffix(1);
 	}
 	
-	return str;
+	return string;
 }
 
 Context::Context(const EERIE_SCRIPT * script, size_t pos, Entity * sender, Entity * entity,
@@ -52,7 +53,7 @@ Context::Context(const EERIE_SCRIPT * script, size_t pos, Entity * sender, Entit
 	, m_parameters(parameters)
 { }
 
-std::string Context::getStringVar(const std::string & name) const {
+std::string Context::getStringVar(std::string_view name) const {
 	
 	if(name.empty()) {
 		return std::string();
@@ -81,14 +82,14 @@ std::string Context::getStringVar(const std::string & name) const {
 		return var ? var->text : "void";
 	}
 	
-	return name;
+	return std::string(name);
 }
 
 #define ScriptParserWarning ARX_LOG(isSuppressed(*this, "?") ? Logger::Debug : Logger::Warning) << ScriptContextPrefix(*this) << ": "
 
 std::string Context::getCommand(bool skipNewlines) {
 	
-	const std::string & esdat = m_script->data;
+	std::string_view esdat = m_script->data;
 	
 	skipWhitespace(skipNewlines);
 	
@@ -123,7 +124,7 @@ std::string Context::getCommand(bool skipNewlines) {
 
 std::string Context::getWord() {
 	
-	const std::string & esdat = m_script->data;
+	std::string_view esdat = m_script->data;
 	
 	skipWhitespace(false, true);
 	
@@ -201,7 +202,7 @@ std::string Context::getWord() {
 
 void Context::skipWord() {
 	
-	const std::string & esdat = m_script->data;
+	std::string_view esdat = m_script->data;
 	
 	skipWhitespace(false, true);
 	
@@ -240,7 +241,7 @@ void Context::skipWord() {
 
 void Context::skipWhitespace(bool skipNewlines, bool warnNewlines) {
 	
-	const std::string & esdat = m_script->data;
+	std::string_view esdat = m_script->data;
 	
 	// First ignores spaces & unused chars
 	for(; m_pos != esdat.size() && isWhitespace(esdat[m_pos]); m_pos++) {
@@ -281,7 +282,7 @@ bool Context::getBool() {
 	return (word == "on" || word == "yes");
 }
 
-float Context::getFloatVar(const std::string & name) const {
+float Context::getFloatVar(std::string_view name) const {
 	
 	if(name.empty()) {
 		return 0.f;
@@ -307,14 +308,18 @@ float Context::getFloatVar(const std::string & name) const {
 		return GETVarValueFloat(getEntity()->m_variables, name);
 	}
 	
-	return float(atof(name.c_str()));
+	try {
+		return boost::lexical_cast<float>(name);
+	} catch(...) {
+		return 0.f;
+	}
 }
 
 size_t Context::skipCommand() {
 	
 	skipWhitespace();
 	
-	const std::string & esdat = m_script->data;
+	std::string_view esdat = m_script->data;
 	
 	if(m_pos == esdat.size() || esdat[m_pos] == '\n') {
 		return size_t(-1);
@@ -335,13 +340,13 @@ size_t Context::skipCommand() {
 	return oldpos;
 }
 
-bool Context::jumpToLabel(const std::string & target, bool substack) {
+bool Context::jumpToLabel(std::string_view target, bool substack) {
 	
 	if(substack) {
 		m_stack.push_back(m_pos);
 	}
 	
-	size_t targetpos = FindScriptPos(m_script, ">>" + target);
+	size_t targetpos = FindScriptPos(m_script, std::string(">>") += target);
 	if(targetpos == size_t(-1)) {
 		return false;
 	}
@@ -400,25 +405,25 @@ void Context::skipBlock() {
 
 namespace {
 
-typedef std::set<std::string> SuppressedCommands;
-typedef std::map<std::string, SuppressedCommands> SuppressionsForFile;
+typedef std::set<std::string_view> SuppressedCommands;
+typedef std::map<std::string_view, SuppressedCommands> SuppressionsForFile;
 typedef std::map<size_t, SuppressionsForFile> SuppressionsForPos;
 
 size_t suppressionCount = 0;
 SuppressionsForPos suppressions;
 SuppressionsForPos blockSuppressions;
 
-void suppress(const std::string & script, size_t pos, const std::string & command) {
+void suppress(std::string_view script, size_t pos, std::string_view command) {
 	suppressionCount++;
 	suppressions[pos][script].insert(command);
 }
 
-void suppressBlockEnd(const std::string & script, size_t pos, const std::string & command) {
+void suppressBlockEnd(std::string_view script, size_t pos, std::string_view command) {
 	suppressionCount++;
 	blockSuppressions[pos][script].insert(command);
 }
 
-bool contains(const SuppressionsForPos & list, const Context & context, const std::string & command) {
+bool contains(const SuppressionsForPos & list, const Context & context, std::string_view command) {
 	
 	SuppressionsForPos::const_iterator i0 = list.find(context.getPosition());
 	if(i0 == list.end()) {
@@ -789,7 +794,7 @@ size_t initSuppressions() {
 		
 	public:
 		
-		explicit FakeCommand(const std::string & name) : Command(name) { }
+		explicit FakeCommand(std::string_view name) : Command(name) { }
 		
 		Result execute(Context & context) {
 			ARX_UNUSED(context);
@@ -809,11 +814,11 @@ size_t initSuppressions() {
 	return suppressionCount;
 }
 
-bool isSuppressed(const Context & context, const std::string & command) {
+bool isSuppressed(const Context & context, std::string_view command) {
 	return contains(suppressions, context, command);
 }
 
-bool isBlockEndSuprressed(const Context & context, const std::string & command) {
+bool isBlockEndSuprressed(const Context & context, std::string_view command) {
 	return contains(blockSuppressions, context, command);
 }
 

src/scene/ChangeLevel.cpp
@@ -109,6 +109,7 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 
 #include "script/Script.h"
 #include "script/ScriptEvent.h"
+#include "script/ScriptUtils.h"
 
 #include "util/String.h"
 
@@ -1658,7 +1659,7 @@ static long ARX_CHANGELEVEL_Pop_Player(std::string_view target, float angle) {
 	}
 	ARX_PLAYER_Quest_Init();
 	for(int i = 0; i < asp->nb_PlayerQuest; i++) {
-		ARX_PLAYER_Quest_Add(script::loadUnlocalized(util::toLowercase(util::loadString(dat + pos, SAVED_QUEST_SLOT_SIZE))));
+		ARX_PLAYER_Quest_Add(util::toLowercase(script::toLocalizationKey(util::loadString(dat + pos, SAVED_QUEST_SLOT_SIZE))));
 		pos += SAVED_QUEST_SLOT_SIZE;
 	}
 	
@@ -1680,7 +1681,7 @@ static long ARX_CHANGELEVEL_Pop_Player(std::string_view target, float angle) {
 	for(int i = 0; i < asp->Nb_Mapmarkers; i++) {
 		const SavedMapMarkerData * acmd = reinterpret_cast<const SavedMapMarkerData *>(dat + pos);
 		pos += sizeof(SavedMapMarkerData);
-		g_miniMap.mapMarkerAdd(Vec2f(acmd->x, acmd->y), acmd->lvl, script::loadUnlocalized(util::toLowercase(util::loadString(acmd->name))));
+		g_miniMap.mapMarkerAdd(Vec2f(acmd->x, acmd->y), acmd->lvl, util::toLowercase(script::toLocalizationKey(util::loadString(acmd->name))));
 	}
 	
 	ARX_PLAYER_Restore_Skin();
@@ -1855,7 +1856,7 @@ static Entity * ARX_CHANGELEVEL_Pop_IO(std::string_view idString, EntityInstance
 		io->angle = ais->angle;
 		io->scale = ais->scale;
 		io->weight = ais->weight;
-		io->locname = script::loadUnlocalized(util::toLowercase(util::loadString(ais->locname)));
+		io->locname = util::toLowercase(script::toLocalizationKey(util::loadString(ais->locname)));
 		io->gameFlags = GameFlags::load(ais->gameFlags); // TODO save/load flags
 		io->material = Material(ais->material); // TODO save/load enum
 		
@@ -1906,7 +1907,7 @@ static Entity * ARX_CHANGELEVEL_Pop_IO(std::string_view idString, EntityInstance
 		io->stepmaterial = util::toLowercase(util::loadString(ais->stepmaterial));
 		io->armormaterial = util::toLowercase(util::loadString(ais->armormaterial));
 		io->weaponmaterial = util::toLowercase(util::loadString(ais->weaponmaterial));
-		io->strikespeech = script::loadUnlocalized(util::toLowercase(util::loadString(ais->strikespeech)));
+		io->strikespeech = util::toLowercase(script::toLocalizationKey(util::loadString(ais->strikespeech)));
 		
 		for(size_t i = 0; i < MAX_ANIMS; i++) {
 			

src/script/ScriptEvent.h
@@ -32,14 +32,7 @@ struct SCRIPT_EVENT {
 
 extern SCRIPT_EVENT AS_EVENT[];
 
-namespace script {
-
-//! strip [] brackets
-std::string loadUnlocalized(const std::string & str);
-
-class Command;
-
-} // namespace script
+namespace script { class Command; }
 
 class ScriptEvent {
 	

src/script/ScriptUtils.h
@@ -31,6 +31,9 @@
 
 namespace script {
 
+//! strip [] brackets
+std::string_view toLocalizationKey(std::string_view str);
+
 inline u64 flag(char c) {
 	if(c >= '0' && c <= '9') {
 		return (u64(1) << (c - '0'));
@@ -45,7 +48,7 @@ inline bool test_flag(u64 flg, char c) {
 	return (flg & flag(c)) != 0;
 }
 
-inline u64 flags(const std::string & flags) {
+inline u64 flags(std::string_view flags) {
 	
 	
 	size_t i = 0;
@@ -95,7 +98,7 @@ public:
 	explicit Context(const EERIE_SCRIPT * script, size_t pos, Entity * sender, Entity * entity,
 	                 ScriptMessage msg, const ScriptParameters & parameters);
 	
-	std::string getStringVar(const std::string & name) const;
+	std::string getStringVar(std::string_view name) const;
 	std::string getFlags();
 	std::string getWord();
 	void skipWord();
@@ -113,7 +116,7 @@ public:
 	
 	float getFloat();
 	
-	float getFloatVar(const std::string & name) const;
+	float getFloatVar(std::string_view name) const;
 	
 	/*!
 	 * Skip input until the end of the current line.
@@ -123,7 +126,7 @@ public:
 	
 	void skipBlock();
 	
-	bool jumpToLabel(const std::string & target, bool substack = false);
+	bool jumpToLabel(std::string_view target, bool substack = false);
 	bool returnToCaller();
 	
 	const EERIE_SCRIPT * getScript() const { return m_script; }
@@ -152,7 +155,7 @@ public:
 	
 	static const long AnyEntity = -1;
 	
-	explicit Command(const std::string & name, long entityFlags = 0)
+	explicit Command(std::string_view name, long entityFlags = 0)
 		: m_name(name), m_entityFlags(entityFlags) { }
 	
 	virtual Result execute(Context & context) = 0;
@@ -171,9 +174,9 @@ public:
 	
 };
 
-bool isSuppressed(const Context & context, const std::string & command);
+bool isSuppressed(const Context & context, std::string_view command);
 
-bool isBlockEndSuprressed(const Context & context, const std::string & command);
+bool isBlockEndSuprressed(const Context & context, std::string_view command);
 
 size_t initSuppressions();
 

src/script/ScriptedConversation.cpp
@@ -197,11 +197,7 @@ public:
 		
 		DebugScript(' ' << options << " \"" << text << '"');
 		
-		if(!text.empty() && text[0] == '[') {
-			text = loadUnlocalized(text);
-		}
-		
-		notification_add(std::move(text));
+		notification_add(std::string(toLocalizationKey(text)));
 		
 		return Success;
 	}
@@ -373,7 +369,7 @@ public:
 			return Success;
 		}
 		
-		std::string data = loadUnlocalized(context.getStringVar(text));
+		std::string data(toLocalizationKey(context.getStringVar(text)));
 		
 		DebugScript(' ' << options << ' ' << data); // TODO debug more
 		
@@ -424,7 +420,7 @@ public:
 			return Success;
 		}
 		
-		std::string data = loadUnlocalized(context.getStringVar(text));
+		std::string data(toLocalizationKey(context.getStringVar(text)));
 		
 		if(data.empty()) {
 			return Success;
@@ -451,7 +447,7 @@ public:
 	
 	Result execute(Context & context) {
 		
-		context.getEntity()->strikespeech = script::loadUnlocalized(context.getWord());
+		context.getEntity()->strikespeech = toLocalizationKey(context.getWord());
 		
 		DebugScript(' ' << context.getEntity()->strikespeech);
 		

src/script/ScriptedIOProperties.cpp
@@ -249,7 +249,7 @@ public:
 	
 	Result execute(Context & context) {
 		
-		context.getEntity()->locname = loadUnlocalized(context.getWord());
+		context.getEntity()->locname = toLocalizationKey(context.getWord());
 		
 		DebugScript(' ' << context.getEntity()->locname);
 		

src/script/ScriptedInterface.cpp
@@ -142,11 +142,11 @@ public:
 			type = Note::SmallNote;
 		}
 		
-		std::string text = loadUnlocalized(context.getWord());
+		std::string text = context.getWord();
 		
 		DebugScript(' ' << tpname << ' ' << text);
 		
-		ARX_INTERFACE_NoteOpen(type, text);
+		ARX_INTERFACE_NoteOpen(type, toLocalizationKey(text));
 		
 		return Success;
 	}
@@ -341,11 +341,11 @@ public:
 		
 		if(remove) {
 			
-			std::string marker = loadUnlocalized(context.getWord());
+			std::string marker = context.getWord();
 			
 			DebugScript(' ' << options << ' ' << marker);
 			
-			g_miniMap.mapMarkerRemove(marker);
+			g_miniMap.mapMarkerRemove(toLocalizationKey(marker));
 			
 		} else {
 			
@@ -353,11 +353,11 @@ public:
 			float y = context.getFloat();
 			int level = int(context.getFloat());
 			
-			std::string marker = loadUnlocalized(context.getWord());
+			std::string marker = context.getWord();
 			
 			DebugScript(' ' << options << ' ' << x << ' ' << y << ' ' << level << ' ' << marker);
 			
-			g_miniMap.mapMarkerAdd(Vec2f(x, y), level, std::move(marker));
+			g_miniMap.mapMarkerAdd(Vec2f(x, y), level, std::string(toLocalizationKey(marker)));
 			
 		}
 		

src/script/ScriptedPlayer.cpp
@@ -229,7 +229,7 @@ public:
 	
 	Result execute(Context & context) {
 		
-		std::string name = loadUnlocalized(context.getWord());
+		std::string name(toLocalizationKey(context.getWord()));
 		
 		DebugScript(' ' << name);
 		

commit 8bdfb46591219e65ba4f5f80513b8c7bbe9614b8

ScriptUtils: Fix converting arguments trailing junk to float
Was broken in commit 485cbf8. The old atof() version parses however much
it can while the boost::lexical_cast version defaults to 0.f if ti can't
parse the entire string. Revert to the old behavior but faster.

Fixes: issue #1616

src/script/ScriptUtils.cpp
@@ -26,6 +26,8 @@
 
 #include "game/Entity.h"
 #include "graphics/data/Mesh.h"
+#include "util/Number.h"
+
 
 namespace script {
 
@@ -309,11 +311,7 @@ float Context::getFloatVar(std::string_view name) const {
 		return GETVarValueFloat(getEntity()->m_variables, name);
 	}
 	
-	try {
-		return boost::lexical_cast<float>(name);
-	} catch(...) {
-		return 0.f;
-	}
+	return util::parseFloat(name);
 }
 
 size_t Context::skipCommand() {