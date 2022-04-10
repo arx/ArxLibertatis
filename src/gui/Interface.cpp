arx/ArxLibertatis

commit 1e0b0ca686816c0bb92f79d7a6ed39dd73581797

Inventory: Replace duplicated function

src/gui/Interface.cpp
@@ -1813,11 +1813,12 @@ void ArxGame::manageEditorControls() {
 				bSecondary = true;
 			}
 			
-			RemoveFromAllInventories(FlyingOverIO);
+                       removeFromInventories(FlyingOverIO);
 			FlyingOverIO->show = SHOW_FLAG_IN_INVENTORY;
 			
-			if(FlyingOverIO->ioflags & IO_GOLD)
+			if(FlyingOverIO->ioflags & IO_GOLD) {
 				ARX_SOUND_PlayInterface(g_snd.GOLD);
+			}
 			
 			ARX_SOUND_PlayInterface(g_snd.INVSTD);
 			
@@ -2012,17 +2013,17 @@ void ArxGame::manageEditorControls() {
 				
 				if(bOk) {
 					
+					if(io && io->show == SHOW_FLAG_ON_PLAYER) {
+						ARX_EQUIPMENT_UnEquip(entities.player(), io);
+						removeFromInventories(io);
+						io->bbox2D.max.x = -1;
+					}
+					
 					Set_DragInter(io);
 					
 					if(io) {
 						
 						ARX_PLAYER_Remove_Invisibility();
-						
-						if(DRAGINTER->show == SHOW_FLAG_ON_PLAYER) {
-							ARX_EQUIPMENT_UnEquip(entities.player(), DRAGINTER);
-							RemoveFromAllInventories(DRAGINTER);
-							DRAGINTER->bbox2D.max.x = -1;
-						}
 						
 						if((io->ioflags & IO_NPC) || (io->ioflags & IO_FIX)) {
 							Set_DragInter(NULL);

src/game/Entity.cpp
@@ -278,7 +278,7 @@ void Entity::cleanReferences() {
 	
 	ARX_INTERACTIVE_DestroyDynamicInfo(this);
 	
-	RemoveFromAllInventories(this);
+	removeFromInventories(this);
 	
 	ARX_SCRIPT_Timer_Clear_For_IO(this);
 	

src/game/Equipment.cpp
@@ -926,22 +926,9 @@ void ARX_EQUIPMENT_Equip(Entity * target, Entity * toequip)
 	if(!target || !toequip || target != entities.player())
 		return;
 
-	EntityHandle validid = EntityHandle();
+	EntityHandle validid = toequip->index();
 
-	for(size_t i = 0; i < entities.size(); i++) {
-		const EntityHandle handle = EntityHandle(i);
-		Entity * e = entities[handle];
-		
-		if(e == toequip) {
-			validid = handle;
-			break;
-		}
-	}
-
-	if(validid == EntityHandle())
-		return;
-
-	RemoveFromAllInventories(toequip);
+	removeFromInventories(toequip);
 	toequip->show = SHOW_FLAG_ON_PLAYER; // on player
 
 	if(toequip == DRAGINTER)

src/game/Inventory.cpp
@@ -963,38 +963,6 @@ Vec3f GetItemWorldPositionSound(const Entity * io) {
 }
 
 /*!
- * \brief Seeks an IO in all Inventories to remove it
- */
-void RemoveFromAllInventories(const Entity * io) {
-	
-	if(!io) {
-		return;
-	}
-	
-	// Seek IO in Player Inventory/ies
-	playerInventory.remove(io);
-	
-	// Seek IO in Other IO's Inventories
-	for(size_t i = 0; i < entities.size(); i++) {
-		const EntityHandle handle = EntityHandle(i);
-		Entity * e = entities[handle];
-		
-		if(!e || !e->inventory)
-			continue;
-		
-		INVENTORY_DATA * id = e->inventory;
-		for(long j = 0; j < id->m_size.y; j++) {
-		for(long k = 0; k < id->m_size.x; k++) {
-			if(id->slot[k][j].io == io) {
-				id->slot[k][j].io = NULL;
-				id->slot[k][j].show = true;
-			}
-		}
-		}
-	}
-}
-
-/*!
  * \brief Seeks an IO in all Inventories to replace it by another IO
  */
 void CheckForInventoryReplaceMe(Entity * io, Entity * old) {
@@ -1257,7 +1225,7 @@ void ARX_INVENTORY_TakeAllFromSecondaryInventory() {
 			Entity * io = slot.io;
 			
 			if(!(io->ioflags & IO_GOLD))
-				RemoveFromAllInventories(io);
+				removeFromInventories(io);
 			
 			if(playerInventory.insert(io)) {
 				bSound = true;
@@ -1290,7 +1258,7 @@ void ARX_INVENTORY_ReOrder() {
 		
 		Entity * io = slot.io;
 		
-		RemoveFromAllInventories(io);
+		getIoInventory(TSecondaryInventory->io).remove(io);
 		
 		InventoryPos pos(TSecondaryInventory->io->index(), 0, 0, 0);
 		bool inserted = getIoInventory(TSecondaryInventory->io).insert(io, pos);

src/game/Inventory.h
@@ -299,7 +299,6 @@ std::pair<Entity *, int> GetFromInventory(const Vec2s & pos);
 bool IsInPlayerInventory(Entity * io);
 bool IsInSecondaryInventory(Entity * io);
 bool InInventoryPos(const Vec2s & pos);
-void RemoveFromAllInventories(const Entity * io);
 Entity * ARX_INVENTORY_GetTorchLowestDurability();
 long Player_Arrow_Count();
 Entity * Player_Arrow_Count_Decrease();

src/game/Player.cpp
@@ -274,9 +274,9 @@ void ARX_PLAYER_ClickedOnTorch(Entity * io)
 		ARX_SOUND_PlaySFX(g_snd.TORCH_START);
 		player.torch_loop = ARX_SOUND_PlaySFX_loop(g_snd.TORCH_LOOP, NULL, 1.f);
 		
-		RemoveFromAllInventories(io);
-		player.torch = io;
+		removeFromInventories(io);
 		io->show = SHOW_FLAG_ON_PLAYER;
+		player.torch = io;
 
 		if(DRAGINTER == io)
 			DRAGINTER = NULL;

src/gui/hud/PlayerInventory.cpp
@@ -528,7 +528,7 @@ void PlayerInventoryHud::dragEntity(Entity * io, const InventoryPos & pos) {
 	}
 	
 	Set_DragInter(io, pos);
-	RemoveFromAllInventories(io);
+	removeFromInventories(io);
 	ARX_INVENTORY_IdentifyIO(io);
 	
 }

src/gui/hud/SecondaryInventory.cpp
@@ -402,7 +402,7 @@ void SecondaryInventoryHud::dragEntity(Entity * io, const InventoryPos & pos) {
 	}
 	
 	Set_DragInter(io, pos);
-	RemoveFromAllInventories(io);
+	removeFromInventories(io);
 	ARX_INVENTORY_IdentifyIO(io);
 	
 }

commit a5f419a055d62319f1a36c95e8239aee8f79aeed

Interface: Fix double click sound when unequipping armor
Was broken in commit 1e0b0ca.

src/gui/Interface.cpp
@@ -1889,7 +1889,7 @@ void ArxGame::manageEditorControls() {
 				if(io && (!g_cursorOverBook || io->show == SHOW_FLAG_ON_PLAYER)) {
 					
 					if(io->show == SHOW_FLAG_ON_PLAYER) {
-						ARX_EQUIPMENT_UnEquip(entities.player(), io);
+						ARX_EQUIPMENT_UnEquip(entities.player(), io, 1);
 						io->bbox2D.max.x = -1;
 					}
 					