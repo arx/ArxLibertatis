38eb8eb602f6e949d130b7538e1b7e0418351036
Daniel Scharrer <daniel@constexpr.org>
2022-04-10 13:32:02 +0200

    Update copyright years

--- src/graphics/image/ImageSave.cpp
+++ src/graphics/image/ImageSave.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 Arx Libertatis Team (see the AUTHORS file)
+ * Copyright 2018-2021 Arx Libertatis Team (see the AUTHORS file)
  *
  * This file is part of Arx Libertatis.
  *
