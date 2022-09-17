755fb6c679d6b1dd81d1a625a3d3daf9b261653b
Daniel Scharrer <daniel@constexpr.org>
2022-09-17 22:13:10 +0200

    MagicMissile: Declare methods final

--- src/graphics/effects/MagicMissile.h
+++ src/graphics/effects/MagicMissile.h
@@ -57,8 +57,8 @@ public:
 	void SetTTL(GameDuration aulTTL);
 	
 	void Create(const Vec3f & startPos, const Anglef & angles);
-	void Update(GameDuration timeDelta) override;
-	void Render() override;
+	void Update(GameDuration timeDelta) final;
+	void Render() final;
 	
 	bool bExplo;
 	bool bMove;
