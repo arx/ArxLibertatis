d7d7f16eac20eaee15af6dcee0eb3ac3b7a5f44b
Daniel Scharrer <daniel@constexpr.org>
2022-09-17 20:52:44 +0200

    AnimationRender: Make parameter const

--- src/animation/AnimationRender.cpp
+++ src/animation/AnimationRender.cpp
@@ -854,7 +854,7 @@ static void PrepareAnimatedObjectHalo(HaloInfo & haloInfo, const Vec3f & pos,
 
 // TODO copy-paste halo
 static void AddAnimatedObjectHalo(const HaloInfo & haloInfo, const unsigned short * paf,
-                                  float invisibility, EERIE_3DOBJ * eobj, Entity * io,
+                                  float invisibility, EERIE_3DOBJ * eobj, const Entity * io,
                                   TexturedVertex * tvList) {
 	
 	IO_HALO * curhalo = nullptr;
