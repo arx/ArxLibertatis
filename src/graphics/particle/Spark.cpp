1dfb6c4006fc63e837c074ade5d2305063b2bc41
Daniel Scharrer <daniel@constexpr.org>
2023-01-03 10:46:46 +0100

    Update copyright years

--- src/graphics/particle/Spark.cpp
+++ src/graphics/particle/Spark.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 Arx Libertatis Team (see the AUTHORS file)
+ * Copyright 2016-2022 Arx Libertatis Team (see the AUTHORS file)
  *
  * This file is part of Arx Libertatis.
  *
--- src/gui/Note.cpp
+++ src/gui/Note.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 Arx Libertatis Team (see the AUTHORS file)
+ * Copyright 2012-2022 Arx Libertatis Team (see the AUTHORS file)
  *
  * This file is part of Arx Libertatis.
  *
