arx/ArxLibertatis

commit 239cff7ef9691ad808bacffc3367e8ea94a4d41a

NPC: Use std::array

src/scene/SaveFormat.h
@@ -764,16 +764,16 @@ struct SavedExtraRotate {
 	operator EERIE_EXTRA_ROTATE() const {
 		EERIE_EXTRA_ROTATE a;
 		static_assert(SAVED_MAX_EXTRA_ROTATE <= MAX_EXTRA_ROTATE, "array size mismatch");
-		std::transform(group_number, group_number + SAVED_MAX_EXTRA_ROTATE, a.group_number, saved_toObjGroup);
-		std::copy(group_rotate, group_rotate + SAVED_MAX_EXTRA_ROTATE, a.group_rotate);
+		std::transform(group_number, group_number + SAVED_MAX_EXTRA_ROTATE, a.group_number.begin(), saved_toObjGroup);
+		std::copy(group_rotate, group_rotate + SAVED_MAX_EXTRA_ROTATE, a.group_rotate.begin());
 		return a;
 	}
 	
 	SavedExtraRotate & operator=(const EERIE_EXTRA_ROTATE & b) {
 		flags = 0;
 		static_assert(SAVED_MAX_EXTRA_ROTATE <= MAX_EXTRA_ROTATE, "array size mismatch");
-		std::transform(b.group_number, b.group_number + SAVED_MAX_EXTRA_ROTATE, group_number, saved_fromObjGroup);
-		std::copy(b.group_rotate, b.group_rotate + SAVED_MAX_EXTRA_ROTATE, group_rotate);
+		std::transform(b.group_number.begin(), b.group_number.end(), group_number, saved_fromObjGroup);
+		std::copy(b.group_rotate.begin(), b.group_rotate.end(), group_rotate);
 		#ifdef ARX_DEBUG
 		for(size_t i = SAVED_MAX_EXTRA_ROTATE; i < MAX_EXTRA_ROTATE; i++) {
 			arx_assert(b.group_number[i] == ObjVertGroup());

src/game/NPC.h
@@ -47,6 +47,7 @@ ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
 #ifndef ARX_GAME_NPC_H
 #define ARX_GAME_NPC_H
 
+#include <array>
 #include <string_view>
 
 #include "game/Entity.h"
@@ -131,8 +132,8 @@ struct IO_PATHFIND {
 };
 
 struct EERIE_EXTRA_ROTATE {
-	ObjVertGroup group_number[MAX_EXTRA_ROTATE];
-	Anglef group_rotate[MAX_EXTRA_ROTATE];
+	std::array<ObjVertGroup, MAX_EXTRA_ROTATE> group_number;
+	std::array<Anglef, MAX_EXTRA_ROTATE> group_rotate;
 };
 
 struct EERIE_EXTRA_SCALE {

commit 6a970e07983d47d3b4a4fdea6431d99c03aefff0

ChangeLevel: Fix out of bounds access to NPC extra_rotate data in saves
This affects both reading and writing saves so could corrupt both the
rotation angles as well as the first stacked target. The write also
touches the saved blood color but that is overwritten with the correct
value later.

Only affects NPCs with extra rotation data - i.e. those that hat the
look around (-l) or stare at (-a) behaviors active at some point.

Was broken in commit 239cff7.

Fixes: issue #1668

src/scene/ChangeLevel.cpp
@@ -2068,7 +2068,7 @@ static Entity * ARX_CHANGELEVEL_Pop_IO(std::string_view idString, EntityInstance
 						io->_npcdata->ex_rotate = new EERIE_EXTRA_ROTATE();
 					}
 					static_assert(SAVED_MAX_EXTRA_ROTATE <= MAX_EXTRA_ROTATE, "array size mismatch");
-					for(size_t i = 0; i < MAX_EXTRA_ROTATE; i++) {
+					for(size_t i = 0; i < SAVED_MAX_EXTRA_ROTATE; i++) {
 						if(!io->obj ||
 						   as->ex_rotate.group_number[i] < 0 ||
 						   size_t(as->ex_rotate.group_number[i]) >= io->obj->grouplist.size()) {
@@ -2079,6 +2079,10 @@ static Entity * ARX_CHANGELEVEL_Pop_IO(std::string_view idString, EntityInstance
 							io->_npcdata->ex_rotate->group_number[i] = VertexGroupId(as->ex_rotate.group_number[i]);
 							io->_npcdata->ex_rotate->group_rotate[i] = as->ex_rotate.group_rotate[i];
 						}
+					}
+					for(size_t i = SAVED_MAX_EXTRA_ROTATE; i < MAX_EXTRA_ROTATE; i++) {
+						io->_npcdata->ex_rotate->group_number[i] = { };
+						io->_npcdata->ex_rotate->group_rotate[i] = { };
 					}
 				}
 				

src/scene/SaveFormat.h
@@ -768,11 +768,12 @@ struct SavedExtraRotate {
 	SavedExtraRotate & operator=(const EERIE_EXTRA_ROTATE & b) {
 		flags = 0;
 		static_assert(SAVED_MAX_EXTRA_ROTATE <= MAX_EXTRA_ROTATE, "array size mismatch");
-		std::transform(b.group_number.begin(), b.group_number.end(), group_number, [](VertexGroupId group) {
+		std::transform(b.group_number.begin(), b.group_number.begin() + SAVED_MAX_EXTRA_ROTATE, group_number,
+		               [](VertexGroupId group) {
 			// TODO this breaks when the object changes between saving and loading
 			return group ? s16(group) : s16(-1);
 		});
-		std::copy(b.group_rotate.begin(), b.group_rotate.end(), group_rotate);
+		std::copy(b.group_rotate.begin(), b.group_rotate.begin() + SAVED_MAX_EXTRA_ROTATE, group_rotate);
 		#ifdef ARX_DEBUG
 		for(size_t i = SAVED_MAX_EXTRA_ROTATE; i < MAX_EXTRA_ROTATE; i++) {
 			arx_assert(!b.group_number[i]);